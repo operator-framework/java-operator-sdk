89277dff7716844a60d71056ea26f4d77e2bc246
csviri <csviri@gmail.com>
2019-05-28 22:22:27 +0200

    - decoration updates of maven modules - default finalizer for controller

--- operator-framework/src/main/java/jkube/operator/Controller.java
+++ operator-framework/src/main/java/jkube/operator/Controller.java
@@ -4,10 +4,13 @@ import io.fabric8.kubernetes.client.CustomResource;
 import io.fabric8.kubernetes.client.CustomResourceDoneable;
 import io.fabric8.kubernetes.client.CustomResourceList;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE})
 public @interface Controller {
 
     String DEFAULT_FINALIZER = "default";
