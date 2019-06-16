f5092b76788eb6d1ad3ab23e23aec3751c442287
csviri <csviri@gmail.com>
2019-06-16 22:55:03 +0200

    extracting shared files

--- sample/spring-boot/src/main/java/com/github/containersolutions/operator/sample/CustomServiceList.java
+++
@@ -1,6 +0,0 @@
-package com.github.containersolutions.operator.sample;
-
-import io.fabric8.kubernetes.client.CustomResourceList;
-
-public class CustomServiceList extends CustomResourceList<CustomService> {
-}
