28fa02d5184c8261a2e68b7c8e8d8b9e7a09111f
csviri <csviri@gmail.com>
2019-09-19 12:51:57 +0200

    requirements for event scheduler

--- operator-framework/src/main/java/com/github/containersolutions/operator/EventScheduler.java
+++ operator-framework/src/main/java/com/github/containersolutions/operator/EventScheduler.java
@@ -4,7 +4,6 @@ package com.github.containersolutions.operator;
 import io.fabric8.kubernetes.client.CustomResource;
 import io.fabric8.kubernetes.client.KubernetesClientException;
 import io.fabric8.kubernetes.client.Watcher;
-
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
 import org.slf4j.Logger;
@@ -16,6 +15,28 @@ import java.util.Map;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+
+/**
+ * Requirements:
+ * <ul>
+ *   <li>Only 1 event should be processed at a time for same custom resource
+ *   (metadata.name is the id, but kind and api should be taken into account)</li>
+ *   <li>If event processing fails it should be rescheduled with retry - with limited number of retried
+ *   and exponential time slacks (pluggable reschedule strategy in future?)</li>
+ *   <li>if there are multiple events received for the same resource process only the last one. (Others can be discarded)
+ *   User resourceVersion to check which is the latest. Put the new one at the and of the queue?
+ *   </li>
+ *   <li>Avoid starvation, so on retry put back resource at the end of the queue.</li>
+ *   <li>The selecting event from a queue should not be naive. So for example:
+ *     If we cannot pick the last event because an event for that resource is currently processing just gor for the next one.
+ *     (Maybe is good to represent this queue with a list.) Or if and event is rescheduled
+ *     (skip if there is not enough time left since last execution)
+ *   </li>
+ *   <li>Threading approach thus thread pool size and/or implementation should be configurable</li>
+ * </ul>
+ *
+ * @param <R>
+ */
 
 public class EventScheduler<R extends CustomResource> implements Watcher<R> {
 
