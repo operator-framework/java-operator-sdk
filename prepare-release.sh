1d4b4dea00aec1a440c38cd4805cca343f0196a9
Adam Sandor <10675791+adam-sandor@users.noreply.github.com>
2019-09-27 20:20:24 +0200

    add script to prepare version numbers for release

---
+++ prepare-release.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+mvn mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.nextIncrementalVersion} versions:commit
+
