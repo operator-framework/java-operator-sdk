0515c3f2bcdda3b619fe7b3722bcfe7be3163337
Adam Sandor <10675791+adam-sandor@users.noreply.github.com>
2019-12-10 16:19:57 +0100

    add mysql-schema-operator sample

---
+++ samples/mysql-schema/src/main/java/com/github/containersolutions/operator/sample/SchemaController.java
@@ -0,0 +1,89 @@
+package com.github.containersolutions.operator.sample;
+
+import com.github.containersolutions.operator.Context;
+import com.github.containersolutions.operator.api.Controller;
+import com.github.containersolutions.operator.api.ResourceController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Optional;
+
+import static java.lang.String.format;
+
+@Controller(customResourceClass = Schema.class,
+        kind = SchemaController.KIND,
+        group = SchemaController.GROUP,
+        customResourceListClass = SchemaList.class,
+        customResourceDonebaleClass = SchemaDoneable.class)
+public class SchemaController implements ResourceController<Schema> {
+
+    static final String KIND = "MySQLSchema";
+    static final String GROUP = "mysql.sample.javaoperatorsdk";
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @Override
+    public Optional<Schema> createOrUpdateResource(Schema schema, Context<Schema> context) {
+        try {
+            Connection connection = getConnection();
+            ResultSet resultSet = connection.createStatement().executeQuery(
+                    format("SELECT schema_name FROM information_schema.schemata WHERE schema_name = \"%1$s\"",
+                            schema.getMetadata().getName()));
+            if (!resultSet.first()) {
+                connection.createStatement().execute(format("CREATE SCHEMA `%1$s` DEFAULT CHARACTER SET %2$s",
+                        schema.getMetadata().getName(),
+                        schema.getSpec().getEncoding()));
+
+                SchemaStatus status = new SchemaStatus();
+                status.setUrl(format("jdbc:mysql://%1$s/%2$s",
+                        System.getenv("MYSQL_HOST"),
+                        schema.getMetadata().getName()));
+                status.setStatus("CREATED");
+                schema.setStatus(status);
+
+                log.info("Schema {} created", schema.getMetadata().getName());
+                return Optional.of(schema);
+            } else {
+                return Optional.of(schema);
+            }
+        } catch (SQLException e) {
+            log.error("Error while creating Schema", e);
+
+            SchemaStatus status = new SchemaStatus();
+            status.setUrl(null);
+            status.setStatus("ERROR");
+            schema.setStatus(status);
+
+            return Optional.of(schema);
+        }
+    }
+
+    @Override
+    public boolean deleteResource(Schema schema, Context<Schema> context) {
+        log.info("Execution deleteResource for: {}", schema.getMetadata().getName());
+
+        try {
+            Connection connection = getConnection();
+            connection.createStatement().execute("DROP DATABASE `" + schema.getMetadata().getName() + "`");
+            log.info("Deleted Schema '{}'", schema.getMetadata().getName());
+
+            return true;
+        } catch (SQLException e) {
+            log.error("Error while trying to delete Schema", e);
+            return false;
+        }
+    }
+
+    private Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(format("jdbc:mysql://%1$s:%2$s?user=%3$s&password=%4$s",
+                System.getenv("MYSQL_HOST"),
+                System.getenv("MYSQL_PORT") != null ? System.getenv("MYSQL_PORT") : "3306",
+                System.getenv("MYSQL_USER"),
+                System.getenv("MYSQL_PASSWORD")));
+    }
+
+}
