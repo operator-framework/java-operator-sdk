0360db487856787ad6729f47ffe86bbe27ddf346
Adam Sandor <10675791+adam-sandor@users.noreply.github.com>
2019-12-10 16:19:57 +0100

    add mysql-schema-operator sample

---
+++ samples/mysql-schema/src/main/java/com/github/containersolutions/operator/sample/SchemaStatus.java
@@ -0,0 +1,24 @@
+package com.github.containersolutions.operator.sample;
+
+public class SchemaStatus {
+
+    private String url;
+
+    private String status;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
