2c2f7783189b74b61b8da80e16b7156b0057ee17
csviri <csviri@gmail.com>
2020-02-13 15:14:24 +0100

    improving on corner case situation

--- operator-framework/src/main/java/com/github/containersolutions/operator/processing/EventScheduler.java
+++ operator-framework/src/main/java/com/github/containersolutions/operator/processing/EventScheduler.java
@@ -75,6 +75,14 @@ public class EventScheduler implements Watcher<CustomResource> {
         log.debug("Scheduling event: {}", event);
         try {
             lock.lock();
+            if (event.getResource().getMetadata().getDeletionTimestamp() != null && event.getAction() == Action.DELETED) {
+                // Not that we always use finalizers, we want to process delete event just in corner case,
+                // when we are not able to add finalizer (lets say because of optimistic error, and the resource was deleted instantly).
+                // We want to skip in case of finalizer was there since we don't want to execute delete method always at least 2x,
+                // which would be the result if we don't skip here. (If there is no deletion timestamp if resource deleted without finalizer.
+                log.debug("Skipping delete event since deletion timestamp is present on resource, so finalizer was in place.");
+                return;
+            }
             if (eventStore.receivedMoreRecentEventBefore(event)) {
                 log.debug("Skipping event processing since was processed event with newer version before. {}", event);
                 return;
