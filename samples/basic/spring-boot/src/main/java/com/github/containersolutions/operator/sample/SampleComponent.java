865aab58f42f3794d7a66e7e6054841674d1d575
csviri <csviri@gmail.com>
2020-04-28 10:24:26 +0200

    Revert "removed spring boot support, added spring boot sample"
    This reverts commit cff8b86dc74f5c564d8e7bf9661112d9a4b3f7cd.
    
    # Conflicts:
    #	README.md
    #	pom.xml

---
+++ samples/basic/spring-boot/src/main/java/com/github/containersolutions/operator/sample/SampleComponent.java
@@ -0,0 +1,34 @@
+package com.github.containersolutions.operator.sample;
+
+import com.github.containersolutions.operator.Operator;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.dsl.internal.CustomResourceOperationsImpl;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+/**
+ * This component just showcases what beans are registered.
+ */
+@Component
+public class SampleComponent {
+
+    private final Operator operator;
+
+    private final KubernetesClient kubernetesClient;
+
+    private final CustomServiceController customServiceController;
+
+    /**
+     * You can use qualifier for custom resource operation in case there are more custom resources for the operator
+     */
+    private final CustomResourceOperationsImpl customResourceOperations;
+
+    public SampleComponent(Operator operator, KubernetesClient kubernetesClient,
+                           CustomServiceController customServiceController,
+                           @Qualifier(CustomServiceController.KIND) CustomResourceOperationsImpl customResourceOperations) {
+        this.operator = operator;
+        this.kubernetesClient = kubernetesClient;
+        this.customServiceController = customServiceController;
+        this.customResourceOperations = customResourceOperations;
+    }
+}
