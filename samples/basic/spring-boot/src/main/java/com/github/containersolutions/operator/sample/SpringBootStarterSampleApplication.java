4fb9261e278e58161130514ebf1e9e05e8f7a601
csviri <csviri@gmail.com>
2020-04-28 10:19:42 +0200

    Revert "removed spring boot support, added spring boot sample"
    This reverts commit cff8b86dc74f5c564d8e7bf9661112d9a4b3f7cd.
    
    # Conflicts:
    #	README.md
    #	pom.xml

--- samples/basic/spring-boot/src/main/java/com/github/containersolutions/operator/sample/SpringBootStarterSampleApplication.java
+++ samples/basic/spring-boot/src/main/java/com/github/containersolutions/operator/sample/SpringBootStarterSampleApplication.java
@@ -1,8 +1,16 @@
 package com.github.containersolutions.operator.sample;
 
+import com.github.containersolutions.operator.api.Controller;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
+/**
+ * Note that we have multiple options here either we can add this component scan as seen below. Or annotate controllers
+ * with @Component or @Service annotation or just register the bean within a spring "@Configuration".
+ */
+@ComponentScan(includeFilters = {@ComponentScan.Filter(type = FilterType.ANNOTATION, value = Controller.class)})
 @SpringBootApplication
 public class SpringBootStarterSampleApplication {
 
