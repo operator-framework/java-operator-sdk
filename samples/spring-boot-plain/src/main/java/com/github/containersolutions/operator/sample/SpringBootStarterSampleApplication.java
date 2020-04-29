c4b18f088c555c4bed8f9bd84d56422df6a5d7cf
csviri <csviri@gmail.com>
2020-04-29 14:03:16 +0200

    fix sample logging issue

--- samples/spring-boot-plain/src/main/java/com/github/containersolutions/operator/sample/SpringBootStarterSampleApplication.java
+++ samples/spring-boot-plain/src/main/java/com/github/containersolutions/operator/sample/SpringBootStarterSampleApplication.java
@@ -3,10 +3,6 @@ package com.github.containersolutions.operator.sample;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Note that we have multiple options here either we can add this component scan as seen below. Or annotate controllers
- * with @Component or @Service annotation or just register the bean within a spring "@Configuration".
- */
 @SpringBootApplication
 public class SpringBootStarterSampleApplication {
 
