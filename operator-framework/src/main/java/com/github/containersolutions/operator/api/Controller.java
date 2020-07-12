b28f9dfec7b510473298b7e01f2976dfd414035a
attila.meszaros <attila.meszaros@container-solutions.com>
2020-07-12 17:13:33 +0200

    - docs code improvement

--- operator-framework/src/main/java/com/github/containersolutions/operator/api/Controller.java
+++ operator-framework/src/main/java/com/github/containersolutions/operator/api/Controller.java
@@ -20,7 +20,7 @@ public @interface Controller {
     String finalizerName() default DEFAULT_FINALIZER;
 
     /**
-     * If true, will process new event only if generation increased since the last processing, otherwise will
+     * If true, will dispatch new event to the controller if generation increased since the last processing, otherwise will
      * process all events.
      * See generation meta attribute
      * <a href="https://kubernetes.io/docs/tasks/access-kubernetes-api/custom-resources/custom-resource-definitions/#status-subresource">here</a>
