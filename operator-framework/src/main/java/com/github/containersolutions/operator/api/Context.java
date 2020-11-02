12aeacc8b96e5b7a3a17f8b466a8c6ff75646922
Marek Ruszczyk <kirek007@gmail.com>
2020-11-02 15:32:05 +0100

    Update package and group name

--- operator-framework/src/main/java/com/github/containersolutions/operator/api/Context.java
+++
@@ -1,9 +0,0 @@
-package com.github.containersolutions.operator.api;
-
-import io.fabric8.kubernetes.client.CustomResource;
-
-public interface Context<T extends CustomResource> {
-
-    RetryInfo retryInfo();
-
-}
