7075ddf2b7eeb9bda805f9700681f057b243ff00
Soroosh Sarabadani <soroosh.sarabadani@zalando.de>
2020-11-20 15:56:55 +0100

    Generate the class with javapoet

--- operator-framework/src/main/java/io/javaoperatorsdk/operator/processing/ControllerAnnotationProcessor.java
+++ operator-framework/src/main/java/io/javaoperatorsdk/operator/processing/ControllerAnnotationProcessor.java
@@ -1,11 +1,16 @@
 package io.javaoperatorsdk.operator.processing;
 
 import com.google.auto.service.AutoService;
+import com.squareup.javapoet.*;
 import com.sun.tools.javac.code.Symbol;
+import io.fabric8.kubernetes.api.builder.Function;
+import io.fabric8.kubernetes.client.CustomResourceDoneable;
+import io.quarkus.runtime.annotations.RegisterForReflection;
 
 import javax.annotation.processing.*;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
+import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.type.DeclaredType;
 import javax.lang.model.type.TypeMirror;
@@ -26,31 +31,34 @@ public class ControllerAnnotationProcessor extends AbstractProcessor {
                     = roundEnv.getElementsAnnotatedWith(annotation);
             annotatedElements.stream().filter(element -> element instanceof Symbol.ClassSymbol)
                     .map(e -> (Symbol.ClassSymbol) e)
-                    .forEach(controllerClassSymbol -> {
-                        generateDoneableClass(controllerClassSymbol);
-                    });
+                    .forEach(controllerClassSymbol -> generateDoneableClass(controllerClassSymbol));
         }
         return false;
     }
 
     private void generateDoneableClass(Symbol.ClassSymbol controllerClassSymbol) {
-        JavaFileObject builderFile = null;
         try {
             // TODO: the resourceType retrieval logic is currently very fragile, done for testing purposes and need to be improved to cover all possible conditions
             final TypeMirror resourceType = ((DeclaredType) controllerClassSymbol.getInterfaces().head).getTypeArguments().get(0);
             Symbol.ClassSymbol customerResourceSymbol = (Symbol.ClassSymbol) processingEnv.getElementUtils().getTypeElement(resourceType.toString());
-            builderFile = processingEnv.getFiler()
+            JavaFileObject builderFile = processingEnv.getFiler()
                     .createSourceFile(customerResourceSymbol.className() + "Doneable");
             try (PrintWriter out = new PrintWriter(builderFile.openWriter())) {
-                out.println("package " + customerResourceSymbol.packge().fullname + ";");
-                out.println("import io.quarkus.runtime.annotations.RegisterForReflection;");
-                out.println("import io.fabric8.kubernetes.api.builder.Function;");
-                out.println("import io.fabric8.kubernetes.client.CustomResourceDoneable;");
-                out.println();
-                out.println("@RegisterForReflection");
-                out.println("public class " + customerResourceSymbol.name + "Doneable " + " extends CustomResourceDoneable<" + customerResourceSymbol.name + "> {");
-                out.println("public " + customerResourceSymbol.name + "Doneable(" + customerResourceSymbol.name + " resource, Function function){ super(resource,function);}");
-                out.println("}");
+                final MethodSpec constructor = MethodSpec.constructorBuilder()
+                        .addModifiers(Modifier.PUBLIC)
+                        .addParameter(TypeName.get(resourceType), "resource")
+                        .addParameter(Function.class, "function")
+                        .addStatement("super(resource,function);")
+                        .build();
+                final TypeSpec typeSpec = TypeSpec.classBuilder(customerResourceSymbol.name + "Doneable")
+                        .addAnnotation(RegisterForReflection.class)
+                        .superclass(ParameterizedTypeName.get(ClassName.get(CustomResourceDoneable.class), TypeName.get(resourceType)))
+                        .addModifiers(Modifier.PUBLIC)
+                        .addMethod(constructor)
+                        .build();
+                JavaFile file = JavaFile.builder(customerResourceSymbol.packge().fullname.toString(), typeSpec)
+                        .build();
+                file.writeTo(out);
             }
         } catch (IOException ioException) {
             ioException.printStackTrace();
