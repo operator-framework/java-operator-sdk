07e4741c12b6a0d28610e2726fd4d6fa543f49a9
Chris Laprun <metacosm@gmail.com>
2020-11-26 12:19:02 +0000

    feat: Controllers can now be named for configuration purposes

--- operator-framework/src/main/java/io/javaoperatorsdk/operator/api/Controller.java
+++ operator-framework/src/main/java/io/javaoperatorsdk/operator/api/Controller.java
@@ -8,19 +8,21 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE})
 public @interface Controller {
-  String NULL = "";
-
-  String crdName();
-
-  /**
-   * Optional finalizer name, if it is not, the crdName will be used as the name of the finalizer
+    String NULL = "";
+    
+    String crdName();
+    
+    String name() default NULL;
+    
+    /**
+     * Optional finalizer name, if it is not,
+      the crdName will be used as the name of the finalizer
    * too.
-   */
-  String finalizerName() default NULL;
-
-  /**
-   * If true, will dispatch new event to the controller if generation increased since the last
-   * processing, otherwise will process all events. See generation meta attribute <a
+     */
+    String finalizerName() default NULL;
+    
+    /**
+     * If true, will dispatch new event to the controller if generation increased since the last* processing, otherwise will process all events. See generation meta attribute <a
    * href="https://kubernetes.io/docs/tasks/access-kubernetes-api/custom-resources/custom-resource-definitions/#status-subresource">here</a>
    */
   boolean generationAwareEventProcessing() default true;
