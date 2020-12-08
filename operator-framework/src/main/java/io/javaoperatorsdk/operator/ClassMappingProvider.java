ba4a9554503688334944a91aaac9d3dd559672fa
Soroosh Sarabadani <soroosh.sarabadani@zalando.de>
2020-12-08 23:12:26 +0100

    resolve conflicts

--- operator-framework/src/main/java/io/javaoperatorsdk/operator/ClassMappingProvider.java
+++ operator-framework/src/main/java/io/javaoperatorsdk/operator/ClassMappingProvider.java
@@ -1,55 +1,55 @@
 package io.javaoperatorsdk.operator;
 
-import org.apache.commons.lang3.ClassUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.*;
 import java.util.stream.Collectors;
-
+import org.apache.commons.lang3.ClassUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 class ClassMappingProvider {
-    private static final Logger log = LoggerFactory.getLogger(ClassMappingProvider.class);
+  private static final Logger log = LoggerFactory.getLogger(ClassMappingProvider.class);
 
-    static <T, V> Map<T, V> provide(final String resourcePath, T key, V value) {
-        Map<T, V> result = new HashMap();
-        try {
-            final var classLoader = Thread.currentThread().getContextClassLoader();
-            final Enumeration<URL> customResourcesMetadataList = classLoader.getResources(resourcePath);
-            for (Iterator<URL> it = customResourcesMetadataList.asIterator(); it.hasNext(); ) {
-                URL url = it.next();
+  static <T, V> Map<T, V> provide(final String resourcePath, T key, V value) {
+    Map<T, V> result = new HashMap();
+    try {
+      final var classLoader = Thread.currentThread().getContextClassLoader();
+      final Enumeration<URL> customResourcesMetadataList = classLoader.getResources(resourcePath);
+      for (Iterator<URL> it = customResourcesMetadataList.asIterator(); it.hasNext(); ) {
+        URL url = it.next();
 
-                List<String> classNamePairs = retrieveClassNamePairs(url);
-                classNamePairs.forEach(clazzPair -> {
-                    try {
-                        final String[] classNames = clazzPair.split(",");
-                        if (classNames.length != 2) {
-                            throw new IllegalStateException(String.format("%s is not valid Mapping metadata, defined in %s", clazzPair, url.toString()));
-                        }
+        List<String> classNamePairs = retrieveClassNamePairs(url);
+        classNamePairs.forEach(
+            clazzPair -> {
+              try {
+                final String[] classNames = clazzPair.split(",");
+                if (classNames.length != 2) {
+                  throw new IllegalStateException(
+                      String.format(
+                          "%s is not valid Mapping metadata, defined in %s",
+                          clazzPair, url.toString()));
+                }
 
-                        result.put(
-                                (T) ClassUtils.getClass(classNames[0]),
-                                (V) ClassUtils.getClass(classNames[1])
-                        );
-                    } catch (ClassNotFoundException e) {
-                        throw new RuntimeException(e);
-                    }
-                });
-            }
-            log.debug("Loaded Controller to CustomResource mappings {}", result);
-            return result;
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+                result.put(
+                    (T) ClassUtils.getClass(classNames[0]), (V) ClassUtils.getClass(classNames[1]));
+              } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+              }
+            });
+      }
+      log.debug("Loaded Controller to CustomResource mappings {}", result);
+      return result;
+    } catch (IOException e) {
+      throw new RuntimeException(e);
     }
+  }
 
-    private static List<String> retrieveClassNamePairs(URL url) throws IOException {
-        return new BufferedReader(
-                new InputStreamReader(url.openStream())
-        ).lines().collect(Collectors.toList());
-    }
+  private static List<String> retrieveClassNamePairs(URL url) throws IOException {
+    return new BufferedReader(new InputStreamReader(url.openStream()))
+        .lines()
+        .collect(Collectors.toList());
+  }
 }
--- operator-framework/src/main/java/io/javaoperatorsdk/operator/ControllerUtils.java
+++ operator-framework/src/main/java/io/javaoperatorsdk/operator/ControllerUtils.java
@@ -4,76 +4,76 @@ import io.fabric8.kubernetes.client.CustomResource;
 import io.fabric8.kubernetes.client.CustomResourceDoneable;
 import io.javaoperatorsdk.operator.api.Controller;
 import io.javaoperatorsdk.operator.api.ResourceController;
-
 import java.util.Map;
-
 
 public class ControllerUtils {
 
-    private static final String FINALIZER_NAME_SUFFIX = "/finalizer";
-    public static final String CONTROLLERS_RESOURCE_PATH = "javaoperatorsdk/controllers";
-    public static final String DONEABLES_RESOURCE_PATH = "javaoperatorsdk/doneables";
-    private static Map<Class<? extends ResourceController>, Class<? extends CustomResource>> controllerToCustomResourceMappings;
-    private static Map<Class<? extends CustomResource>, Class<? extends CustomResourceDoneable>> resourceToDoneableMappings;
+  private static final String FINALIZER_NAME_SUFFIX = "/finalizer";
+  public static final String CONTROLLERS_RESOURCE_PATH = "javaoperatorsdk/controllers";
+  public static final String DONEABLES_RESOURCE_PATH = "javaoperatorsdk/doneables";
+  private static Map<Class<? extends ResourceController>, Class<? extends CustomResource>>
+      controllerToCustomResourceMappings;
+  private static Map<Class<? extends CustomResource>, Class<? extends CustomResourceDoneable>>
+      resourceToDoneableMappings;
 
+  static {
+    controllerToCustomResourceMappings =
+        ClassMappingProvider.provide(
+            CONTROLLERS_RESOURCE_PATH, ResourceController.class, CustomResource.class);
+    resourceToDoneableMappings =
+        ClassMappingProvider.provide(
+            DONEABLES_RESOURCE_PATH, CustomResource.class, CustomResourceDoneable.class);
+  }
 
-    static {
-        controllerToCustomResourceMappings = ClassMappingProvider
-                .provide(CONTROLLERS_RESOURCE_PATH,
-                        ResourceController.class,
-                        CustomResource.class);
-        resourceToDoneableMappings = ClassMappingProvider
-                .provide(DONEABLES_RESOURCE_PATH,
-                        CustomResource.class,
-                        CustomResourceDoneable.class
-                );
+  static String getFinalizer(ResourceController controller) {
+    final String annotationFinalizerName = getAnnotation(controller).finalizerName();
+    if (!Controller.NULL.equals(annotationFinalizerName)) {
+      return annotationFinalizerName;
     }
+    return getAnnotation(controller).crdName() + FINALIZER_NAME_SUFFIX;
+  }
 
-    static String getFinalizer(ResourceController controller) {
-        final String annotationFinalizerName = getAnnotation(controller).finalizerName();
-        if (!Controller.NULL.equals(annotationFinalizerName)) {
-            return annotationFinalizerName;
-        }
-        return getAnnotation(controller).crdName() + FINALIZER_NAME_SUFFIX;
-    }
+  static boolean getGenerationEventProcessing(ResourceController<?> controller) {
+    return getAnnotation(controller).generationAwareEventProcessing();
+  }
 
-    static boolean getGenerationEventProcessing(ResourceController<?> controller) {
-        return getAnnotation(controller).generationAwareEventProcessing();
+  static <R extends CustomResource> Class<R> getCustomResourceClass(
+      ResourceController<R> controller) {
+    final Class<? extends CustomResource> customResourceClass =
+        controllerToCustomResourceMappings.get(controller.getClass());
+    if (customResourceClass == null) {
+      throw new IllegalArgumentException(
+          String.format(
+              "No custom resource has been found for controller %s",
+              controller.getClass().getCanonicalName()));
     }
+    return (Class<R>) customResourceClass;
+  }
 
-    static <R extends CustomResource> Class<R> getCustomResourceClass(ResourceController<R> controller) {
-        final Class<? extends CustomResource> customResourceClass = controllerToCustomResourceMappings
-                .get(controller.getClass());
-        if (customResourceClass == null) {
-            throw new IllegalArgumentException(
-                    String.format(
-                            "No custom resource has been found for controller %s",
-                            controller.getClass().getCanonicalName()
-                    )
-            );
-        }
-        return (Class<R>) customResourceClass;
-    }
+  static String getCrdName(ResourceController controller) {
+    return getAnnotation(controller).crdName();
+  }
 
-    static String getCrdName(ResourceController controller) {
-        return getAnnotation(controller).crdName();
+  public static <T extends CustomResource>
+      Class<? extends CustomResourceDoneable<T>> getCustomResourceDoneableClass(
+          ResourceController<T> controller) {
+    final Class<T> customResourceClass = getCustomResourceClass(controller);
+    final Class<? extends CustomResourceDoneable<T>> doneableClass =
+        (Class<? extends CustomResourceDoneable<T>>)
+            resourceToDoneableMappings.get(customResourceClass);
+    if (doneableClass == null) {
+      throw new RuntimeException(
+          String.format("No matching Doneable class found for %s", customResourceClass));
     }
+    return doneableClass;
+  }
 
-    public static <T extends CustomResource> Class<? extends CustomResourceDoneable<T>>
-    getCustomResourceDoneableClass(ResourceController<T> controller) {
-        final Class<T> customResourceClass = getCustomResourceClass(controller);
-        final Class<? extends CustomResourceDoneable<T>> doneableClass = (Class<? extends CustomResourceDoneable<T>>) resourceToDoneableMappings.get(customResourceClass);
-        if (doneableClass == null) {
-            throw new RuntimeException(String.format("No matching Doneable class found for %s", customResourceClass));
-        }
-        return doneableClass;
-    }
+  private static Controller getAnnotation(ResourceController<?> controller) {
+    return controller.getClass().getAnnotation(Controller.class);
+  }
 
-    private static Controller getAnnotation(ResourceController<?> controller) {
-        return controller.getClass().getAnnotation(Controller.class);
-    }
-
-    public static boolean hasGivenFinalizer(CustomResource resource, String finalizer) {
-        return resource.getMetadata().getFinalizers() != null && resource.getMetadata().getFinalizers().contains(finalizer);
-    }
+  public static boolean hasGivenFinalizer(CustomResource resource, String finalizer) {
+    return resource.getMetadata().getFinalizers() != null
+        && resource.getMetadata().getFinalizers().contains(finalizer);
+  }
 }
