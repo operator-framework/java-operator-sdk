53fccb9cb70858556d8b0c78b0dfbd5e87a578ed
Chris Laprun <metacosm@gmail.com>
2020-12-09 20:26:35 +0100

    feat: check that injection properly works

--- samples/quarkus/src/main/java/io/javaoperatorsdk/operator/sample/QuarkusOperator.java
+++ samples/quarkus/src/main/java/io/javaoperatorsdk/operator/sample/QuarkusOperator.java
@@ -4,6 +4,7 @@ import javax.inject.Inject;
 
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.javaoperatorsdk.operator.Operator;
+import io.javaoperatorsdk.operator.api.config.ConfigurationService;
 import io.quarkus.runtime.Quarkus;
 import io.quarkus.runtime.QuarkusApplication;
 import io.quarkus.runtime.annotations.QuarkusMain;
@@ -17,8 +18,8 @@ public class QuarkusOperator implements QuarkusApplication {
     @Inject
     Operator operator;
     
-    /*@Inject
-    ConfigurationService configuration;*/
+    @Inject
+    ConfigurationService configuration;
     
     public static void main(String... args) {
         Quarkus.run(QuarkusOperator.class, args);
@@ -27,9 +28,9 @@ public class QuarkusOperator implements QuarkusApplication {
     @Override
     public int run(String... args) throws Exception {
         System.out.println("operator = " + operator);
-        /*final var config = configuration.getConfigurationFor(new CustomServiceController(client));
+        final var config = configuration.getConfigurationFor(new CustomServiceController(client));
         System.out.println("CR class: " + config.getCustomResourceClass());
-        System.out.println("Doneable class = " + config.getDoneableClass());*/
+        System.out.println("Doneable class = " + config.getDoneableClass());
         Quarkus.waitForExit();
         return 0;
     }
