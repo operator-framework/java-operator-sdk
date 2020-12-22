5cbd79245086a419d36dd960bf552481c77a452c
soroosh <soroosh.sarabadani@gmail.com>
2020-12-22 10:50:46 +0100

    log warning crd not found

--- operator-framework-spring-boot-starter-test/src/main/java/io/javaoperatorsdk/operator/springboot/starter/test/TestConfiguration.java
+++ operator-framework-spring-boot-starter-test/src/main/java/io/javaoperatorsdk/operator/springboot/starter/test/TestConfiguration.java
@@ -12,6 +12,8 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import okhttp3.mockwebserver.MockWebServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,6 +21,8 @@ import org.springframework.util.ResourceUtils;
 
 @Configuration
 public class TestConfiguration {
+
+  private static final Logger log = LoggerFactory.getLogger(TestConfiguration.class);
 
   @Value("${io.javaoperatorsdk.test.crdPaths}")
   private List<String> crdPaths;
@@ -37,8 +41,7 @@ public class TestConfiguration {
   }
 
   @Bean
-  public KubernetesClient kubernetesClient(KubernetesMockServer server)
-      throws FileNotFoundException {
+  public KubernetesClient kubernetesClient(KubernetesMockServer server) {
     final var client = server.createClient();
 
     crdPaths.forEach(
@@ -47,8 +50,9 @@ public class TestConfiguration {
           try {
             crd =
                 Serialization.unmarshal(
-                    new FileInputStream(ResourceUtils.getFile("classpath:test-crd.yaml")));
+                    new FileInputStream(ResourceUtils.getFile(crdPath)));
           } catch (FileNotFoundException e) {
+            log.warn("CRD with path {} not found!", crdPath);
             e.printStackTrace();
             return;
           }
