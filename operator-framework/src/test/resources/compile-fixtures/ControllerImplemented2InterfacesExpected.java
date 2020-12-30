11a8c8cab7ad741578668d9efab9a8c85c5935f0
soroosh <soroosh.sarabadani@gmail.com>
2020-12-30 23:57:53 +0100

    fix test fixtures

--- operator-framework/src/test/resources/compile-fixtures/ControllerImplemented2InterfacesExpected.java
+++ operator-framework/src/test/resources/compile-fixtures/ControllerImplemented2InterfacesExpected.java
@@ -2,6 +2,7 @@ package io;
 
 import io.fabric8.kubernetes.api.builder.Function;
 import io.fabric8.kubernetes.client.CustomResourceDoneable;
+import java.lang.SuppressWarnings;
 
 public class MyCustomResourceDoneable extends CustomResourceDoneable<ControllerImplemented2Interfaces.MyCustomResource> {
     @SuppressWarnings("unchecked")
