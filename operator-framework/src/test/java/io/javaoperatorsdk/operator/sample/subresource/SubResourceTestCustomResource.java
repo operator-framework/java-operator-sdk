de1c9017fcd0c1760661c153012c6cc5c92544f1
Chris Laprun <metacosm@gmail.com>
2021-01-08 14:10:09 +0100

    fix: add Plural annotation to match CRD manifest

--- operator-framework/src/test/java/io/javaoperatorsdk/operator/sample/subresource/SubResourceTestCustomResource.java
+++ operator-framework/src/test/java/io/javaoperatorsdk/operator/sample/subresource/SubResourceTestCustomResource.java
@@ -4,11 +4,13 @@ import io.fabric8.kubernetes.api.model.Namespaced;
 import io.fabric8.kubernetes.client.CustomResource;
 import io.fabric8.kubernetes.model.annotation.Group;
 import io.fabric8.kubernetes.model.annotation.Kind;
+import io.fabric8.kubernetes.model.annotation.Plural;
 import io.fabric8.kubernetes.model.annotation.Version;
 
 @Group("sample.javaoperatorsdk")
 @Version("v1")
 @Kind("SubresourceSample")
+@Plural("subresourcesample")
 public class SubResourceTestCustomResource
     extends CustomResource<SubResourceTestCustomResourceSpec, SubResourceTestCustomResourceStatus>
     implements Namespaced {}
