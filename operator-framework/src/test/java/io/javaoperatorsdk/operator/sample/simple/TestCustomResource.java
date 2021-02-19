c6461a522cf94bc70cc825a90143753afe65afa5
Chris Laprun <metacosm@gmail.com>
2021-02-19 17:20:39 +0100

    feat: add ShortNames annotation to CRs

--- operator-framework/src/test/java/io/javaoperatorsdk/operator/sample/simple/TestCustomResource.java
+++ operator-framework/src/test/java/io/javaoperatorsdk/operator/sample/simple/TestCustomResource.java
@@ -4,11 +4,13 @@ import io.fabric8.kubernetes.api.model.Namespaced;
 import io.fabric8.kubernetes.client.CustomResource;
 import io.fabric8.kubernetes.model.annotation.Group;
 import io.fabric8.kubernetes.model.annotation.Kind;
+import io.fabric8.kubernetes.model.annotation.ShortNames;
 import io.fabric8.kubernetes.model.annotation.Version;
 
 @Group("sample.javaoperatorsdk")
 @Version("v1")
 @Kind("CustomService")
+@ShortNames("cs")
 public class TestCustomResource
     extends CustomResource<TestCustomResourceSpec, TestCustomResourceStatus>
     implements Namespaced {}
