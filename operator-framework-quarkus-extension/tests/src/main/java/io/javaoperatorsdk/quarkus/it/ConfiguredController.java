9d4cfef7ebe97761714c00fa017bfe08325d7af7
Chris Laprun <metacosm@gmail.com>
2021-03-09 14:27:10 +0100

    feat: extract quarkus extension to the Quarkiverse
    The extension is now found at
    io.quarkiverse.operatorsdk:quarkus-operator-sdk.

--- operator-framework-quarkus-extension/tests/src/main/java/io/javaoperatorsdk/quarkus/it/ConfiguredController.java
+++
@@ -1,9 +0,0 @@
-package io.javaoperatorsdk.quarkus.it;
-
-import io.javaoperatorsdk.operator.api.Controller;
-
-@Controller(name = ConfiguredController.NAME, namespaces = "foo")
-public class ConfiguredController extends AbstractController<ChildTestResource> {
-
-  public static final String NAME = "annotation";
-}
