e93b0cb4d27752e3c4d53c38824ffb77cfe6a1c2
Chris Laprun <metacosm@gmail.com>
2021-05-04 22:06:34 +0200

    chore: remove Spring Boot starters
    The starters have been moved to their own repositories in the
    java-operator-sdk organization.
    Fixes #414

--- operator-framework-spring-boot-starter/src/test/java/io/javaoperatorsdk/operator/springboot/starter/model/TestResourceList.java
+++
@@ -1,5 +0,0 @@
-package io.javaoperatorsdk.operator.springboot.starter.model;
-
-import io.fabric8.kubernetes.client.CustomResourceList;
-
-public class TestResourceList extends CustomResourceList<TestResource> {}
