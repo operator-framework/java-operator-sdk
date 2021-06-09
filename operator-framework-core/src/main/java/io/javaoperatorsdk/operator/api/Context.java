b91abe27082b57546afba98312cbb61d419d59cb
Tibor Eszes <tibor.eszes@container-solutions.com>
2021-06-09 21:21:41 +0200

    Add javadoc to everywhere in Context.java

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/api/Context.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/api/Context.java
@@ -4,9 +4,25 @@ import io.fabric8.kubernetes.client.CustomResource;
 import io.javaoperatorsdk.operator.processing.event.EventList;
 import java.util.Optional;
 
+/**
+ * Encapsulates information about the state of the cluster with respect to the provided custom
+ * resource class.
+ *
+ * @param <T> the {@link CustomResource} for which the context contains information
+ */
 public interface Context<T extends CustomResource> {
 
+  /**
+   * Gets the events related to the relevant custom resource
+   *
+   * @return the list of relevant {@link io.javaoperatorsdk.operator.processing.event.Event}s
+   */
   EventList getEvents();
 
+  /**
+   * Retrieves an object describing how to process retries.
+   *
+   * @return a {@link RetryInfo} object or null
+   */
   Optional<RetryInfo> getRetryInfo();
 }
