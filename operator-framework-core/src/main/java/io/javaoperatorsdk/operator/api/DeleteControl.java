adc4d6d930175b4eb326e329b9ead0d668c3c1a1
Tibor Eszes <tibor.eszes@container-solutions.com>
2021-06-09 19:33:11 +0200

    Add javadoc to everywhere in DeleteControl.java

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/api/DeleteControl.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/api/DeleteControl.java
@@ -1,5 +1,14 @@
 package io.javaoperatorsdk.operator.api;
 
+import io.fabric8.kubernetes.client.CustomResource;
+
+/**
+ * Encapsulates information about the logic executed when instances of the given custom resource are
+ * deleted. Returned from {@link ResourceController#createOrUpdateResource}. Informs the framework
+ * whether the finalizer associated with the custom resource can be deleted to allow K8s to delete
+ * the resource {@link DeleteControl#DEFAULT_DELETE}, or if the finalizer should stay in place
+ * {@link DeleteControl#NO_FINALIZER_REMOVAL}.
+ */
 public enum DeleteControl {
   DEFAULT_DELETE,
   NO_FINALIZER_REMOVAL
