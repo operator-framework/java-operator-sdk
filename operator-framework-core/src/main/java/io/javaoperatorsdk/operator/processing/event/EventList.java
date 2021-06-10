5be6c41d826769226233ac72bfd87a6b9beadd78
Tibor Eszes <tibor.eszes@container-solutions.com>
2021-06-10 11:58:39 +0200

    Add javadoc to everywhere in EventList.java

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/event/EventList.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/event/EventList.java
@@ -3,18 +3,41 @@ package io.javaoperatorsdk.operator.processing.event;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Encapsulates a list of {@link Event}s and related functionality.
+ */
 public class EventList {
 
+  /**
+   * The list of {@link Event}s encapsulated.
+   */
   private final List<Event> eventList;
 
+  /**
+   * Creates an instance out of a provided list of {@link Event}s
+   *
+   * @param eventList the list of {@link Event}s
+   */
   public EventList(List<Event> eventList) {
     this.eventList = eventList;
   }
 
+  /**
+   * Gets the encapsulated list of {@link Event}s.
+   *
+   * @return the list of {@link Event}s
+   */
   public List<Event> getList() {
     return eventList;
   }
 
+  /**
+   * Gets the last {@link Event} that occurred that matches the provided type.
+   *
+   * @param eventType the class of the sought {@link Event}
+   * @param <T> the class that the sought {@link Event} should be returned as
+   * @return the sought {@link Event}
+   */
   public <T extends Event> Optional<T> getLatestOfType(Class<T> eventType) {
     for (int i = eventList.size() - 1; i >= 0; i--) {
       Event event = eventList.get(i);
