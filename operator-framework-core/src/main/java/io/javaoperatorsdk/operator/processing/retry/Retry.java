c41ad5bf6acd720503eef8555cb8ae18de9279af
Tibor Eszes <tibor.eszes@container-solutions.com>
2021-06-10 11:37:31 +0200

    Add javadoc to everywhere in Retry.java

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/retry/Retry.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/retry/Retry.java
@@ -2,7 +2,17 @@ package io.javaoperatorsdk.operator.processing.retry;
 
 import io.javaoperatorsdk.operator.api.config.RetryConfiguration;
 
+/**
+ * Interface to be implemented by user-defined classes processing retries while executing logic in
+ * {@link io.javaoperatorsdk.operator.processing.event.EventHandler}s. Extends
+ * {@link RetryConfiguration} that configures how to process retries.
+ */
 public interface Retry extends RetryConfiguration {
 
+  /**
+   * Starts executing retries.
+   *
+   * @return the {@link RetryExecution} object detailing executed retries
+   */
   RetryExecution initExecution();
 }
