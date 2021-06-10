866fdfe8857ff504b272704f97b13a42e93d17ce
Tibor Eszes <tibor.eszes@container-solutions.com>
2021-06-10 11:16:53 +0200

    Add javadoc to everywhere in RetryInfo.java

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/api/RetryInfo.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/api/RetryInfo.java
@@ -1,8 +1,22 @@
 package io.javaoperatorsdk.operator.api;
 
+/**
+ * Interface to be implemented by user-defined classes encapsulating information about how many
+ * retries have been processed.
+ */
 public interface RetryInfo {
 
+  /**
+   * Gets how many attempts have been made so far.
+   *
+   * @return the number of attempts
+   */
   int getAttemptCount();
 
+  /**
+   * Gets whether the current attempt is the last attempt to be made.
+   *
+   * @return whether the current one is the last attempt
+   */
   boolean isLastAttempt();
 }
