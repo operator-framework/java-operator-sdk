3046bd1e31bd48f45ef5f13cf483eae0d1a83b42
Tibor Eszes <tibor.eszes@container-solutions.com>
2021-06-10 11:23:29 +0200

    Add javadoc to everywhere in RetryExecution.java

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/retry/RetryExecution.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/retry/RetryExecution.java
@@ -3,13 +3,17 @@ package io.javaoperatorsdk.operator.processing.retry;
 import io.javaoperatorsdk.operator.api.RetryInfo;
 import java.util.Optional;
 
+/**
+ * Interface to be implemented by user-defined objects detailing scheduling a currently retried
+ * execution. Extends the {@link RetryInfo} interface, as it details retry attempts made in the
+ * past.
+ */
 public interface RetryExecution extends RetryInfo {
 
   /**
-   * Calculates the delay for the next execution. This method should return 0, when called first
-   * time;
+   * Calculates the delay for the next execution, also called on the first execution.
    *
-   * @return
+   * @return the delay before the next execution in seconds
    */
   Optional<Long> nextDelay();
 }
