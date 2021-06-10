541c78e8bba50f6b723a6d15cf027681cb65e55a
Tibor Eszes <tibor.eszes@container-solutions.com>
2021-06-10 11:45:00 +0200

    Add javadoc to everywhere in EventHandler.java

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/event/EventHandler.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/event/EventHandler.java
@@ -2,10 +2,25 @@ package io.javaoperatorsdk.operator.processing.event;
 
 import java.io.Closeable;
 
+/**
+ * Interface to be implemented by user-defined event handler classes that encapsulate logic to be
+ * run as a response to cluster events. Implements Closeable so that a shutting down
+ * {@link io.javaoperatorsdk.operator.Operator}/{@link io.javaoperatorsdk.operator.api.Controller}
+ * can shut down all dependent event handling.
+ */
 public interface EventHandler extends Closeable {
 
+  /**
+   * Function that gets called as a response to cluster events to execute logic in response to them.
+   *
+   * @param event the {@link Event} instance detailing the cluster event
+   */
   void handleEvent(Event event);
 
+  /**
+   * Provides a default empty implementation to satisfy Closeable even if there is nothing to do on
+   * shutdown.
+   */
   @Override
   default void close() {}
 }
