ee50072f922321b3b675bfd9860237965abde113
Tibor Eszes <tibor.eszes@container-solutions.com>
2021-06-11 11:25:02 +0200

    Add javadoc to everywhere in Event.java

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/event/Event.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/event/Event.java
@@ -1,8 +1,21 @@
 package io.javaoperatorsdk.operator.processing.event;
 
+/**
+ * The interface all user-defined events must implement. Also implemented by {@link AbstractEvent}.
+ */
 public interface Event {
 
+  /**
+   * Gets the UID of the custom resource the event describes.
+   *
+   * @return the UID of the resource
+   */
   String getRelatedCustomResourceUid();
 
+  /**
+   * Gets the {@link EventSource} that the event originated from
+   *
+   * @return the originating {@link EventSource}
+   */
   EventSource getEventSource();
 }
