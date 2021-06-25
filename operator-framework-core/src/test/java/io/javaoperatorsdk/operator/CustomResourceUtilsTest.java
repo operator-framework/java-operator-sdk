908e377290378ec8e5589ff33300fb2392536049
Tibor Eszes <tibor.eszes@container-solutions.com>
2021-06-25 12:24:44 +0200

    Separate CRUtils test cases

--- operator-framework-core/src/test/java/io/javaoperatorsdk/operator/CustomResourceUtilsTest.java
+++ operator-framework-core/src/test/java/io/javaoperatorsdk/operator/CustomResourceUtilsTest.java
@@ -7,23 +7,34 @@ import org.junit.jupiter.api.Test;
 
 public class CustomResourceUtilsTest {
   @Test
-  public void assertNamespacedCustomResource() {
-    var clusterCrd = TestUtils.testCRD("Cluster");
+  public void assertClusterCustomResourceIsCluster() {
+    var crd = TestUtils.testCRD("Cluster");
 
-    CustomResourceUtils.assertCustomResource(TestCustomResource.class, clusterCrd);
+    CustomResourceUtils.assertCustomResource(TestCustomResource.class, crd);
+  }
 
-    Assertions.assertThrows(
-        OperatorException.class,
-        () ->
-            CustomResourceUtils.assertCustomResource(
-                NamespacedTestCustomResource.class, clusterCrd));
-
-    var namespacedCrd = TestUtils.testCRD("Namespaced");
+  @Test
+  public void assertClusterCustomResourceNotNamespaced() {
+    var crd = TestUtils.testCRD("Cluster");
 
     Assertions.assertThrows(
         OperatorException.class,
-        () -> CustomResourceUtils.assertCustomResource(TestCustomResource.class, namespacedCrd));
+        () -> CustomResourceUtils.assertCustomResource(NamespacedTestCustomResource.class, crd));
+  }
 
-    CustomResourceUtils.assertCustomResource(NamespacedTestCustomResource.class, namespacedCrd);
+  @Test
+  public void assertNamespacedCustomResourceIsNamespaced() {
+    var crd = TestUtils.testCRD("Namespaced");
+
+    CustomResourceUtils.assertCustomResource(NamespacedTestCustomResource.class, crd);
+  }
+
+  @Test
+  public void assertNamespacedCustomResourceNotCluster() {
+    var crd = TestUtils.testCRD("Namespaced");
+
+    Assertions.assertThrows(
+        OperatorException.class,
+        () -> CustomResourceUtils.assertCustomResource(TestCustomResource.class, crd));
   }
 }
