d80596c3041b8b9da76bd6271e55012cf44947c9
Chris Laprun <metacosm@gmail.com>
2021-09-16 15:18:41 +0200

    feat: make it easier to override ConfigurationService
    Fixes #542

---
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/api/config/ConfigurationServiceOverrider.java
@@ -0,0 +1,116 @@
+package io.javaoperatorsdk.operator.api.config;
+
+import io.javaoperatorsdk.operator.api.ResourceController;
+import java.util.Set;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.fabric8.kubernetes.client.Config;
+import io.fabric8.kubernetes.client.CustomResource;
+import io.javaoperatorsdk.operator.Metrics;
+
+public class ConfigurationServiceOverrider {
+  private final ConfigurationService original;
+  private Metrics metrics;
+  private Config clientConfig;
+  private boolean checkCR;
+  private int threadNumber;
+  private ObjectMapper mapper;
+  private int timeoutSeconds;
+
+  public ConfigurationServiceOverrider(
+      ConfigurationService original) {
+    this.original = original;
+    this.clientConfig = original.getClientConfiguration();
+    this.checkCR = original.checkCRDAndValidateLocalModel();
+    this.threadNumber = original.concurrentReconciliationThreads();
+    this.mapper = original.getObjectMapper();
+    this.timeoutSeconds = original.getTerminationTimeoutSeconds();
+    this.metrics = original.getMetrics();
+  }
+
+
+  public ConfigurationServiceOverrider withClientConfiguration(Config configuration) {
+    this.clientConfig = configuration;
+    return this;
+  }
+
+  public ConfigurationServiceOverrider checkingCRDAndValidateLocalModel(boolean check) {
+    this.checkCR = check;
+    return this;
+  }
+
+  public ConfigurationServiceOverrider withConcurrentReconciliationThreads(int threadNumber) {
+    this.threadNumber = threadNumber;
+    return this;
+  }
+
+  public ConfigurationServiceOverrider withObjectMapper(ObjectMapper mapper) {
+    this.mapper = mapper;
+    return this;
+  }
+
+  public ConfigurationServiceOverrider withTerminationTimeoutSeconds(int timeoutSeconds) {
+    this.timeoutSeconds = timeoutSeconds;
+    return this;
+  }
+
+  public ConfigurationServiceOverrider withMetrics(Metrics metrics) {
+    this.metrics = metrics;
+    return this;
+  }
+
+  public ConfigurationService build() {
+    return new ConfigurationService() {
+      @Override
+      public <R extends CustomResource> ControllerConfiguration<R> getConfigurationFor(
+          ResourceController<R> controller) {
+        return original.getConfigurationFor(controller);
+      }
+
+      @Override
+      public Set<String> getKnownControllerNames() {
+        return original.getKnownControllerNames();
+      }
+
+      @Override
+      public Version getVersion() {
+        return original.getVersion();
+      }
+
+      @Override
+      public Config getClientConfiguration() {
+        return clientConfig;
+      }
+
+      @Override
+      public boolean checkCRDAndValidateLocalModel() {
+        return checkCR;
+      }
+
+      @Override
+      public int concurrentReconciliationThreads() {
+        return threadNumber;
+      }
+
+      @Override
+      public ObjectMapper getObjectMapper() {
+        return mapper;
+      }
+
+      @Override
+      public int getTerminationTimeoutSeconds() {
+        return timeoutSeconds;
+      }
+
+      @Override
+      public Metrics getMetrics() {
+        return metrics;
+      }
+    };
+  }
+
+  public static ConfigurationServiceOverrider override(ConfigurationService original) {
+    return new ConfigurationServiceOverrider(original);
+  }
+}
--- samples/pure-java/src/main/java/io/javaoperatorsdk/operator/sample/PureJavaApplicationRunner.java
+++ samples/pure-java/src/main/java/io/javaoperatorsdk/operator/sample/PureJavaApplicationRunner.java
@@ -3,13 +3,17 @@ package io.javaoperatorsdk.operator.sample;
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.javaoperatorsdk.operator.Operator;
+import io.javaoperatorsdk.operator.api.config.ConfigurationServiceOverrider;
 import io.javaoperatorsdk.operator.config.runtime.DefaultConfigurationService;
 
 public class PureJavaApplicationRunner {
 
   public static void main(String[] args) {
     KubernetesClient client = new DefaultKubernetesClient();
-    Operator operator = new Operator(client, DefaultConfigurationService.instance());
+    Operator operator = new Operator(client,
+        ConfigurationServiceOverrider.override(DefaultConfigurationService.instance())
+            .withConcurrentReconciliationThreads(2)
+            .build());
     operator.register(new CustomServiceController(client));
     operator.start();
   }
