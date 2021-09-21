5514f181ff08a9731feee0833c8edf8aebda12c4
Chris Laprun <metacosm@gmail.com>
2021-09-21 21:04:40 +0200

    feat: make debugging thread pool configurable

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/api/config/ExecutorServiceProducer.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/api/config/ExecutorServiceProducer.java
@@ -1,10 +1,7 @@
 package io.javaoperatorsdk.operator.api.config;
 
-import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.RejectedExecutionHandler;
-import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
@@ -16,8 +13,7 @@ class ExecutorServiceProducer {
 
   static ExecutorService getExecutor(int threadPoolSize) {
     final var gotSet =
-        executor.compareAndSet(null, new DebugThreadPoolExecutor(threadPoolSize, threadPoolSize, 0L,
-            TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>()));
+        executor.compareAndSet(null, new InstrumentedExecutorService(threadPoolSize));
     final var result = executor.get();
     if (!gotSet) {
       // check that we didn't try to change the pool size
@@ -30,35 +26,19 @@ class ExecutorServiceProducer {
     return result;
   }
 
-  private static class DebugThreadPoolExecutor extends ThreadPoolExecutor {
+  private static class InstrumentedExecutorService extends ThreadPoolExecutor {
+    private final boolean debug;
 
-    public DebugThreadPoolExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime,
-        TimeUnit unit,
-        BlockingQueue<Runnable> workQueue) {
-      super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue);
-    }
-
-    public DebugThreadPoolExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime,
-        TimeUnit unit, BlockingQueue<Runnable> workQueue,
-        ThreadFactory threadFactory) {
-      super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, threadFactory);
-    }
-
-    public DebugThreadPoolExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime,
-        TimeUnit unit, BlockingQueue<Runnable> workQueue,
-        RejectedExecutionHandler handler) {
-      super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, handler);
-    }
-
-    public DebugThreadPoolExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime,
-        TimeUnit unit, BlockingQueue<Runnable> workQueue,
-        ThreadFactory threadFactory, RejectedExecutionHandler handler) {
-      super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, threadFactory, handler);
+    public InstrumentedExecutorService(int corePoolSize) {
+      super(corePoolSize, corePoolSize, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>());
+      debug = Utils.debugThreadPool();
     }
 
     @Override
     public void shutdown() {
-      Thread.dumpStack();
+      if (debug) {
+        Thread.dumpStack();
+      }
       super.shutdown();
     }
   }
