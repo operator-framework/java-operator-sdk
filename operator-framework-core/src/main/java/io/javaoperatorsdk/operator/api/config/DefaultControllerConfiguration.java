0a79dde74d54f60e0260fdba5e279169dad85d11
csviri <csviri@gmail.com>
2021-10-01 10:50:15 +0200

    putting back depracted method

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/api/config/DefaultControllerConfiguration.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/api/config/DefaultControllerConfiguration.java
@@ -54,6 +54,29 @@ public class DefaultControllerConfiguration<R extends CustomResource<?, ?>>
     setConfigurationService(service);
   }
 
+  @Deprecated
+  public DefaultControllerConfiguration(
+      String associatedControllerClassName,
+      String name,
+      String crdName,
+      String finalizer,
+      boolean generationAware,
+      Set<String> namespaces,
+      RetryConfiguration retryConfiguration) {
+    this(
+        associatedControllerClassName,
+        name,
+        crdName,
+        finalizer,
+        generationAware,
+        namespaces,
+        retryConfiguration,
+        null,
+        null,
+        null,
+        null);
+  }
+
   @Override
   public String getName() {
     return name;
