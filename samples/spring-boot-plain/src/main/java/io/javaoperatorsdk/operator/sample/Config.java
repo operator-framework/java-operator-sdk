9972a7bcf4e40478f4cb4ad3d7343467c1089813
Ioannis Canellos <iocanel@gmail.com>
2021-10-06 14:22:24 +0300

    refactor: Allow creating an Operator without a KubernetesClient instance.

--- samples/spring-boot-plain/src/main/java/io/javaoperatorsdk/operator/sample/Config.java
+++ samples/spring-boot-plain/src/main/java/io/javaoperatorsdk/operator/sample/Config.java
@@ -5,8 +5,6 @@ import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import io.fabric8.kubernetes.client.DefaultKubernetesClient;
-import io.fabric8.kubernetes.client.KubernetesClient;
 import io.javaoperatorsdk.operator.Operator;
 import io.javaoperatorsdk.operator.api.ResourceController;
 import io.javaoperatorsdk.operator.config.runtime.DefaultConfigurationService;
@@ -15,19 +13,14 @@ import io.javaoperatorsdk.operator.config.runtime.DefaultConfigurationService;
 public class Config {
 
   @Bean
-  public KubernetesClient kubernetesClient() {
-    return new DefaultKubernetesClient();
-  }
-
-  @Bean
-  public CustomServiceController customServiceController(KubernetesClient client) {
-    return new CustomServiceController(client);
+  public CustomServiceController customServiceController() {
+    return new CustomServiceController();
   }
 
   // Register all controller beans
   @Bean(initMethod = "start", destroyMethod = "stop")
-  public Operator operator(KubernetesClient client, List<ResourceController> controllers) {
-    Operator operator = new Operator(client, DefaultConfigurationService.instance());
+  public Operator operator(List<ResourceController> controllers) {
+    Operator operator = new Operator(DefaultConfigurationService.instance());
     controllers.forEach(operator::register);
     return operator;
   }
