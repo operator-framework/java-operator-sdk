8e9300e4fa655e8e582c22093b7b1cfc13f8a5c3
Attila Mészáros <csviri@gmail.com>
2021-10-13 15:15:28 +0200

    Refined Interface of `EventSource` and `EventSourceManager` (#597)
    * WIP
    
    * Addressing Custom Resource by Name and Namespace refactor + Informer Cache WIP
    
    * fix: DefaultEventHandler init from EventSourceManager
    
    * fix: custom resource selector test improvement
    
    * fix: wip test imrpovements
    
    * fix: test improvements
    
    * fix: further improvements
    
    * fix: build
    
    * feature: add mvn jar to gitignore
    
    * Exposing CustomResourceEventSource and informers
    
    * fix: cleanup
    
    * fix: remove caching optimization since it not possible anymore with informer
    
    * fix:  formatting
    
    * refactor: make name/namespace final
    
    * feature: Simple label selector support
    
    * fix: formatting
    
    * fix: code inspection reports
    
    * fix: merge from v2
    
    * fix: removed most deprecated apis
    
    * chore: renaming vars named k8sClient to kubernetsClient
    
    * chore(deps): bump jandex-maven-plugin from 1.1.1 to 1.2.1 (#592)
    
    Bumps [jandex-maven-plugin](https://github.com/wildfly/jandex-maven-plugin) from 1.1.1 to 1.2.1.
    - [Release notes](https://github.com/wildfly/jandex-maven-plugin/releases)
    - [Commits](https://github.com/wildfly/jandex-maven-plugin/compare/1.1.1...1.2.1)
    
    ---
    updated-dependencies:
    - dependency-name: org.jboss.jandex:jandex-maven-plugin
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * chore(deps-dev): bump mockito-core from 3.12.4 to 4.0.0 (#591)
    
    Bumps [mockito-core](https://github.com/mockito/mockito) from 3.12.4 to 4.0.0.
    - [Release notes](https://github.com/mockito/mockito/releases)
    - [Commits](https://github.com/mockito/mockito/compare/v3.12.4...v4.0.0)
    
    ---
    updated-dependencies:
    - dependency-name: org.mockito:mockito-core
      dependency-type: direct:development
      update-type: version-update:semver-major
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * feature: Build PR on v2
    
    * chore(ci): use Java 17
    
    * chore(ci): use only Temurin distribution
    
    * fix: Updated informer mapping to CustomResourceID
    
    * chore: add generics to PostExecutionControl to reduce IDEs noise (#594)
    
    * chore: polish the junit5 extension (#593)
    
    * fix: EventSourceManager API wip
    
    * fix: code review fixes
    
    * fix: improvements of Event Source related APIs
    
    * fix: remarks from code review
    
    Co-authored-by: Chris Laprun <metacosm@gmail.com>
    Co-authored-by: Ioannis Canellos <iocanel@gmail.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Luca Burgazzoli <lburgazzoli@users.noreply.github.com>

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/event/EventSourceManager.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/event/EventSourceManager.java
@@ -2,8 +2,7 @@ package io.javaoperatorsdk.operator.processing.event;
 
 import java.io.Closeable;
 import java.io.IOException;
-import java.util.Map;
-import java.util.Optional;
+import java.util.Set;
 
 import io.fabric8.kubernetes.client.CustomResource;
 import io.javaoperatorsdk.operator.OperatorException;
@@ -14,29 +13,15 @@ public interface EventSourceManager<T extends CustomResource<?, ?>> extends Clos
   /**
    * Add the {@link EventSource} identified by the given <code>name</code> to the event manager.
    *
-   * @param name the name of the {@link EventSource} to add
    * @param eventSource the {@link EventSource} to register
    * @throws IllegalStateException if an {@link EventSource} with the same name is already
    *         registered.
    * @throws OperatorException if an error occurred during the registration process
    */
-  void registerEventSource(String name, EventSource eventSource)
+  void registerEventSource(EventSource eventSource)
       throws IllegalStateException, OperatorException;
 
-  /**
-   * Remove the {@link EventSource} identified by the given <code>name</code> from the event
-   * manager.
-   *
-   * @param name the name of the {@link EventSource} to remove
-   * @return an optional {@link EventSource} which would be empty if no {@link EventSource} have
-   *         been registered with the given name.
-   */
-  Optional<EventSource> deRegisterEventSource(String name);
-
-  Optional<EventSource> deRegisterCustomResourceFromEventSource(
-      String name, CustomResourceID customResourceUid);
-
-  Map<String, EventSource> getRegisteredEventSources();
+  Set<EventSource> getRegisteredEventSources();
 
   CustomResourceEventSource<T> getCustomResourceEventSource();
 
