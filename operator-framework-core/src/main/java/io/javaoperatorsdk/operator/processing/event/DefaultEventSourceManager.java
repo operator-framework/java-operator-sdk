ec137dc1fff73515f6a95ab8e7d98d0caa444ca5
Chris Laprun <metacosm@gmail.com>
2021-10-21 22:42:34 +0200

    fix: more generics fixes

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/event/DefaultEventSourceManager.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/event/DefaultEventSourceManager.java
@@ -15,7 +15,7 @@ import io.javaoperatorsdk.operator.processing.event.internal.CustomResourceEvent
 import io.javaoperatorsdk.operator.processing.event.internal.TimerEventSource;
 
 public class DefaultEventSourceManager<R extends CustomResource<?, ?>>
-    implements EventSourceManager {
+    implements EventSourceManager<R> {
 
   private static final Logger log = LoggerFactory.getLogger(DefaultEventSourceManager.class);
 
@@ -23,7 +23,7 @@ public class DefaultEventSourceManager<R extends CustomResource<?, ?>>
   private final Set<EventSource> eventSources = Collections.synchronizedSet(new HashSet<>());
   private DefaultEventHandler<R> defaultEventHandler;
   private TimerEventSource<R> retryAndRescheduleTimerEventSource;
-  private CustomResourceEventSource customResourceEventSource;
+  private CustomResourceEventSource<R> customResourceEventSource;
 
   DefaultEventSourceManager(DefaultEventHandler<R> defaultEventHandler) {
     init(defaultEventHandler);
@@ -57,7 +57,7 @@ public class DefaultEventSourceManager<R extends CustomResource<?, ?>>
         try {
           eventSource.close();
         } catch (Exception e) {
-          log.warn("Error closing {} -> {}", eventSource);
+          log.warn("Error closing {} -> {}", eventSource, e);
         }
       }
       eventSources.clear();
@@ -98,7 +98,7 @@ public class DefaultEventSourceManager<R extends CustomResource<?, ?>>
     }
   }
 
-  public TimerEventSource getRetryAndRescheduleTimerEventSource() {
+  public TimerEventSource<R> getRetryAndRescheduleTimerEventSource() {
     return retryAndRescheduleTimerEventSource;
   }
 
