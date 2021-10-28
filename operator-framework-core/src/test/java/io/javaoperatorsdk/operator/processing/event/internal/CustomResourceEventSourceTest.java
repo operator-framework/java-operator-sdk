0cc051237f1639b9a419f9b0beaf3d1c8cb0e31d
Chris Laprun <metacosm@users.noreply.github.com>
2021-10-28 13:04:59 +0200

    fix: restart event handler (#632)
    Properly start event handler when starting the event source. 
    Minor clean-ups.
    Fixes #630
    Co-authored-by: csviri <csviri@gmail.com>

--- operator-framework-core/src/test/java/io/javaoperatorsdk/operator/processing/event/internal/CustomResourceEventSourceTest.java
+++ operator-framework-core/src/test/java/io/javaoperatorsdk/operator/processing/event/internal/CustomResourceEventSourceTest.java
@@ -1,5 +1,6 @@
 package io.javaoperatorsdk.operator.processing.event.internal;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -7,7 +8,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import io.fabric8.kubernetes.api.model.KubernetesResourceList;
+import io.fabric8.kubernetes.api.model.ListOptions;
+import io.fabric8.kubernetes.client.Watch;
 import io.fabric8.kubernetes.client.Watcher;
+import io.fabric8.kubernetes.client.dsl.FilterWatchListMultiDeletable;
 import io.fabric8.kubernetes.client.dsl.MixedOperation;
 import io.fabric8.kubernetes.client.dsl.Resource;
 import io.javaoperatorsdk.operator.Metrics;
@@ -15,11 +19,19 @@ import io.javaoperatorsdk.operator.TestUtils;
 import io.javaoperatorsdk.operator.api.config.ConfigurationService;
 import io.javaoperatorsdk.operator.api.config.DefaultControllerConfiguration;
 import io.javaoperatorsdk.operator.processing.ConfiguredController;
+import io.javaoperatorsdk.operator.processing.CustomResourceCache;
+import io.javaoperatorsdk.operator.processing.DefaultEventHandler;
+import io.javaoperatorsdk.operator.processing.EventDispatcher;
+import io.javaoperatorsdk.operator.processing.event.DefaultEventSourceManager;
 import io.javaoperatorsdk.operator.processing.event.EventHandler;
 import io.javaoperatorsdk.operator.sample.simple.TestCustomResource;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.doCallRealMethod;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.timeout;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -100,6 +112,46 @@ class CustomResourceEventSourceTest {
 
     customResourceEventSource.eventReceived(Watcher.Action.MODIFIED, customResource1);
     verify(eventHandler, times(2)).handleEvent(any());
+  }
+
+  @Test
+  public void restartingShouldResumeEventHandling() throws IOException {
+    final var cr = TestUtils.testCustomResource();
+
+    CustomResourceCache customResourceCache = new CustomResourceCache();
+    customResourceCache.cacheResource(cr);
+    DefaultEventSourceManager defaultEventSourceManagerMock =
+        mock(DefaultEventSourceManager.class);
+    EventDispatcher eventDispatcherMock = mock(EventDispatcher.class);
+    DefaultEventHandler local = new DefaultEventHandler(eventDispatcherMock, "Test",
+        null);
+    local.setEventSourceManager(defaultEventSourceManagerMock);
+    when(defaultEventSourceManagerMock.getCache()).thenReturn(customResourceCache);
+    doCallRealMethod().when(defaultEventSourceManagerMock).getLatestResource(any());
+    doCallRealMethod().when(defaultEventSourceManagerMock).getLatestResources(any());
+    doCallRealMethod().when(defaultEventSourceManagerMock).getLatestResourceUids(any());
+    doCallRealMethod().when(defaultEventSourceManagerMock).cacheResource(any(), any());
+
+    final var mock = mock(FilterWatchListMultiDeletable.class);
+    when(mock.watch((ListOptions) any(), any())).thenReturn(mock(Watch.class));
+    when(client.inAnyNamespace()).thenReturn(mock);
+
+    customResourceEventSource.setEventHandler(local);
+
+    customResourceEventSource.eventReceived(Watcher.Action.MODIFIED, cr);
+    verify(eventDispatcherMock, timeout(50).times(1)).handleExecution(any());
+
+    customResourceEventSource.close();
+    assertFalse(local.isRunning());
+    customResourceEventSource.eventReceived(Watcher.Action.MODIFIED, cr);
+    // mockito times method is not reset and keeps increasing so here we stay at 1 call
+    verify(eventDispatcherMock, timeout(50).times(1)).handleExecution(any());
+
+    customResourceEventSource.start();
+    assertTrue(local.isRunning());
+    customResourceEventSource.eventReceived(Watcher.Action.MODIFIED, cr);
+    // we're expecting another call to the dispatcher, so total number of calls should now be 2
+    verify(eventDispatcherMock, timeout(50).times(2)).handleExecution(any());
   }
 
   private static class TestConfiguredController extends ConfiguredController<TestCustomResource> {
