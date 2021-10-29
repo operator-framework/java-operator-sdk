f2e481e0e69e152f58a39f2bc12efb95833f649a
Ádám Sándor <10675791+adam-sandor@users.noreply.github.com>
2021-10-29 13:19:09 +0200

    add nicers tostring methods

--- sample-operators/tomcat-operator/src/main/java/io/javaoperatorsdk/operator/sample/Tomcat.java
+++ sample-operators/tomcat-operator/src/main/java/io/javaoperatorsdk/operator/sample/Tomcat.java
@@ -4,8 +4,14 @@ import io.fabric8.kubernetes.api.model.Namespaced;
 import io.fabric8.kubernetes.client.CustomResource;
 import io.fabric8.kubernetes.model.annotation.Group;
 import io.fabric8.kubernetes.model.annotation.Version;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 
 @Group("tomcatoperator.io")
 @Version("v1")
 public class Tomcat extends CustomResource<TomcatSpec, TomcatStatus> implements Namespaced {
+
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.JSON_STYLE);
+    }
 }
--- sample-operators/tomcat-operator/src/main/java/io/javaoperatorsdk/operator/sample/Webapp.java
+++ sample-operators/tomcat-operator/src/main/java/io/javaoperatorsdk/operator/sample/Webapp.java
@@ -4,6 +4,8 @@ import io.fabric8.kubernetes.api.model.Namespaced;
 import io.fabric8.kubernetes.client.CustomResource;
 import io.fabric8.kubernetes.model.annotation.Group;
 import io.fabric8.kubernetes.model.annotation.Version;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 
 /**
  * Represents a web application deployed in a Tomcat deployment
@@ -11,4 +13,8 @@ import io.fabric8.kubernetes.model.annotation.Version;
 @Group("tomcatoperator.io")
 @Version("v1")
 public class Webapp extends CustomResource<WebappSpec, WebappStatus> implements Namespaced {
+
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.JSON_STYLE);
+    }
 }
