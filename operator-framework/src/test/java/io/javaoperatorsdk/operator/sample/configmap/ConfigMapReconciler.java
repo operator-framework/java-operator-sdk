6853b843a5e0254b69ccc3900ba7d82a9c1f5c83
csviri <csviri@gmail.com>
2021-11-12 14:24:58 +0100

    fix: add config map IT

--- operator-framework/src/test/java/io/javaoperatorsdk/operator/sample/configmap/ConfigMapReconciler.java
+++ operator-framework/src/test/java/io/javaoperatorsdk/operator/sample/configmap/ConfigMapReconciler.java
@@ -4,37 +4,33 @@ import io.fabric8.kubernetes.api.model.ConfigMap;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.javaoperatorsdk.operator.api.reconciler.*;
 import io.javaoperatorsdk.operator.junit.KubernetesClientAware;
+import io.javaoperatorsdk.operator.support.TestExecutionInfoProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static io.javaoperatorsdk.operator.api.reconciler.ControllerConfiguration.NO_FINALIZER;
 
 @ControllerConfiguration(finalizerName = NO_FINALIZER)
 public class ConfigMapReconciler
-    implements Reconciler<ConfigMap>, KubernetesClientAware {
+    implements Reconciler<ConfigMap>, TestExecutionInfoProvider {
 
   private static final Logger log = LoggerFactory.getLogger(ConfigMapReconciler.class);
-
-  private KubernetesClient kubernetesClient;
-
-  @Override
-  public KubernetesClient getKubernetesClient() {
-    return kubernetesClient;
-  }
-
-  @Override
-  public void setKubernetesClient(KubernetesClient kubernetesClient) {
-    this.kubernetesClient = kubernetesClient;
-  }
+  private final AtomicInteger numberOfExecutions = new AtomicInteger(0);
 
   @Override
   public UpdateControl<ConfigMap> reconcile(
           ConfigMap resource, Context context) {
 
-    log.info("Reconcile config map: {}",resource);
-
+    log.info("Reconcile config map: {}",resource.getMetadata().getName());
+    numberOfExecutions.incrementAndGet();
     return UpdateControl.noUpdate();
   }
 
 
+  @Override
+  public int getNumberOfExecutions() {
+    return numberOfExecutions.get();
+  }
 }
