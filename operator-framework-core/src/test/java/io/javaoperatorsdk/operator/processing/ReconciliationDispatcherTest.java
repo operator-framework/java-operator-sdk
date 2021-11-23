7e0ac59dc9f4e2bc8e91a020393f09b5e155b067
csviri <csviri@gmail.com>
2021-11-23 13:59:12 +0100

    fix: format

--- operator-framework-core/src/test/java/io/javaoperatorsdk/operator/processing/ReconciliationDispatcherTest.java
+++ operator-framework-core/src/test/java/io/javaoperatorsdk/operator/processing/ReconciliationDispatcherTest.java
@@ -330,18 +330,18 @@ class ReconciliationDispatcherTest {
 
     Reconciler<ObservedGenCustomResource> reconciler = mock(Reconciler.class);
     ControllerConfiguration<ObservedGenCustomResource> config =
-            mock(ControllerConfiguration.class);
+        mock(ControllerConfiguration.class);
     CustomResourceFacade<ObservedGenCustomResource> facade = mock(CustomResourceFacade.class);
     when(config.isGenerationAware()).thenReturn(true);
     when(reconciler.reconcile(any(), any()))
-            .thenReturn(UpdateControl.noUpdate());
+        .thenReturn(UpdateControl.noUpdate());
     when(facade.updateStatus(observedGenResource)).thenReturn(observedGenResource);
     var dispatcher = init(observedGenResource, reconciler, config, facade);
 
     PostExecutionControl<ObservedGenCustomResource> control = dispatcher.handleExecution(
-            executionScopeWithCREvent(observedGenResource));
+        executionScopeWithCREvent(observedGenResource));
     assertThat(control.getUpdatedCustomResource().get().getStatus().getObservedGeneration())
-            .isEqualTo(1L);
+        .isEqualTo(1L);
   }
 
   @Test
