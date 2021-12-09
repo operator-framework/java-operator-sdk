b4ddaae6b07d2780c2af76d61ff43d2b15d38768
csviri <csviri@gmail.com>
2021-12-09 11:07:35 +0100

    refactor: mysql sample refactor

--- sample-operators/mysql-schema/src/main/java/io/javaoperatorsdk/operator/sample/MySQLSchemaReconciler.java
+++ sample-operators/mysql-schema/src/main/java/io/javaoperatorsdk/operator/sample/MySQLSchemaReconciler.java
@@ -2,10 +2,7 @@ package io.javaoperatorsdk.operator.sample;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Base64;
 import java.util.Optional;
 
@@ -13,19 +10,20 @@ import org.apache.commons.lang3.RandomStringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import io.fabric8.kubernetes.api.model.OwnerReference;
 import io.fabric8.kubernetes.api.model.Secret;
 import io.fabric8.kubernetes.api.model.SecretBuilder;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.javaoperatorsdk.operator.api.reconciler.*;
+import io.javaoperatorsdk.operator.sample.schema.SchemaService;
 
 import static java.lang.String.format;
 
 @ControllerConfiguration
 public class MySQLSchemaReconciler
     implements Reconciler<MySQLSchema>, ErrorStatusHandler<MySQLSchema> {
-  static final String USERNAME_FORMAT = "%s-user";
-  static final String SECRET_FORMAT = "%s-secret";
-
+  public static final String SECRET_FORMAT = "%s-secret";
+  public static final String USERNAME_FORMAT = "%s-user";
   private final Logger log = LoggerFactory.getLogger(getClass());
 
   private final KubernetesClient kubernetesClient;
@@ -40,98 +38,36 @@ public class MySQLSchemaReconciler
   public UpdateControl<MySQLSchema> reconcile(MySQLSchema schema,
       Context context) {
     try (Connection connection = getConnection()) {
-      if (!schemaExists(connection, schema.getMetadata().getName())) {
-        try (Statement statement = connection.createStatement()) {
-          statement.execute(
-              format(
-                  "CREATE SCHEMA `%1$s` DEFAULT CHARACTER SET %2$s",
-                  schema.getMetadata().getName(), schema.getSpec().getEncoding()));
-        }
-
+      var dbSchema = SchemaService.getSchema(connection, schema.getMetadata().getName());
+      if (!dbSchema.isPresent()) {
+        var schemaName = schema.getMetadata().getName();
         String password = RandomStringUtils.randomAlphanumeric(16);
-        String userName = String.format(USERNAME_FORMAT, schema.getMetadata().getName());
-        String secretName = String.format(SECRET_FORMAT, schema.getMetadata().getName());
-        try (Statement statement = connection.createStatement()) {
-          statement.execute(format("CREATE USER '%1$s' IDENTIFIED BY '%2$s'", userName, password));
-        }
-        try (Statement statement = connection.createStatement()) {
-          statement.execute(
-              format("GRANT ALL ON `%1$s`.* TO '%2$s'", schema.getMetadata().getName(), userName));
-        }
-        Secret credentialsSecret =
-            new SecretBuilder()
-                .withNewMetadata()
-                .withName(secretName)
-                .endMetadata()
-                .addToData(
-                    "MYSQL_USERNAME", Base64.getEncoder().encodeToString(userName.getBytes()))
-                .addToData(
-                    "MYSQL_PASSWORD", Base64.getEncoder().encodeToString(password.getBytes()))
-                .build();
-        this.kubernetesClient
-            .secrets()
-            .inNamespace(schema.getMetadata().getNamespace())
-            .create(credentialsSecret);
+        String secretName = String.format(SECRET_FORMAT, schemaName);
+        String userName = String.format(USERNAME_FORMAT, schemaName);
 
-        SchemaStatus status = new SchemaStatus();
-        status.setUrl(
-            format(
-                "jdbc:mysql://%1$s/%2$s",
-                System.getenv("MYSQL_HOST"), schema.getMetadata().getName()));
-        status.setUserName(userName);
-        status.setSecretName(secretName);
-        status.setStatus("CREATED");
-        schema.setStatus(status);
+        SchemaService.createSchemaAndRelatedUser(connection, schemaName,
+            schema.getSpec().getEncoding(), userName, password);
+        createSecret(schema, password, secretName, userName);
+        updateStatusPojo(schema, secretName, userName);
         log.info("Schema {} created - updating CR status", schema.getMetadata().getName());
-
         return UpdateControl.updateStatus(schema);
       }
       return UpdateControl.noUpdate();
     } catch (SQLException e) {
       log.error("Error while creating Schema", e);
-      SchemaStatus status = new SchemaStatus();
-      status.setUrl(null);
-      status.setUserName(null);
-      status.setSecretName(null);
-      status.setStatus("ERROR: " + e.getMessage());
-      schema.setStatus(status);
-
-      return UpdateControl.updateStatus(schema);
+      throw new IllegalStateException(e);
     }
-  }
-
-  @Override
-  public Optional<MySQLSchema> updateErrorStatus(MySQLSchema resource, RetryInfo retryInfo,
-      RuntimeException e) {
-
-    return Optional.empty();
   }
 
   @Override
   public DeleteControl cleanup(MySQLSchema schema, Context context) {
     log.info("Execution deleteResource for: {}", schema.getMetadata().getName());
-
     try (Connection connection = getConnection()) {
-      if (schemaExists(connection, schema.getMetadata().getName())) {
-        try (Statement statement = connection.createStatement()) {
-          statement.execute(format("DROP DATABASE `%1$s`", schema.getMetadata().getName()));
-        }
-        log.info("Deleted Schema '{}'", schema.getMetadata().getName());
-
-        if (schema.getStatus() != null) {
-          if (userExists(connection, schema.getStatus().getUserName())) {
-            try (Statement statement = connection.createStatement()) {
-              statement.execute(format("DROP USER '%1$s'", schema.getStatus().getUserName()));
-            }
-            log.info("Deleted User '{}'", schema.getStatus().getUserName());
-          }
-        }
-
-        this.kubernetesClient
-            .secrets()
-            .inNamespace(schema.getMetadata().getNamespace())
-            .withName(schema.getStatus().getSecretName())
-            .delete();
+      var dbSchema = SchemaService.getSchema(connection, schema.getMetadata().getName());
+      if (dbSchema.isPresent()) {
+        var userName = schema.getStatus() != null ? schema.getStatus().getUserName() : null;
+        SchemaService.deleteSchemaAndRelatedUser(connection, schema.getMetadata().getName(),
+            userName);
       } else {
         log.info(
             "Delete event ignored for schema '{}', real schema doesn't exist",
@@ -144,6 +80,18 @@ public class MySQLSchemaReconciler
     }
   }
 
+  @Override
+  public Optional<MySQLSchema> updateErrorStatus(MySQLSchema schema, RetryInfo retryInfo,
+      RuntimeException e) {
+    SchemaStatus status = new SchemaStatus();
+    status.setUrl(null);
+    status.setUserName(null);
+    status.setSecretName(null);
+    status.setStatus("ERROR: " + e.getMessage());
+    schema.setStatus(status);
+    return Optional.empty();
+  }
+
   private Connection getConnection() throws SQLException {
     String connectionString =
         format("jdbc:mysql://%1$s:%2$s", mysqlDbConfig.getHost(), mysqlDbConfig.getPort());
@@ -153,26 +101,36 @@ public class MySQLSchemaReconciler
         mysqlDbConfig.getPassword());
   }
 
-  private boolean schemaExists(Connection connection, String schemaName) throws SQLException {
-    try (PreparedStatement ps =
-        connection.prepareStatement(
-            "SELECT * FROM information_schema.schemata WHERE schema_name = ?")) {
-      ps.setString(1, schemaName);
-      try (ResultSet resultSet = ps.executeQuery()) {
-        // CATALOG_NAME, SCHEMA_NAME, DEFAULT_CHARACTER_SET_NAME, DEFAULT_COLLATION_NAME, SQL_PATH
-        var exists = resultSet.next();
-        return exists;
-      }
-    }
+  private void updateStatusPojo(MySQLSchema schema, String secretName, String userName) {
+    SchemaStatus status = new SchemaStatus();
+    status.setUrl(
+        format(
+            "jdbc:mysql://%1$s/%2$s",
+            System.getenv("MYSQL_HOST"), schema.getMetadata().getName()));
+    status.setUserName(userName);
+    status.setSecretName(secretName);
+    status.setStatus("CREATED");
+    schema.setStatus(status);
   }
 
-  private boolean userExists(Connection connection, String userName) throws SQLException {
-    try (PreparedStatement ps =
-        connection.prepareStatement("SELECT User FROM mysql.user WHERE User = ?")) {
-      ps.setString(1, userName);
-      try (ResultSet resultSet = ps.executeQuery()) {
-        return resultSet.first();
-      }
-    }
+  private void createSecret(MySQLSchema schema, String password, String secretName,
+      String userName) {
+    Secret credentialsSecret =
+        new SecretBuilder()
+            .withNewMetadata()
+            .withName(secretName)
+            .withOwnerReferences(new OwnerReference("mysql.sample.javaoperatorsdk/v1",
+                false, false, "MySQLSchema",
+                schema.getMetadata().getName(), schema.getMetadata().getUid()))
+            .endMetadata()
+            .addToData(
+                "MYSQL_USERNAME", Base64.getEncoder().encodeToString(userName.getBytes()))
+            .addToData(
+                "MYSQL_PASSWORD", Base64.getEncoder().encodeToString(password.getBytes()))
+            .build();
+    this.kubernetesClient
+        .secrets()
+        .inNamespace(schema.getMetadata().getNamespace())
+        .create(credentialsSecret);
   }
 }
---
+++ sample-operators/mysql-schema/src/main/java/io/javaoperatorsdk/operator/sample/schema/SchemaService.java
@@ -0,0 +1,109 @@
+package io.javaoperatorsdk.operator.sample.schema;
+
+import java.sql.*;
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.javaoperatorsdk.operator.sample.MySQLDbConfig;
+
+import static java.lang.String.format;
+
+public class SchemaService {
+
+
+  private static final Logger log = LoggerFactory.getLogger(SchemaService.class);
+
+  private final MySQLDbConfig mySQLDbConfig;
+
+  public SchemaService(MySQLDbConfig mySQLDbConfig) {
+    this.mySQLDbConfig = mySQLDbConfig;
+  }
+
+  public Optional<Schema> getSchema(String name) {
+    try (Connection connection = getConnection()) {
+      return getSchema(connection, name);
+    } catch (SQLException e) {
+      throw new IllegalStateException(e);
+    }
+  }
+
+  public static void createSchemaAndRelatedUser(Connection connection, String schemaName,
+      String encoding,
+      String userName,
+      String password) {
+    try {
+      try (Statement statement = connection.createStatement()) {
+        statement.execute(
+            format(
+                "CREATE SCHEMA `%1$s` DEFAULT CHARACTER SET %2$s",
+                schemaName, encoding));
+      }
+
+      try (Statement statement = connection.createStatement()) {
+        statement.execute(format("CREATE USER '%1$s' IDENTIFIED BY '%2$s'", userName, password));
+      }
+
+      try (Statement statement = connection.createStatement()) {
+        statement.execute(
+            format("GRANT ALL ON `%1$s`.* TO '%2$s'", schemaName, userName));
+      }
+    } catch (SQLException e) {
+      throw new IllegalStateException(e);
+    }
+  }
+
+  public static void deleteSchemaAndRelatedUser(Connection connection, String schemaName,
+      String userName) {
+    try {
+      try (Statement statement = connection.createStatement()) {
+        statement.execute(format("DROP DATABASE `%1$s`", schemaName));
+      }
+      log.info("Deleted Schema '{}'", schemaName);
+      if (userName != null) {
+        try (Statement statement = connection.createStatement()) {
+          statement.execute(format("DROP USER '%1$s'", userName));
+        }
+        log.info("Deleted User '{}'", userName);
+      }
+
+    } catch (SQLException e) {
+      throw new IllegalStateException(e);
+    }
+  }
+
+  public static Optional<Schema> getSchema(Connection connection, String schemaName) {
+    try (PreparedStatement ps =
+        connection.prepareStatement(
+            "SELECT * FROM information_schema.schemata WHERE schema_name = ?")) {
+      ps.setString(1, schemaName);
+      try (ResultSet resultSet = ps.executeQuery()) {
+        // CATALOG_NAME, SCHEMA_NAME, DEFAULT_CHARACTER_SET_NAME, DEFAULT_COLLATION_NAME, SQL_PATH
+        var exists = resultSet.next();
+        if (!exists) {
+          return Optional.empty();
+        } else {
+          return Optional.of(new Schema(resultSet.getString("SCHEMA_NAME"),
+              resultSet.getString("DEFAULT_CHARACTER_SET_NAME")));
+        }
+      }
+    } catch (SQLException e) {
+      throw new IllegalStateException(e);
+    }
+  }
+
+  private Connection getConnection() {
+    try {
+      String connectionString =
+          format("jdbc:mysql://%1$s:%2$s", mySQLDbConfig.getHost(), mySQLDbConfig.getPort());
+
+      log.debug("Connecting to '{}' with user '{}'", connectionString, mySQLDbConfig.getUser());
+      return DriverManager.getConnection(connectionString, mySQLDbConfig.getUser(),
+          mySQLDbConfig.getPassword());
+    } catch (SQLException e) {
+      throw new IllegalStateException(e);
+    }
+  }
+
+}
