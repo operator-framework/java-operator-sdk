fd525907912e5a31a742928f7a9e8f6cadba79e1
Chris Laprun <metacosm@users.noreply.github.com>
2021-12-22 17:46:59 +0100

    chore(docs): fix javadoc and improve it (#774)

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/event/source/EventSource.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/event/source/EventSource.java
@@ -3,10 +3,30 @@ package io.javaoperatorsdk.operator.processing.event.source;
 import io.javaoperatorsdk.operator.processing.LifecycleAware;
 import io.javaoperatorsdk.operator.processing.event.EventHandler;
 
+/**
+ * Creates an event source to trigger your reconciler whenever something happens to a secondary or
+ * external resource that would not normally trigger your reconciler (as the primary resources are
+ * not changed). To register EventSources with so that your reconciler is triggered, please make
+ * your reconciler implement
+ * {@link io.javaoperatorsdk.operator.api.reconciler.EventSourceInitializer}.
+ */
 public interface EventSource extends LifecycleAware {
+
+  /**
+   * An optional name for your EventSource. This is only required if you need to register multiple
+   * EventSources for the same resource type (e.g. {@code Deployment}).
+   * 
+   * @return the name associated with this EventSource
+   */
   default String name() {
     return getClass().getCanonicalName();
   }
 
+  /**
+   * Sets the {@link EventHandler} that is linked to your reconciler when this EventSource is
+   * registered.
+   * 
+   * @param handler the {@link EventHandler} associated with your reconciler
+   */
   void setEventHandler(EventHandler handler);
 }
