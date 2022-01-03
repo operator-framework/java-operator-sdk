db359ad4b2f7dd6f81029e52a7c63d15460c3c6e
Chris Laprun <metacosm@users.noreply.github.com>
2022-01-03 16:59:10 +0100

    Minor fixes (#783)
    * fix: remove unneeded Kind annotation and add comment where it's needed
    
    * fix: toString methods were incorrect

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/event/Event.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/event/Event.java
@@ -16,7 +16,7 @@ public class Event {
 
   @Override
   public String toString() {
-    return "DefaultEvent{" +
+    return "Event{" +
         "relatedCustomResource=" + relatedCustomResource +
         '}';
   }
