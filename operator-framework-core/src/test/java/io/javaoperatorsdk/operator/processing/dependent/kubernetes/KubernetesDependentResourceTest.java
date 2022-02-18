38fbed8fbd7fbed45c54adbdf47459459ff0f8f1
csviri <csviri@gmail.com>
2022-02-18 10:08:40 +0100

    fix: unit test dependent resource

--- operator-framework-core/src/test/java/io/javaoperatorsdk/operator/processing/dependent/kubernetes/KubernetesDependentResourceTest.java
+++ operator-framework-core/src/test/java/io/javaoperatorsdk/operator/processing/dependent/kubernetes/KubernetesDependentResourceTest.java
@@ -3,20 +3,27 @@ package io.javaoperatorsdk.operator.processing.dependent.kubernetes;
 import io.fabric8.kubernetes.api.model.ConfigMap;
 import io.fabric8.kubernetes.api.model.HasMetadata;
 import io.fabric8.kubernetes.api.model.ObjectMeta;
+import io.javaoperatorsdk.operator.api.config.informer.InformerConfiguration;
 import io.javaoperatorsdk.operator.api.reconciler.Context;
+import io.javaoperatorsdk.operator.processing.event.ResourceID;
+import io.javaoperatorsdk.operator.processing.event.source.AssociatedSecondaryResourceIdentifier;
 import io.javaoperatorsdk.operator.processing.event.source.informer.InformerEventSource;
 import io.javaoperatorsdk.operator.sample.simple.TestCustomResource;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 class KubernetesDependentResourceTest {
 
     private TemporalResourceCache temporalResourceCacheMock = mock(TemporalResourceCache.class);
     private InformerEventSource informerEventSourceMock = mock(InformerEventSource.class);
+    private AssociatedSecondaryResourceIdentifier associatedResourceIdentifierMock
+            = mock(AssociatedSecondaryResourceIdentifier.class);
 
     KubernetesDependentResource<ConfigMap, TestCustomResource> kubernetesDependentResource = new KubernetesDependentResource() {
         {
@@ -31,17 +38,34 @@ class KubernetesDependentResourceTest {
 
     @BeforeEach
     public void setup() {
-
+        InformerConfiguration informerConfigurationMock = mock(InformerConfiguration.class);
+        when(informerEventSourceMock.getConfiguration()).thenReturn(informerConfigurationMock);
+        when(informerConfigurationMock.getAssociatedResourceIdentifier()).thenReturn(associatedResourceIdentifierMock);
+        when(associatedResourceIdentifierMock.associatedSecondaryID(any()))
+                .thenReturn(ResourceID.fromResource(testResource()));
     }
 
     @Test
     void getResourceCheckTheTemporalCacheFirst() {
+        when(temporalResourceCacheMock.getResourceFromCache(any())).thenReturn(Optional.of(testResource()));
 
+        kubernetesDependentResource.getResource(primaryResource());
+
+        verify(temporalResourceCacheMock,times(1)).getResourceFromCache(any());
+        verify(informerEventSourceMock,never()).get(any());
     }
 
     @Test
     void getResourceGetsResourceFromInformerIfNotInTemporalCache() {
+        var resource = testResource();
+        when(temporalResourceCacheMock.getResourceFromCache(any())).thenReturn(Optional.empty());
+        when(informerEventSourceMock.get(any())).thenReturn(Optional.of(resource));
 
+        var res =kubernetesDependentResource.getResource(primaryResource());
+
+        verify(temporalResourceCacheMock,times(1)).getResourceFromCache(any());
+        verify(informerEventSourceMock,times(1)).get(any());
+        assertThat(res.orElseThrow()).isEqualTo(resource);
     }
 
     TestCustomResource primaryResource() {
