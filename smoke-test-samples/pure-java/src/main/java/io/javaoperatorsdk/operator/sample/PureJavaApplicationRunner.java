a8d4c4e9ed3b13c2f7424793b392c25a4e4df5f2
csviri <csviri@gmail.com>
2022-03-01 10:25:02 +0100

    feat: executor service override

--- smoke-test-samples/pure-java/src/main/java/io/javaoperatorsdk/operator/sample/PureJavaApplicationRunner.java
+++ smoke-test-samples/pure-java/src/main/java/io/javaoperatorsdk/operator/sample/PureJavaApplicationRunner.java
@@ -4,13 +4,18 @@ import io.javaoperatorsdk.operator.Operator;
 import io.javaoperatorsdk.operator.api.config.ConfigurationServiceOverrider;
 import io.javaoperatorsdk.operator.config.runtime.DefaultConfigurationService;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class PureJavaApplicationRunner {
 
   public static void main(String[] args) {
     Operator operator =
-        new Operator(ConfigurationServiceOverrider.override(DefaultConfigurationService.instance())
-            .withConcurrentReconciliationThreads(2)
-            .build());
+        new Operator(
+            ConfigurationServiceOverrider.override(DefaultConfigurationService.instance())
+                .withExecutorService(Executors.newCachedThreadPool())
+                .withConcurrentReconciliationThreads(2)
+                .build());
     operator.register(new CustomServiceReconciler());
     operator.start();
   }
