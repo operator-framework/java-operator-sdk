193dfa71911f447958a778a7e7c311bccf7cb87b
csviri <csviri@gmail.com>
2022-03-07 20:04:46 +0100

    test

--- operator-framework/src/test/java/io/javaoperatorsdk/operator/sample/conditionchecker/ConditionCheckerDeployment.java
+++ operator-framework/src/test/java/io/javaoperatorsdk/operator/sample/conditionchecker/ConditionCheckerDeployment.java
@@ -3,12 +3,17 @@ package io.javaoperatorsdk.operator.sample.conditionchecker;
 import io.fabric8.kubernetes.api.model.apps.Deployment;
 import io.javaoperatorsdk.operator.ReconcilerUtils;
 import io.javaoperatorsdk.operator.api.reconciler.Context;
+import io.javaoperatorsdk.operator.api.reconciler.EventSourceContext;
 import io.javaoperatorsdk.operator.processing.dependent.kubernetes.CrudKubernetesDependentResource;
+import io.javaoperatorsdk.operator.processing.event.source.EventSource;
 
 public class ConditionCheckerDeployment
     extends CrudKubernetesDependentResource<Deployment, ConditionCheckerTestCustomResource> {
 
-
+  @Override
+  public EventSource eventSource(EventSourceContext<ConditionCheckerTestCustomResource> context) {
+    return super.eventSource(context);
+  }
 
   @Override
   protected Deployment desired(ConditionCheckerTestCustomResource primary, Context context) {
