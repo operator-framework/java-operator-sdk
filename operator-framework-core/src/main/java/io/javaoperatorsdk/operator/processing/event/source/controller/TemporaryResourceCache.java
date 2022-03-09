d42dc63d61ede0123fac8aa4b940f6f28b0084c4
csviri <csviri@gmail.com>
2022-03-09 11:13:55 +0100

    wip

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/event/source/controller/TemporaryResourceCache.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/event/source/controller/TemporaryResourceCache.java
@@ -1,9 +1,8 @@
 package io.javaoperatorsdk.operator.processing.event.source.controller;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.locks.ReentrantLock;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,53 +14,37 @@ public class TemporaryResourceCache<T extends HasMetadata> {
 
   private static final Logger log = LoggerFactory.getLogger(TemporaryResourceCache.class);
 
-  private final Map<ResourceID, T> cache = new ConcurrentHashMap<>();
-  private final ReentrantLock lock = new ReentrantLock();
+  private final Map<ResourceID, T> cache = new HashMap<>();
   private final ControllerResourceCache<T> managedInformerEventSource;
 
   public TemporaryResourceCache(ControllerResourceCache<T> managedInformerEventSource) {
     this.managedInformerEventSource = managedInformerEventSource;
   }
 
-  public void removeResourceFromCache(T resource) {
-    lock.lock();
-    try {
-      cache.remove(ResourceID.fromResource(resource));
-    } finally {
-      lock.unlock();
+  public synchronized void removeResourceFromCache(T resource) {
+    cache.remove(ResourceID.fromResource(resource));
+  }
+
+  public synchronized void putUpdatedResource(T newResource, String previousResourceVersion) {
+    var resourceId = ResourceID.fromResource(newResource);
+    var informerCacheResource = managedInformerEventSource.get(resourceId);
+    if (informerCacheResource.isEmpty()) {
+      log.debug("No cached value present for resource: {}", newResource);
+      return;
+    }
+    // if this is not true that means the cache was already updated
+    if (informerCacheResource.get().getMetadata().getResourceVersion()
+        .equals(previousResourceVersion)) {
+      log.debug("Putting resource to temporal cache with id: {}", resourceId);
+      cache.put(resourceId, newResource);
+    } else {
+      // if something is in cache it's surely obsolete now
+      cache.remove(resourceId);
     }
   }
 
-  public void putUpdatedResource(T newResource, String previousResourceVersion) {
-    lock.lock();
-    try {
-      var resourceId = ResourceID.fromResource(newResource);
-      var informerCacheResource = managedInformerEventSource.get(resourceId);
-      if (informerCacheResource.isEmpty()) {
-        log.debug("No cached value present for resource: {}", newResource);
-        return;
-      }
-      // if this is not true that means the cache was already updated
-      if (informerCacheResource.get().getMetadata().getResourceVersion()
-          .equals(previousResourceVersion)) {
-        log.debug("Putting resource to temporal cache with id: {}", resourceId);
-        cache.put(resourceId, newResource);
-      } else {
-        // if something is in cache it's surely obsolete now
-        cache.remove(resourceId);
-      }
-    } finally {
-      lock.unlock();
-    }
-  }
-
-  public Optional<T> getResourceFromCache(ResourceID resourceID) {
-    try {
-      lock.lock();
-      return Optional.ofNullable(cache.get(resourceID));
-    } finally {
-      lock.unlock();
-    }
+  public synchronized Optional<T> getResourceFromCache(ResourceID resourceID) {
+    return Optional.ofNullable(cache.get(resourceID));
   }
 }
 
