d81db18bdce549bdfad5760e1ce0c2c455c4187d
Chris Laprun <metacosm@users.noreply.github.com>
2022-03-10 10:12:10 +0100

    fix: debugThreadPool & shouldCheckCRDAndValidateLocalModel should work (#1015)
    Fixes #1014

--- operator-framework-core/src/test/java/io/javaoperatorsdk/operator/api/config/UtilsTest.java
+++ operator-framework-core/src/test/java/io/javaoperatorsdk/operator/api/config/UtilsTest.java
@@ -13,6 +13,62 @@ import static org.junit.jupiter.api.Assertions.*;
 class UtilsTest {
 
   @Test
+  void shouldCheckCRDAndValidateLocalModelByDefault() {
+    assertTrue(Utils.shouldCheckCRDAndValidateLocalModel());
+  }
+
+  @Test
+  void shouldNotDebugThreadPoolByDefault() {
+    assertFalse(Utils.debugThreadPool());
+  }
+
+  @Test
+  void askingForNonexistentPropertyShouldReturnDefault() {
+    final var key = "foo";
+    assertNull(System.getProperty(key));
+    assertFalse(Utils.getBooleanFromSystemPropsOrDefault(key, false));
+    assertTrue(Utils.getBooleanFromSystemPropsOrDefault(key, true));
+  }
+
+  @Test
+  void askingForExistingPropertyShouldReturnItIfBoolean() {
+    final var key = "foo";
+    try {
+      System.setProperty(key, "true");
+      assertNotNull(System.getProperty(key));
+      assertTrue(Utils.getBooleanFromSystemPropsOrDefault(key, false));
+      assertTrue(Utils.getBooleanFromSystemPropsOrDefault(key, true));
+
+      System.setProperty(key, "TruE");
+      assertTrue(Utils.getBooleanFromSystemPropsOrDefault(key, false));
+      assertTrue(Utils.getBooleanFromSystemPropsOrDefault(key, true));
+
+      System.setProperty(key, " \tTRUE  ");
+      assertTrue(Utils.getBooleanFromSystemPropsOrDefault(key, false));
+      assertTrue(Utils.getBooleanFromSystemPropsOrDefault(key, true));
+
+      System.setProperty(key, " \nFalSe \t ");
+      assertFalse(Utils.getBooleanFromSystemPropsOrDefault(key, false));
+      assertFalse(Utils.getBooleanFromSystemPropsOrDefault(key, true));
+    } finally {
+      System.clearProperty(key);
+    }
+  }
+
+  @Test
+  void askingForExistingNonBooleanPropertyShouldReturnDefaultValue() {
+    final var key = "foo";
+    try {
+      System.setProperty(key, "bar");
+      assertNotNull(System.getProperty(key));
+      assertFalse(Utils.getBooleanFromSystemPropsOrDefault(key, false));
+      assertTrue(Utils.getBooleanFromSystemPropsOrDefault(key, true));
+    } finally {
+      System.clearProperty(key);
+    }
+  }
+
+  @Test
   void getsFirstTypeArgumentFromExtendedClass() {
     Class<?> res =
         Utils.getFirstTypeArgumentFromExtendedClass(TestKubernetesDependentResource.class);
