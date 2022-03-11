aadba2b05afa8cd2cf4fab98919b89bcf3d1d0f0
Chris Laprun <metacosm@gmail.com>
2022-03-11 14:44:40 +0100

    refactor: remove configurationService method

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/Controller.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/Controller.java
@@ -16,7 +16,6 @@ import io.fabric8.kubernetes.client.dsl.Resource;
 import io.javaoperatorsdk.operator.CustomResourceUtils;
 import io.javaoperatorsdk.operator.MissingCRDException;
 import io.javaoperatorsdk.operator.OperatorException;
-import io.javaoperatorsdk.operator.api.config.ConfigurationService;
 import io.javaoperatorsdk.operator.api.config.ConfigurationServiceProvider;
 import io.javaoperatorsdk.operator.api.config.ControllerConfiguration;
 import io.javaoperatorsdk.operator.api.monitoring.Metrics;
@@ -122,7 +121,7 @@ public class Controller<P extends HasMetadata> implements Reconciler<P>,
 
 
   private Metrics metrics() {
-    final var metrics = configurationService().getMetrics();
+    final var metrics = ConfigurationServiceProvider.instance().getMetrics();
     return metrics != null ? metrics : Metrics.NOOP;
   }
 
@@ -199,7 +198,7 @@ public class Controller<P extends HasMetadata> implements Reconciler<P>,
     try {
       // check that the custom resource is known by the cluster if configured that way
       final CustomResourceDefinition crd; // todo: check proper CRD spec version based on config
-      if (configurationService().checkCRDAndValidateLocalModel()
+      if (ConfigurationServiceProvider.instance().checkCRDAndValidateLocalModel()
           && CustomResource.class.isAssignableFrom(resClass)) {
         crd = kubernetesClient.apiextensions().v1().customResourceDefinitions().withName(crdName)
             .get();
@@ -222,10 +221,6 @@ public class Controller<P extends HasMetadata> implements Reconciler<P>,
     } catch (MissingCRDException e) {
       throwMissingCRDException(crdName, specVersion, controllerName);
     }
-  }
-
-  private ConfigurationService configurationService() {
-    return ConfigurationServiceProvider.instance();
   }
 
   private void throwMissingCRDException(String crdName, String specVersion, String controllerName) {
