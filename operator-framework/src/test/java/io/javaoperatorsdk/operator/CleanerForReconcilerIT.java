49ef6c7aa7142e9082f4d2b441566fe80b1f9e98
csviri <csviri@gmail.com>
2022-03-17 11:22:21 +0100

    fix: reconciler cleaner IT

---
+++ operator-framework/src/test/java/io/javaoperatorsdk/operator/CleanerForReconcilerIT.java
@@ -0,0 +1,49 @@
+package io.javaoperatorsdk.operator;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.fabric8.kubernetes.api.model.ObjectMeta;
+import io.javaoperatorsdk.operator.junit.OperatorExtension;
+import io.javaoperatorsdk.operator.sample.cleanerforreconciler.CleanerForReconcilerCustomResource;
+import io.javaoperatorsdk.operator.sample.cleanerforreconciler.CleanerForReconcilerTestReconciler;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
+
+class CleanerForReconcilerIT {
+
+  public static final String TEST_RESOURCE_NAME = "cleaner-for-reconciler-test1";
+
+  @RegisterExtension
+  OperatorExtension operator =
+      OperatorExtension.builder().withReconciler(new CleanerForReconcilerTestReconciler()).build();
+
+
+  @Test
+  void addsFinalizerAndCallsCleanupIfCleanerImplemented() {
+    var testResource = createTestResource();
+    operator.create(CleanerForReconcilerCustomResource.class, testResource);
+
+    await().until(() -> !operator.get(CleanerForReconcilerCustomResource.class, TEST_RESOURCE_NAME)
+        .getMetadata().getFinalizers().isEmpty());
+
+    operator.delete(CleanerForReconcilerCustomResource.class, testResource);
+
+    await().until(
+        () -> operator.get(CleanerForReconcilerCustomResource.class, TEST_RESOURCE_NAME) == null);
+
+    CleanerForReconcilerTestReconciler reconciler =
+        (CleanerForReconcilerTestReconciler) operator.getFirstReconciler();
+    assertThat(reconciler.getNumberOfExecutions()).isEqualTo(1);
+    assertThat(reconciler.getNumberOfCleanupExecutions()).isEqualTo(1);
+  }
+
+  private CleanerForReconcilerCustomResource createTestResource() {
+    CleanerForReconcilerCustomResource cr = new CleanerForReconcilerCustomResource();
+    cr.setMetadata(new ObjectMeta());
+    cr.getMetadata().setName(TEST_RESOURCE_NAME);
+    return cr;
+  }
+
+}
