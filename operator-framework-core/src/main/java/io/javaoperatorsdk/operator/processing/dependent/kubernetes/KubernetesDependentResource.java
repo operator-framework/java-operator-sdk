41bcbf1cf8a0073e9c567feca46a43fc15526f11
csviri <csviri@gmail.com>
2022-03-24 15:55:32 +0100

    feat: by default standalone dependent resource inherits namespace

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/dependent/kubernetes/KubernetesDependentResource.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/dependent/kubernetes/KubernetesDependentResource.java
@@ -128,7 +128,8 @@ public abstract class KubernetesDependentResource<R extends HasMetadata, P exten
   @Override
   public EventSource initEventSource(EventSourceContext<P> context) {
     if (informerEventSource == null) {
-      configureWith(null, null, KubernetesDependent.ADD_OWNER_REFERENCE_DEFAULT);
+      configureWith(null, context.getControllerConfiguration().getNamespaces(),
+          KubernetesDependent.ADD_OWNER_REFERENCE_DEFAULT);
       log.warn("Using default configuration for " + resourceType().getSimpleName()
           + " KubernetesDependentResource, call configureWith to provide configuration");
     }
