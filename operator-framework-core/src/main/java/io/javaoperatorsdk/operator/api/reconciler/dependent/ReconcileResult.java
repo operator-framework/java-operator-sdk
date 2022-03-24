d86a838b3242ef5f95bbfb21470161497cd3ddb1
Chris Laprun <metacosm@gmail.com>
2022-03-24 13:53:21 +0100

    feat: ReconcileResult can now report errors as well

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/api/reconciler/dependent/ReconcileResult.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/api/reconciler/dependent/ReconcileResult.java
@@ -2,26 +2,44 @@ package io.javaoperatorsdk.operator.api.reconciler.dependent;
 
 import java.util.Optional;
 
+import io.fabric8.kubernetes.api.model.HasMetadata;
+import io.javaoperatorsdk.operator.processing.event.ResourceID;
+
 public class ReconcileResult<R> {
 
   private final R resource;
   private final Operation operation;
+  private final Exception error;
 
   public static <T> ReconcileResult<T> resourceCreated(T resource) {
-    return new ReconcileResult<>(resource, Operation.CREATED);
+    return new ReconcileResult<>(resource, Operation.CREATED, null);
   }
 
   public static <T> ReconcileResult<T> resourceUpdated(T resource) {
-    return new ReconcileResult<>(resource, Operation.UPDATED);
+    return new ReconcileResult<>(resource, Operation.UPDATED, null);
   }
 
   public static <T> ReconcileResult<T> noOperation(T resource) {
-    return new ReconcileResult<>(resource, Operation.NONE);
+    return new ReconcileResult<>(resource, Operation.NONE, null);
   }
 
-  private ReconcileResult(R resource, Operation operation) {
+  public static <T> ReconcileResult<T> error(T resource, Exception error) {
+    return new ReconcileResult<>(resource, Operation.ERROR, error);
+  }
+
+  @Override
+  public String toString() {
+    return getResource()
+        .map(r -> r instanceof HasMetadata ? ResourceID.fromResource((HasMetadata) r) : r)
+        .orElse("no resource")
+        + " -> " + operation
+        + getError().map(e -> " (" + e.getMessage() + ")").orElse("");
+  }
+
+  private ReconcileResult(R resource, Operation operation, Exception error) {
     this.resource = resource;
     this.operation = operation;
+    this.error = error;
   }
 
   public Optional<R> getResource() {
@@ -32,7 +50,11 @@ public class ReconcileResult<R> {
     return operation;
   }
 
+  public Optional<Exception> getError() {
+    return Optional.ofNullable(error);
+  }
+
   public enum Operation {
-    CREATED, UPDATED, NONE
+    CREATED, UPDATED, NONE, ERROR
   }
 }
