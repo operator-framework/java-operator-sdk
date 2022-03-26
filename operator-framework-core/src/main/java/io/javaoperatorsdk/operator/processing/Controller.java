4f45652474dbdb90b0eb6e005592188f87c8fea0
Chris Laprun <metacosm@gmail.com>
2022-03-27 00:25:37 +0100

    refactor: improve error reporting, remove redundant logging

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/Controller.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/Controller.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
