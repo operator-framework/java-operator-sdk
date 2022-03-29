09309ada2c38a039bef6727eea887ab9b2c5b504
csviri <csviri@gmail.com>
2022-03-29 15:36:49 +0200

    fix: event source holder not an event source

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/dependent/AbstractEventSourceHolderDependentResource.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/dependent/AbstractEventSourceHolderDependentResource.java
@@ -1,33 +1,21 @@
 package io.javaoperatorsdk.operator.processing.dependent;
 
 import io.fabric8.kubernetes.api.model.HasMetadata;
-import io.javaoperatorsdk.operator.OperatorException;
 import io.javaoperatorsdk.operator.api.reconciler.Context;
 import io.javaoperatorsdk.operator.api.reconciler.EventSourceContext;
 import io.javaoperatorsdk.operator.api.reconciler.dependent.EventSourceProvider;
 import io.javaoperatorsdk.operator.api.reconciler.dependent.RecentOperationCacheFiller;
 import io.javaoperatorsdk.operator.api.reconciler.dependent.RecentOperationEventFilter;
-import io.javaoperatorsdk.operator.processing.event.EventHandler;
 import io.javaoperatorsdk.operator.processing.event.ResourceID;
 import io.javaoperatorsdk.operator.processing.event.source.EventSource;
 import io.javaoperatorsdk.operator.processing.event.source.ResourceEventSource;
 
 public abstract class AbstractEventSourceHolderDependentResource<R, P extends HasMetadata, T extends ResourceEventSource<R, P>>
     extends AbstractDependentResource<R, P>
-    implements ResourceEventSource<R, P>, EventSourceProvider<P> {
+    implements EventSourceProvider<P> {
   private T eventSource;
   private boolean isFilteringEventSource;
   private boolean isCacheFillerEventSource;
-
-  @Override
-  public void start() throws OperatorException {
-    eventSource.start();
-  }
-
-  @Override
-  public void stop() throws OperatorException {
-    eventSource.stop();
-  }
 
   public EventSource initEventSource(EventSourceContext<P> context) {
     // some sub-classes (e.g. KubernetesDependentResource) can have their event source created
@@ -41,18 +29,13 @@ public abstract class AbstractEventSourceHolderDependentResource<R, P extends Ha
     // already been set before this method is called
     isFilteringEventSource = eventSource instanceof RecentOperationEventFilter;
     isCacheFillerEventSource = eventSource instanceof RecentOperationCacheFiller;
-    return this;
+    return eventSource;
   }
 
   protected abstract T createEventSource(EventSourceContext<P> context);
 
   protected void setEventSource(T eventSource) {
     this.eventSource = eventSource;
-  }
-
-  @Override
-  public void setEventHandler(EventHandler handler) {
-    eventSource.setEventHandler(handler);
   }
 
   protected T eventSource() {
