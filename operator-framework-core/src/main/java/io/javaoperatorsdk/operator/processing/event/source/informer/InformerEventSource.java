a54dedfa5446dcd4e65a892b59c3d61a0a79fa0f
csviri <csviri@gmail.com>
2022-04-04 13:33:52 +0200

    fix: additional log message

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/event/source/informer/InformerEventSource.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/event/source/informer/InformerEventSource.java
@@ -85,11 +85,17 @@ public class InformerEventSource<R extends HasMetadata, P extends HasMetadata>
 
   @Override
   public void onAdd(R resource) {
+    if (log.isDebugEnabled()) {
+      log.debug("On add event received for resource id: {}", ResourceID.fromResource(resource));
+    }
     onAddOrUpdate("add", resource, () -> InformerEventSource.super.onAdd(resource));
   }
 
   @Override
   public void onUpdate(R oldObject, R newObject) {
+    if (log.isDebugEnabled()) {
+      log.debug("On update event received for resource id: {}", ResourceID.fromResource(newObject));
+    }
     onAddOrUpdate("update", newObject,
         () -> InformerEventSource.super.onUpdate(oldObject, newObject));
   }
@@ -118,12 +124,12 @@ public class InformerEventSource<R extends HasMetadata, P extends HasMetadata>
   }
 
   @Override
-  public void onDelete(R r, boolean b) {
+  public void onDelete(R resource, boolean b) {
     if (log.isDebugEnabled()) {
-      log.debug("On delete event received for resource id: {}", ResourceID.fromResource(r));
+      log.debug("On delete event received for resource id: {}", ResourceID.fromResource(resource));
     }
-    super.onDelete(r, b);
-    propagateEvent(r);
+    super.onDelete(resource, b);
+    propagateEvent(resource);
   }
 
   private void propagateEvent(R object) {
