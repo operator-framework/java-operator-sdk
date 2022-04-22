3b36028541c69c2c79946550ac920e421825c72e
csviri <csviri@gmail.com>
2022-04-22 11:35:04 +0200

    fixes from rebase

--- sample-operators/webpage/src/main/java/io/javaoperatorsdk/operator/sample/IngressDependentResource.java
+++ sample-operators/webpage/src/main/java/io/javaoperatorsdk/operator/sample/IngressDependentResource.java
@@ -2,14 +2,14 @@ package io.javaoperatorsdk.operator.sample;
 
 import io.fabric8.kubernetes.api.model.networking.v1.Ingress;
 import io.javaoperatorsdk.operator.api.reconciler.Context;
-import io.javaoperatorsdk.operator.processing.dependent.kubernetes.CRUKubernetesDependentResource;
+import io.javaoperatorsdk.operator.processing.dependent.kubernetes.CRUDKubernetesDependentResource;
 import io.javaoperatorsdk.operator.processing.dependent.kubernetes.KubernetesDependent;
 
-import static io.javaoperatorsdk.operator.sample.Utils.*;
+import static io.javaoperatorsdk.operator.sample.Utils.makeDesiredIngress;
 
 // this annotation only activates when using managed dependents and is not otherwise needed
 @KubernetesDependent(labelSelector = WebPageManagedDependentsReconciler.SELECTOR)
-public class IngressDependentResource extends CRUKubernetesDependentResource<Ingress, WebPage> {
+public class IngressDependentResource extends CRUDKubernetesDependentResource<Ingress, WebPage> {
 
   public IngressDependentResource() {
     super(Ingress.class);
