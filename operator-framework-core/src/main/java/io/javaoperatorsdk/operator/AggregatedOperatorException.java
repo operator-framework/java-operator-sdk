28d8381d87ffd8604c61cbcedc8ab8b13e925947
Chris Laprun <metacosm@gmail.com>
2022-05-02 17:27:11 +0200

    fix: avoid NPE when creating AggregatedOperatorException

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/AggregatedOperatorException.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/AggregatedOperatorException.java
@@ -1,6 +1,7 @@
 package io.javaoperatorsdk.operator;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
@@ -9,13 +10,13 @@ public class AggregatedOperatorException extends OperatorException {
   private final List<Exception> causes;
 
   public AggregatedOperatorException(String message, Exception... exceptions) {
-    super(message, Objects.requireNonNull(exceptions)[0]);
-    this.causes = Arrays.asList(exceptions);
+    super(message, exceptions != null && exceptions.length > 0 ? exceptions[0] : null);
+    this.causes = exceptions != null ? Arrays.asList(exceptions) : Collections.emptyList();
   }
 
   public AggregatedOperatorException(String message, List<Exception> exceptions) {
-    super(message, Objects.requireNonNull(exceptions).get(0));
-    this.causes = exceptions;
+    super(message, exceptions != null && !exceptions.isEmpty() ? exceptions.get(0) : null);
+    this.causes = exceptions != null ? exceptions : Collections.emptyList();
   }
 
   public List<Exception> getAggregatedExceptions() {
