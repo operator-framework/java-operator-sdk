01153b1ebf11b544ee7a90bd94239630eb3e4c0d
csviri <csviri@gmail.com>
2022-05-02 10:09:11 +0200

    null check

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/AggregatedOperatorException.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/AggregatedOperatorException.java
@@ -2,18 +2,19 @@ package io.javaoperatorsdk.operator;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 public class AggregatedOperatorException extends OperatorException {
 
   private final List<Exception> causes;
 
   public AggregatedOperatorException(String message, Exception... exceptions) {
-    super(message, exceptions[0]);
+    super(message, Objects.requireNonNull(exceptions)[0]);
     this.causes = Arrays.asList(exceptions);
   }
 
   public AggregatedOperatorException(String message, List<Exception> exceptions) {
-    super(message, exceptions.get(0));
+    super(message, Objects.requireNonNull(exceptions).get(0));
     this.causes = exceptions;
   }
 
