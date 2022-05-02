facfadf17340cb2bfc06a2c90f284fd952969f1a
csviri <csviri@gmail.com>
2022-05-02 10:36:25 +0200

    formatting

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/api/RegisteredController.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/api/RegisteredController.java
@@ -12,7 +12,6 @@ public class RegisteredController {
     this.controller = controller;
   }
 
-
   public void changeNamespaces(Set<String> namespaces) {
     controller.changeNamespaces(namespaces);
   }
