642850248f3101ada5ed723b994059a57210c8aa
Attila Mészáros <csviri@gmail.com>
2022-05-03 08:40:26 +0200

    aggregate exception improvement (#1188)

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/AggregatedOperatorException.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/AggregatedOperatorException.java
@@ -5,19 +5,27 @@ import java.util.Collections;
 import java.util.List;
 
 public class AggregatedOperatorException extends OperatorException {
+
   private final List<Exception> causes;
 
   public AggregatedOperatorException(String message, Exception... exceptions) {
-    super(message);
+    super(message, exceptions != null && exceptions.length > 0 ? exceptions[0] : null);
     this.causes = exceptions != null ? Arrays.asList(exceptions) : Collections.emptyList();
   }
 
   public AggregatedOperatorException(String message, List<Exception> exceptions) {
-    super(message);
+    super(message, exceptions != null && !exceptions.isEmpty() ? exceptions.get(0) : null);
     this.causes = exceptions != null ? exceptions : Collections.emptyList();
   }
 
   public List<Exception> getAggregatedExceptions() {
     return causes;
+  }
+
+  @Override
+  public String toString() {
+    return "AggregatedOperatorException{" +
+        "causes=" + causes +
+        '}';
   }
 }
