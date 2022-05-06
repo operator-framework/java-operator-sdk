5e944f18a04b6d56d788dcfc9a63f13238ad3492
Chris Laprun <metacosm@gmail.com>
2022-05-06 15:19:28 +0200

    feat: make RegisteredController a little more useful
    Allows to retrieve information from registered controllers from
    Operator, in particular if there are any registered controllers, which
    the Quarkus extension uses to determine whether or not to start the
    Operator.

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/RegisteredController.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/RegisteredController.java
@@ -1,6 +1,9 @@
 package io.javaoperatorsdk.operator;
 
+import io.fabric8.kubernetes.api.model.HasMetadata;
+import io.javaoperatorsdk.operator.api.config.ControllerConfiguration;
 import io.javaoperatorsdk.operator.api.config.NamespaceChangeable;
 
-public interface RegisteredController extends NamespaceChangeable {
+public interface RegisteredController<P extends HasMetadata> extends NamespaceChangeable {
+  ControllerConfiguration<P> getConfiguration();
 }
