e8ea4c9c8b20813f2fad0cea43b09bb5c1332ad5
Attila Mészáros <csviri@gmail.com>
2022-05-11 13:35:07 +0200

    Using Annotations to Identify primary for a secondary object if no owner reference can be added (#1197)

---
+++ operator-framework/src/test/java/io/javaoperatorsdk/operator/DependentAnnotationSecondaryMapperIT.java
@@ -0,0 +1,66 @@
+package io.javaoperatorsdk.operator;
+
+import java.time.Duration;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.fabric8.kubernetes.api.model.ConfigMap;
+import io.fabric8.kubernetes.api.model.ObjectMetaBuilder;
+import io.javaoperatorsdk.operator.junit.LocalOperatorExtension;
+import io.javaoperatorsdk.operator.sample.dependentannotationsecondarymapper.DependentAnnotationSecondaryMapperReconciler;
+import io.javaoperatorsdk.operator.sample.dependentannotationsecondarymapper.DependentAnnotationSecondaryMapperResource;
+
+import static io.javaoperatorsdk.operator.processing.event.source.informer.Mappers.DEFAULT_ANNOTATION_FOR_NAME;
+import static io.javaoperatorsdk.operator.processing.event.source.informer.Mappers.DEFAULT_ANNOTATION_FOR_NAMESPACE;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
+
+class DependentAnnotationSecondaryMapperIT {
+
+  public static final String TEST_RESOURCE_NAME = "test1";
+
+  @RegisterExtension
+  LocalOperatorExtension operator =
+      LocalOperatorExtension.builder()
+          .withReconciler(DependentAnnotationSecondaryMapperReconciler.class)
+          .build();
+
+  @Test
+  void mapsSecondaryByAnnotation() {
+    operator.create(DependentAnnotationSecondaryMapperResource.class, testResource());
+
+    var reconciler =
+        operator.getReconcilerOfType(DependentAnnotationSecondaryMapperReconciler.class);
+
+    await().pollDelay(Duration.ofMillis(150)).untilAsserted(() -> {
+      assertThat(reconciler.getNumberOfExecutions()).isEqualTo(1);
+    });
+    var configMap = operator.get(ConfigMap.class, TEST_RESOURCE_NAME);
+
+    var annotations = configMap.getMetadata().getAnnotations();
+
+    assertThat(annotations)
+        .containsEntry(DEFAULT_ANNOTATION_FOR_NAME, TEST_RESOURCE_NAME)
+        .containsEntry(DEFAULT_ANNOTATION_FOR_NAMESPACE, operator.getNamespace());
+
+    assertThat(configMap.getMetadata().getOwnerReferences()).isEmpty();
+
+    configMap.getData().put("additional_data", "data");
+    operator.replace(ConfigMap.class, configMap);
+
+    await().pollDelay(Duration.ofMillis(150)).untilAsserted(() -> {
+      assertThat(reconciler.getNumberOfExecutions()).isEqualTo(2);
+    });
+  }
+
+
+  DependentAnnotationSecondaryMapperResource testResource() {
+    var res = new DependentAnnotationSecondaryMapperResource();
+    res.setMetadata(new ObjectMetaBuilder()
+        .withName(TEST_RESOURCE_NAME)
+        .build());
+    return res;
+  }
+
+}
---
+++ operator-framework/src/test/java/io/javaoperatorsdk/operator/sample/dependentannotationsecondarymapper/DependentAnnotationSecondaryMapperReconciler.java
@@ -0,0 +1,58 @@
+package io.javaoperatorsdk.operator.sample.dependentannotationsecondarymapper;
+
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import io.fabric8.kubernetes.api.model.ConfigMap;
+import io.fabric8.kubernetes.api.model.ObjectMetaBuilder;
+import io.javaoperatorsdk.operator.api.reconciler.*;
+import io.javaoperatorsdk.operator.api.reconciler.dependent.Deleter;
+import io.javaoperatorsdk.operator.api.reconciler.dependent.Dependent;
+import io.javaoperatorsdk.operator.processing.dependent.Creator;
+import io.javaoperatorsdk.operator.processing.dependent.Updater;
+import io.javaoperatorsdk.operator.processing.dependent.kubernetes.KubernetesDependentResource;
+import io.javaoperatorsdk.operator.support.TestExecutionInfoProvider;
+
+@ControllerConfiguration(dependents = {@Dependent(
+    type = DependentAnnotationSecondaryMapperReconciler.ConfigMapDependentResource.class)})
+public class DependentAnnotationSecondaryMapperReconciler
+    implements Reconciler<DependentAnnotationSecondaryMapperResource>, TestExecutionInfoProvider {
+
+  private final AtomicInteger numberOfExecutions = new AtomicInteger(0);
+
+  @Override
+  public UpdateControl<DependentAnnotationSecondaryMapperResource> reconcile(
+      DependentAnnotationSecondaryMapperResource resource,
+      Context<DependentAnnotationSecondaryMapperResource> context) {
+    numberOfExecutions.addAndGet(1);
+    return UpdateControl.noUpdate();
+  }
+
+  public int getNumberOfExecutions() {
+    return numberOfExecutions.get();
+  }
+
+  public static class ConfigMapDependentResource extends
+      KubernetesDependentResource<ConfigMap, DependentAnnotationSecondaryMapperResource>
+      implements Creator<ConfigMap, DependentAnnotationSecondaryMapperResource>,
+      Updater<ConfigMap, DependentAnnotationSecondaryMapperResource>,
+      Deleter<DependentAnnotationSecondaryMapperResource> {
+
+    public ConfigMapDependentResource() {
+      super(ConfigMap.class);
+    }
+
+    @Override
+    protected ConfigMap desired(DependentAnnotationSecondaryMapperResource primary,
+        Context<DependentAnnotationSecondaryMapperResource> context) {
+      ConfigMap configMap = new ConfigMap();
+      configMap.setMetadata(new ObjectMetaBuilder()
+          .withName(primary.getMetadata().getName())
+          .withNamespace(primary.getMetadata().getNamespace())
+          .build());
+      configMap.setData(Map.of("data", primary.getMetadata().getName()));
+      return configMap;
+    }
+  }
+
+}
