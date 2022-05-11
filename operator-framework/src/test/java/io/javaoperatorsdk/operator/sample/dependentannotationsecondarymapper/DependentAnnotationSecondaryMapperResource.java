bb457598099300c3324d169bea1a27a5feb33246
Attila Mészáros <csviri@gmail.com>
2022-05-11 13:35:07 +0200

    Using Annotations to Identify primary for a secondary object if no owner reference can be added (#1197)

---
+++ operator-framework/src/test/java/io/javaoperatorsdk/operator/sample/dependentannotationsecondarymapper/DependentAnnotationSecondaryMapperResource.java
@@ -0,0 +1,17 @@
+package io.javaoperatorsdk.operator.sample.dependentannotationsecondarymapper;
+
+import io.fabric8.kubernetes.api.model.Namespaced;
+import io.fabric8.kubernetes.client.CustomResource;
+import io.fabric8.kubernetes.model.annotation.Group;
+import io.fabric8.kubernetes.model.annotation.Kind;
+import io.fabric8.kubernetes.model.annotation.ShortNames;
+import io.fabric8.kubernetes.model.annotation.Version;
+
+@Group("sample.javaoperatorsdk")
+@Version("v1")
+@Kind("MaxIntervalTestCustomResource")
+@ShortNames("mit")
+public class DependentAnnotationSecondaryMapperResource
+    extends CustomResource<Void, DependentAnnotationSecondaryMapperResourceStatus>
+    implements Namespaced {
+}
