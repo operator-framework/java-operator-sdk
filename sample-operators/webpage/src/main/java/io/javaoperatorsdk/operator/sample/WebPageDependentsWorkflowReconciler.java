163326bd5867119bb50adc7ec2577711dbc93a4a
Attila Mészáros <csviri@gmail.com>
2022-05-25 16:11:35 +0200

    Workflow engine implementation (#1153)

---
+++ sample-operators/webpage/src/main/java/io/javaoperatorsdk/operator/sample/WebPageDependentsWorkflowReconciler.java
@@ -0,0 +1,101 @@
+package io.javaoperatorsdk.operator.sample;
+
+import java.util.Arrays;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.fabric8.kubernetes.api.model.ConfigMap;
+import io.fabric8.kubernetes.api.model.Service;
+import io.fabric8.kubernetes.api.model.apps.Deployment;
+import io.fabric8.kubernetes.api.model.networking.v1.Ingress;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import io.javaoperatorsdk.operator.api.reconciler.*;
+import io.javaoperatorsdk.operator.api.reconciler.dependent.DependentResource;
+import io.javaoperatorsdk.operator.processing.dependent.kubernetes.KubernetesDependentResource;
+import io.javaoperatorsdk.operator.processing.dependent.kubernetes.KubernetesDependentResourceConfig;
+import io.javaoperatorsdk.operator.processing.dependent.workflow.Condition;
+import io.javaoperatorsdk.operator.processing.dependent.workflow.Workflow;
+import io.javaoperatorsdk.operator.processing.dependent.workflow.builder.WorkflowBuilder;
+import io.javaoperatorsdk.operator.processing.event.source.EventSource;
+
+import static io.javaoperatorsdk.operator.sample.Utils.*;
+
+/**
+ * Shows how to implement reconciler using standalone dependent resources.
+ */
+@ControllerConfiguration(
+    labelSelector = WebPageDependentsWorkflowReconciler.DEPENDENT_RESOURCE_LABEL_SELECTOR)
+public class WebPageDependentsWorkflowReconciler
+    implements Reconciler<WebPage>, ErrorStatusHandler<WebPage>, EventSourceInitializer<WebPage> {
+
+  public static final String DEPENDENT_RESOURCE_LABEL_SELECTOR = "!low-level";
+  private static final Logger log =
+      LoggerFactory.getLogger(WebPageDependentsWorkflowReconciler.class);
+
+  private KubernetesDependentResource<ConfigMap, WebPage> configMapDR;
+  private KubernetesDependentResource<Deployment, WebPage> deploymentDR;
+  private KubernetesDependentResource<Service, WebPage> serviceDR;
+  private KubernetesDependentResource<Ingress, WebPage> ingressDR;
+
+  private Workflow<WebPage> workflow;
+
+  public WebPageDependentsWorkflowReconciler(KubernetesClient kubernetesClient) {
+    initDependentResources(kubernetesClient);
+    workflow = new WorkflowBuilder<WebPage>()
+        .addDependent(configMapDR).build()
+        .addDependent(deploymentDR).build()
+        .addDependent(serviceDR).build()
+        .addDependent(ingressDR).withReconcileCondition(new IngressCondition()).build()
+        .build();
+  }
+
+  @Override
+  public Map<String, EventSource> prepareEventSources(EventSourceContext<WebPage> context) {
+    return EventSourceInitializer.nameEventSources(configMapDR.initEventSource(context),
+        deploymentDR.initEventSource(context), serviceDR.initEventSource(context),
+        ingressDR.initEventSource(context));
+  }
+
+  @Override
+  public UpdateControl<WebPage> reconcile(WebPage webPage, Context<WebPage> context)
+      throws Exception {
+    simulateErrorIfRequested(webPage);
+
+    workflow.reconcile(webPage, context);
+
+    webPage.setStatus(
+        createStatus(
+            configMapDR.getSecondaryResource(webPage).orElseThrow().getMetadata().getName()));
+    return UpdateControl.patchStatus(webPage);
+  }
+
+  @Override
+  public ErrorStatusUpdateControl<WebPage> updateErrorStatus(
+      WebPage resource, Context<WebPage> retryInfo, Exception e) {
+    return handleError(resource, e);
+  }
+
+  private void initDependentResources(KubernetesClient client) {
+    this.configMapDR = new ConfigMapDependentResource();
+    this.deploymentDR = new DeploymentDependentResource();
+    this.serviceDR = new ServiceDependentResource();
+    this.ingressDR = new IngressDependentResource();
+
+    Arrays.asList(configMapDR, deploymentDR, serviceDR, ingressDR).forEach(dr -> {
+      dr.setKubernetesClient(client);
+      dr.configureWith(new KubernetesDependentResourceConfig()
+          .setLabelSelector(DEPENDENT_RESOURCE_LABEL_SELECTOR));
+    });
+  }
+
+  static class IngressCondition implements Condition<Ingress, WebPage> {
+    @Override
+    public boolean isMet(DependentResource<Ingress, WebPage> dependentResource, WebPage primary,
+        Context<WebPage> context) {
+      return primary.getSpec().getExposed();
+    }
+  }
+
+}
