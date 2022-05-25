d960a99aa5716e9332f25ef6781bc1ee93e2342f
Attila Mészáros <csviri@gmail.com>
2022-05-25 16:29:47 +0200

    fix: improvements event source management (#1239)

--- operator-framework-core/src/test/java/io/javaoperatorsdk/operator/processing/event/EventSourcesTest.java
+++ operator-framework-core/src/test/java/io/javaoperatorsdk/operator/processing/event/EventSourcesTest.java
@@ -1,8 +1,5 @@
 package io.javaoperatorsdk.operator.processing.event;
 
-import java.util.Set;
-import java.util.stream.Collectors;
-
 import org.junit.jupiter.api.Test;
 
 import io.fabric8.kubernetes.api.model.HasMetadata;
@@ -12,12 +9,16 @@ import io.javaoperatorsdk.operator.api.reconciler.Reconciler;
 import io.javaoperatorsdk.operator.processing.Controller;
 import io.javaoperatorsdk.operator.processing.event.source.EventSource;
 
+import static io.javaoperatorsdk.operator.processing.event.EventSources.CONTROLLER_RESOURCE_EVENT_SOURCE_NAME;
+import static io.javaoperatorsdk.operator.processing.event.EventSources.RETRY_RESCHEDULE_TIMER_EVENT_SOURCE_NAME;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 
 @SuppressWarnings({"unchecked", "rawtypes"})
 class EventSourcesTest {
 
+  public static final String EVENT_SOURCE_NAME = "foo";
   EventSources eventSources = new EventSources();
 
   @Test
@@ -31,20 +32,41 @@ class EventSourcesTest {
   @Test
   void allEventSourcesShouldReturnAll() {
     // initial state doesn't have ControllerResourceEventSource
-    assertEquals(Set.of(eventSources.retryEventSource()), eventSources.allEventSources().collect(
-        Collectors.toSet()));
+    assertThat(eventSources.eventSources()).containsExactly(eventSources.retryEventSource());
+
+    initControllerEventSource();
+
+    assertThat(eventSources.eventSources()).containsExactly(
+        eventSources.controllerResourceEventSource(),
+        eventSources.retryEventSource());
+
+    final var source = mock(EventSource.class);
+    eventSources.add(EVENT_SOURCE_NAME, source);
+    // order matters
+    assertThat(eventSources.eventSources())
+        .containsExactly(eventSources.controllerResourceEventSource(),
+            eventSources.retryEventSource(), source);
+  }
+
+  @Test
+  void eventSourcesIteratorShouldReturnControllerEventSourceAsFirst() {
+    initControllerEventSource();
+    final var source = mock(EventSource.class);
+    eventSources.add(EVENT_SOURCE_NAME, source);
+
+    assertThat(eventSources.iterator()).toIterable().containsExactly(
+        new NamedEventSource(eventSources.controllerResourceEventSource(),
+            CONTROLLER_RESOURCE_EVENT_SOURCE_NAME),
+        new NamedEventSource(eventSources.retryEventSource(),
+            RETRY_RESCHEDULE_TIMER_EVENT_SOURCE_NAME),
+        new NamedEventSource(source, EVENT_SOURCE_NAME));
+  }
+
+  private void initControllerEventSource() {
     final var configuration = MockControllerConfiguration.forResource(HasMetadata.class);
     final var controller = new Controller(mock(Reconciler.class), configuration,
         MockKubernetesClient.client(HasMetadata.class));
     eventSources.initControllerEventSource(controller);
-    assertEquals(
-        Set.of(eventSources.retryEventSource(), eventSources.controllerResourceEventSource()),
-        eventSources.allEventSources().collect(Collectors.toSet()));
-    final var source = mock(EventSource.class);
-    eventSources.add("foo", source);
-    assertEquals(Set.of(eventSources.retryEventSource(),
-        eventSources.controllerResourceEventSource(), source),
-        eventSources.allEventSources().collect(Collectors.toSet()));
   }
 
 }
