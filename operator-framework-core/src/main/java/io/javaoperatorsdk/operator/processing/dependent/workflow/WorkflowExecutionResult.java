4dddf4108740349eb889c55521d5fdb5ad278eff
Attila Mészáros <csviri@gmail.com>
2022-05-25 16:11:35 +0200

    Workflow engine implementation (#1153)

---
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/dependent/workflow/WorkflowExecutionResult.java
@@ -0,0 +1,79 @@
+package io.javaoperatorsdk.operator.processing.dependent.workflow;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import io.javaoperatorsdk.operator.AggregatedOperatorException;
+import io.javaoperatorsdk.operator.api.reconciler.dependent.DependentResource;
+import io.javaoperatorsdk.operator.api.reconciler.dependent.ReconcileResult;
+
+@SuppressWarnings("rawtypes")
+public class WorkflowExecutionResult {
+
+  private List<DependentResource> reconciledDependents;
+  private List<DependentResource> notReadyDependents;
+  private Map<DependentResource, Exception> erroredDependents;
+  private Map<DependentResource, ReconcileResult> reconcileResults;
+
+  public Map<DependentResource, Exception> getErroredDependents() {
+    return erroredDependents;
+  }
+
+  public WorkflowExecutionResult setErroredDependents(
+      Map<DependentResource, Exception> erroredDependents) {
+    this.erroredDependents = erroredDependents;
+    return this;
+  }
+
+  public List<DependentResource> getReconciledDependents() {
+    return reconciledDependents;
+  }
+
+  public WorkflowExecutionResult setReconciledDependents(
+      List<DependentResource> reconciledDependents) {
+    this.reconciledDependents = reconciledDependents;
+    return this;
+  }
+
+  public List<DependentResource> getNotReadyDependents() {
+    return notReadyDependents;
+  }
+
+  public WorkflowExecutionResult setNotReadyDependents(
+      List<DependentResource> notReadyDependents) {
+    this.notReadyDependents = notReadyDependents;
+    return this;
+  }
+
+  public Map<DependentResource, ReconcileResult> getReconcileResults() {
+    return reconcileResults;
+  }
+
+  public WorkflowExecutionResult setReconcileResults(
+      Map<DependentResource, ReconcileResult> reconcileResults) {
+    this.reconcileResults = reconcileResults;
+    return this;
+  }
+
+  public void throwAggregateExceptionIfErrorsPresent() {
+    if (!erroredDependents.isEmpty()) {
+      throw createFinalException();
+    }
+  }
+
+  private AggregatedOperatorException createFinalException() {
+    return new AggregatedOperatorException("Exception during workflow.",
+        new ArrayList<>(erroredDependents.values()));
+  }
+
+  public boolean notReadyDependentsExists() {
+    return !notReadyDependents.isEmpty();
+  }
+
+  public boolean erroredDependentsExists() {
+    return !erroredDependents.isEmpty();
+  }
+
+
+}
---
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/dependent/workflow/builder/DependentBuilder.java
@@ -0,0 +1,46 @@
+package io.javaoperatorsdk.operator.processing.dependent.workflow.builder;
+
+import io.fabric8.kubernetes.api.model.HasMetadata;
+import io.javaoperatorsdk.operator.api.reconciler.dependent.DependentResource;
+import io.javaoperatorsdk.operator.processing.dependent.workflow.Condition;
+import io.javaoperatorsdk.operator.processing.dependent.workflow.DependentResourceNode;
+
+@SuppressWarnings("rawtypes")
+public class DependentBuilder<P extends HasMetadata> {
+
+  private final WorkflowBuilder<P> workflowBuilder;
+  private final DependentResourceNode<?, ?> node;
+
+  public DependentBuilder(WorkflowBuilder<P> workflowBuilder, DependentResourceNode<?, ?> node) {
+    this.workflowBuilder = workflowBuilder;
+    this.node = node;
+  }
+
+  public DependentBuilder<P> dependsOn(DependentResource... dependentResources) {
+    for (var dependentResource : dependentResources) {
+      var dependsOn = workflowBuilder.getNodeByDependentResource(dependentResource);
+      node.addDependsOnRelation(dependsOn);
+    }
+    return this;
+  }
+
+  public DependentBuilder<P> withReconcileCondition(Condition reconcileCondition) {
+    node.setReconcileCondition(reconcileCondition);
+    return this;
+  }
+
+  public DependentBuilder<P> withReadyCondition(Condition readyCondition) {
+    node.setReadyCondition(readyCondition);
+    return this;
+  }
+
+  public DependentBuilder<P> withDeletePostCondition(Condition readyCondition) {
+    node.setDeletePostCondition(readyCondition);
+    return this;
+  }
+
+  public WorkflowBuilder<P> build() {
+    return workflowBuilder;
+  }
+
+}
