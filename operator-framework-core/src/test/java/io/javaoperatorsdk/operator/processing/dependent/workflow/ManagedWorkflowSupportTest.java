b3dbfbdc52dd1b902f84366901bba32b1772abce
Chris Laprun <metacosm@gmail.com>
2022-06-08 21:26:12 +0200

    chore(tests): add more tests

--- operator-framework-core/src/test/java/io/javaoperatorsdk/operator/processing/dependent/workflow/ManagedWorkflowSupportTest.java
+++ operator-framework-core/src/test/java/io/javaoperatorsdk/operator/processing/dependent/workflow/ManagedWorkflowSupportTest.java
@@ -78,6 +78,34 @@ class ManagedWorkflowSupportTest {
         .contains(NAME_4, Index.atIndex(3));
   }
 
+
+  @Test
+  void orderingMultipleRoots() {
+    final var NAME_3 = "name3";
+    final var NAME_4 = "name4";
+    final var NAME_5 = "name5";
+    final var NAME_6 = "name6";
+
+    var res = managedWorkflowSupport
+        .orderAndDetectCycles(List.of(
+            createDRS(NAME_2, NAME_1, NAME_5),
+            createDRS(NAME_1),
+            createDRS(NAME_3, NAME_1),
+            createDRS(NAME_4, NAME_2, NAME_3),
+            createDRS(NAME_5, NAME_1, NAME_6),
+            createDRS(NAME_6)))
+        .stream().map(DependentResourceSpec::getName).collect(Collectors.toList());
+
+    assertThat(res)
+        .containsExactlyInAnyOrder(NAME_1, NAME_5, NAME_6, NAME_2, NAME_3, NAME_4)
+        .contains(NAME_6, Index.atIndex(0))
+        .contains(NAME_1, Index.atIndex(1))
+        .contains(NAME_5, Index.atIndex(2))
+        .contains(NAME_3, Index.atIndex(3))
+        .contains(NAME_2, Index.atIndex(4))
+        .contains(NAME_4, Index.atIndex(5));
+  }
+
   @Test
   void detectsCyclesTrivialCases() {
     String NAME_3 = "name3";
@@ -96,6 +124,13 @@ class ManagedWorkflowSupportTest {
         () -> managedWorkflowSupport.orderAndDetectCycles(List.of(createDRS(NAME_1),
             createDRS(NAME_2, NAME_1),
             createDRS(NAME_3, NAME_1, NAME_4),
+            createDRS(NAME_4, NAME_3))));
+
+    Assertions.assertThrows(OperatorException.class,
+        () -> managedWorkflowSupport.orderAndDetectCycles(List.of(
+            createDRS(NAME_1),
+            createDRS(NAME_2, NAME_1, NAME_4),
+            createDRS(NAME_3, NAME_2),
             createDRS(NAME_4, NAME_3))));
   }
 
