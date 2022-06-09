3f1236224b1e3d1dbcaca3e3da6886e46b1f5160
Chris Laprun <metacosm@gmail.com>
2022-06-09 08:54:05 +0200

    ˆ

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/dependent/kubernetes/CRUDNoGCKubernetesDependentResource.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/dependent/kubernetes/CRUDNoGCKubernetesDependentResource.java
@@ -6,8 +6,7 @@ import io.javaoperatorsdk.operator.processing.dependent.Creator;
 import io.javaoperatorsdk.operator.processing.dependent.Updater;
 
 public class CRUDNoGCKubernetesDependentResource<R extends HasMetadata, P extends HasMetadata>
-    extends
-    KubernetesDependentResource<R, P>
+    extends KubernetesDependentResource<R, P>
     implements Creator<R, P>, Updater<R, P>, Deleter<P> {
 
   public CRUDNoGCKubernetesDependentResource(Class<R> resourceType) {
