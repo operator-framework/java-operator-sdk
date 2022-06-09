07267fd7a1131e251e73b308b72ad3c435fd49c3
csviri <csviri@gmail.com>
2022-06-09 11:13:12 +0200

    fix

--- operator-framework-core/src/test/java/io/javaoperatorsdk/operator/processing/event/source/PatchGenerationTest.java
+++ operator-framework-core/src/test/java/io/javaoperatorsdk/operator/processing/event/source/PatchGenerationTest.java
@@ -3,11 +3,7 @@ package io.javaoperatorsdk.operator.processing.event.source;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.function.Consumer;
 
-import io.fabric8.kubernetes.client.utils.Serialization;
-import io.javaoperatorsdk.operator.api.config.*;
-import io.javaoperatorsdk.operator.sample.simple.TestCustomResourceStatus;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,19 +11,12 @@ import org.slf4j.LoggerFactory;
 import io.fabric8.kubernetes.api.model.ObjectMetaBuilder;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.kubernetes.client.server.mock.EnableKubernetesMockClient;
-import io.javaoperatorsdk.operator.api.reconciler.Constants;
-import io.javaoperatorsdk.operator.api.reconciler.Context;
-import io.javaoperatorsdk.operator.api.reconciler.ControllerConfiguration;
-import io.javaoperatorsdk.operator.api.reconciler.Reconciler;
-import io.javaoperatorsdk.operator.api.reconciler.UpdateControl;
+import io.fabric8.kubernetes.client.utils.Serialization;
+import io.javaoperatorsdk.operator.api.config.*;
 import io.javaoperatorsdk.operator.sample.simple.TestCustomResource;
+import io.javaoperatorsdk.operator.sample.simple.TestCustomResourceStatus;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.awaitility.Awaitility.await;
-import static org.hamcrest.Matchers.is;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.spy;
 
 @EnableKubernetesMockClient(crud = true, https = false)
 class PatchGenerationTest {
@@ -39,30 +28,32 @@ class PatchGenerationTest {
 
   @Test
   void reproducePatchIssueWithMockServer() {
-    TestCustomResource resource = newMyResource("foo", NAMESPACE);
-
-    var created = client.resources(TestCustomResource.class).inNamespace(NAMESPACE).create(resource);
+    var created =
+        client.resources(TestCustomResource.class).inNamespace(NAMESPACE)
+            .create(newMyResource("foo", NAMESPACE));
     var createdWithStatusChanged = ConfigurationService.DEFAULT_CLONER.clone(created);
     createdWithStatusChanged.setStatus(new TestCustomResourceStatus());
     createdWithStatusChanged.getStatus().setConfigMapStatus("somevalue");
 
-    var res = patchStatus(createdWithStatusChanged,created);
+    var res = patchStatus(createdWithStatusChanged, created);
+
     assertThat(res.getMetadata().getGeneration()).isEqualTo(created.getMetadata().getGeneration());
   }
 
-  public TestCustomResource patchStatus(TestCustomResource resource, TestCustomResource originalResource) {
+  public TestCustomResource patchStatus(TestCustomResource resource,
+      TestCustomResource originalResource) {
 
     String resourceVersion = resource.getMetadata().getResourceVersion();
     // don't do optimistic locking on patch
     originalResource.getMetadata().setResourceVersion(null);
     resource.getMetadata().setResourceVersion(null);
     try (var bis = new ByteArrayInputStream(
-            Serialization.asJson(originalResource).getBytes(StandardCharsets.UTF_8))) {
+        Serialization.asJson(originalResource).getBytes(StandardCharsets.UTF_8))) {
       return client.resources(TestCustomResource.class)
-              .inNamespace(NAMESPACE)
-              // will be simplified in fabric8 v6
-              .load(bis)
-              .editStatus(r -> resource);
+          .inNamespace(NAMESPACE)
+          // will be simplified in fabric8 v6
+          .load(bis)
+          .editStatus(r -> resource);
     } catch (IOException e) {
       throw new IllegalStateException(e);
     } finally {
