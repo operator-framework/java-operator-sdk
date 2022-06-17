52394dc5dc957bba9af6a96abe57da463d3f8ba0
csviri <csviri@gmail.com>
2022-06-17 14:37:06 +0200

    integration test added

---
+++ operator-framework/src/test/java/io/javaoperatorsdk/operator/RateLimitIT.java
@@ -0,0 +1,62 @@
+package io.javaoperatorsdk.operator;
+
+import java.time.Duration;
+import java.util.stream.IntStream;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.fabric8.kubernetes.api.model.ObjectMetaBuilder;
+import io.javaoperatorsdk.operator.junit.LocallyRunOperatorExtension;
+import io.javaoperatorsdk.operator.sample.ratelimit.RateLimitCustomResource;
+import io.javaoperatorsdk.operator.sample.ratelimit.RateLimitCustomResourceSpec;
+import io.javaoperatorsdk.operator.sample.ratelimit.RateLimitReconciler;
+
+import static io.javaoperatorsdk.operator.sample.ratelimit.RateLimitReconciler.REFRESH_PERIOD;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
+
+class RateLimitIT {
+
+  private final static Logger log = LoggerFactory.getLogger(RateLimitIT.class);
+
+  @RegisterExtension
+  LocallyRunOperatorExtension operator =
+      LocallyRunOperatorExtension.builder()
+          .withReconciler(new RateLimitReconciler())
+          .build();
+
+  @Test
+  void rateLimitsExecution() {
+    var res = operator.create(RateLimitCustomResource.class, createResource());
+    IntStream.rangeClosed(1, 5).forEach(i -> {
+      log.debug("replacing resource version: {}", i);
+      var resource = createResource();
+      resource.getSpec().setNumber(i);
+      operator.replace(RateLimitCustomResource.class, resource);
+    });
+    await().pollInterval(Duration.ofMillis(100))
+        .pollDelay(Duration.ofMillis(REFRESH_PERIOD / 2))
+        .untilAsserted(() -> assertThat(
+            operator.getReconcilerOfType(RateLimitReconciler.class).getNumberOfExecutions())
+            .isEqualTo(1));
+
+    await().pollDelay(Duration.ofMillis(REFRESH_PERIOD))
+        .untilAsserted(() -> assertThat(
+            operator.getReconcilerOfType(RateLimitReconciler.class).getNumberOfExecutions())
+            .isEqualTo(2));
+  }
+
+  public RateLimitCustomResource createResource() {
+    RateLimitCustomResource res = new RateLimitCustomResource();
+    res.setMetadata(new ObjectMetaBuilder()
+        .withName("test")
+        .build());
+    res.setSpec(new RateLimitCustomResourceSpec());
+    res.getSpec().setNumber(0);
+    return res;
+  }
+
+}
---
+++ operator-framework/src/test/java/io/javaoperatorsdk/operator/sample/ratelimit/RateLimitReconciler.java
@@ -0,0 +1,30 @@
+package io.javaoperatorsdk.operator.sample.ratelimit;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import io.javaoperatorsdk.operator.api.reconciler.*;
+
+@ControllerConfiguration(rateLimiter = @RateLimiter(limitForPeriod = 1,
+    refreshPeriod = RateLimitReconciler.REFRESH_PERIOD,
+    refreshPeriodTimeUnit = TimeUnit.MILLISECONDS))
+public class RateLimitReconciler
+    implements Reconciler<RateLimitCustomResource> {
+
+  public static final int REFRESH_PERIOD = 3000;
+
+  private final AtomicInteger numberOfExecutions = new AtomicInteger(0);
+
+  @Override
+  public UpdateControl<RateLimitCustomResource> reconcile(
+      RateLimitCustomResource resource,
+      Context<RateLimitCustomResource> context) {
+
+    numberOfExecutions.addAndGet(1);
+    return UpdateControl.noUpdate();
+  }
+
+  public int getNumberOfExecutions() {
+    return numberOfExecutions.get();
+  }
+}
