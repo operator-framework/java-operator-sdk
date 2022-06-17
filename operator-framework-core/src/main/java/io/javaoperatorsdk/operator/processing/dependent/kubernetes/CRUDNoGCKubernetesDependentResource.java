a6676b494aec118fedb4bdeedcc35f02083d1b5d
csviri <csviri@gmail.com>
2022-06-17 17:09:06 +0200

    fix: rebase on main

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/dependent/kubernetes/CRUDNoGCKubernetesDependentResource.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/dependent/kubernetes/CRUDNoGCKubernetesDependentResource.java
@@ -1,10 +1,12 @@
 package io.javaoperatorsdk.operator.processing.dependent.kubernetes;
 
 import io.fabric8.kubernetes.api.model.HasMetadata;
+import io.javaoperatorsdk.operator.api.reconciler.Ignore;
 import io.javaoperatorsdk.operator.api.reconciler.dependent.Deleter;
 import io.javaoperatorsdk.operator.processing.dependent.Creator;
 import io.javaoperatorsdk.operator.processing.dependent.Updater;
 
+@Ignore
 public class CRUDNoGCKubernetesDependentResource<R extends HasMetadata, P extends HasMetadata>
     extends KubernetesDependentResource<R, P>
     implements Creator<R, P>, Updater<R, P>, Deleter<P> {
