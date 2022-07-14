fe186f0b551eec9d173c5e1609945322c18be384
csviri <csviri@gmail.com>
2022-07-14 14:40:03 +0200

    docs: javadoc for garbage collected interface

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/api/reconciler/dependent/GarbageCollected.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/api/reconciler/dependent/GarbageCollected.java
@@ -3,13 +3,24 @@ package io.javaoperatorsdk.operator.api.reconciler.dependent;
 import io.fabric8.kubernetes.api.model.HasMetadata;
 
 /**
- * Should be implemented by {@link DependentResource} implementations that are explicitly deleted
- * during reconciliation but which should also benefit from Kubernetes' automated garbage collection
- * during the cleanup phase.
+ * <p>
+ * Can be implemented by a dependent resource extending
+ * {@link io.javaoperatorsdk.operator.processing.dependent.kubernetes.KubernetesDependentResource}
+ * to express that the resource deletion is handled by the controller during "reconcile". So in a
+ * workflow during "reconcile" (not during cleanup) when a "reconcilePrecondition" is not met for
+ * the resource or a resource on which the actual resource depends on is not ready
+ * (readyPostCondition not met) delete is called. During the cleanup however rather than calling
+ * explicitly delete Kubernetes garbage collector should clean up the resource (using owner
+ * references).
+ * </p>
+ * <p>
+ * If a dependent resource implement this interface an owner reference is automatically added to the
+ * managed resource.
+ * </p>
  * <p>
  * See <a href="https://github.com/java-operator-sdk/java-operator-sdk/issues/1127">this issue</a>
  * for more details.
+ * </p>
  */
 public interface GarbageCollected<P extends HasMetadata> extends Deleter<P> {
-
 }
