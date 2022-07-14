f65e1ed3ffd2189ece7e06e649f7148d8e6424ef
Chris Laprun <metacosm@gmail.com>
2022-07-14 18:58:01 +0200

    docs: minor rewording

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/api/reconciler/dependent/GarbageCollected.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/api/reconciler/dependent/GarbageCollected.java
@@ -1,21 +1,24 @@
 package io.javaoperatorsdk.operator.api.reconciler.dependent;
 
 import io.fabric8.kubernetes.api.model.HasMetadata;
+import io.javaoperatorsdk.operator.api.reconciler.Context;
 
 /**
  * <p>
  * Can be implemented by a dependent resource extending
  * {@link io.javaoperatorsdk.operator.processing.dependent.kubernetes.KubernetesDependentResource}
- * to express that the resource deletion is handled by the controller during "reconcile". So in a
- * workflow during "reconcile" (not during cleanup) when a "reconcilePrecondition" is not met for
- * the resource or a resource on which the actual resource depends on is not ready
- * (readyPostCondition not met) delete is called. During the cleanup however rather than calling
- * explicitly delete Kubernetes garbage collector should clean up the resource (using owner
- * references).
+ * to express that the resource deletion is handled by the controller during
+ * {@link DependentResource#reconcile(HasMetadata, Context)}. This takes effect during a
+ * reconciliation workflow, but not during a cleanup workflow, when a {@code reconcilePrecondition}
+ * is not met for the resource or a resource on which the the dependent that implements this
+ * interface depends on is not ready ({@code readyPostCondition} not met). In this case,
+ * {@link #delete(HasMetadata, Context)} is called. During a cleanup workflow, however,
+ * {@link #delete(HasMetadata, Context)} is not called, letting the Kubernetes garbage collector do
+ * its work instead (using owner references).
  * </p>
  * <p>
- * If a dependent resource implement this interface an owner reference is automatically added to the
- * managed resource.
+ * If a dependent resource implement this interface, an owner reference pointing to the associated
+ * primary resource will be automatically added to this managed resource.
  * </p>
  * <p>
  * See <a href="https://github.com/java-operator-sdk/java-operator-sdk/issues/1127">this issue</a>
