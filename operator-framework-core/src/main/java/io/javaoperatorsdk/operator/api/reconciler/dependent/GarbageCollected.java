df44917ef81725c10bbcb772ab7b434d511b13b9
csviri <csviri@gmail.com>
2022-07-15 10:10:20 +0200

    docs: fix of garbage collected javadoc

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/api/reconciler/dependent/GarbageCollected.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/api/reconciler/dependent/GarbageCollected.java
@@ -10,11 +10,9 @@ import io.javaoperatorsdk.operator.api.reconciler.Context;
  * to express that the resource deletion is handled by the controller during
  * {@link DependentResource#reconcile(HasMetadata, Context)}. This takes effect during a
  * reconciliation workflow, but not during a cleanup workflow, when a {@code reconcilePrecondition}
- * is not met for the resource or a resource on which the the dependent that implements this
- * interface depends on is not ready ({@code readyPostCondition} not met). In this case,
- * {@link #delete(HasMetadata, Context)} is called. During a cleanup workflow, however,
- * {@link #delete(HasMetadata, Context)} is not called, letting the Kubernetes garbage collector do
- * its work instead (using owner references).
+ * is not met for the resource. In this case, {@link #delete(HasMetadata, Context)} is called.
+ * During a cleanup workflow, however, {@link #delete(HasMetadata, Context)} is not called, letting
+ * the Kubernetes garbage collector do its work instead (using owner references).
  * </p>
  * <p>
  * If a dependent resource implement this interface, an owner reference pointing to the associated
@@ -24,6 +22,8 @@ import io.javaoperatorsdk.operator.api.reconciler.Context;
  * See <a href="https://github.com/java-operator-sdk/java-operator-sdk/issues/1127">this issue</a>
  * for more details.
  * </p>
+ *
+ * @param <P> primary resource type
  */
 public interface GarbageCollected<P extends HasMetadata> extends Deleter<P> {
 }
