3f3923c425223eb2453188edb5ee12481ec439e8
csviri <csviri@gmail.com>
2022-07-25 15:05:05 +0200

    migrate to v6

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/LeaderElectionManager.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/LeaderElectionManager.java
@@ -6,20 +6,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import io.fabric8.kubernetes.client.KubernetesClient;
-import io.fabric8.kubernetes.client.NamespacedKubernetesClient;
 import io.fabric8.kubernetes.client.extended.leaderelection.LeaderCallbacks;
 import io.fabric8.kubernetes.client.extended.leaderelection.LeaderElectionConfig;
 import io.fabric8.kubernetes.client.extended.leaderelection.LeaderElector;
 import io.fabric8.kubernetes.client.extended.leaderelection.LeaderElectorBuilder;
 import io.fabric8.kubernetes.client.extended.leaderelection.resourcelock.LeaseLock;
 import io.fabric8.kubernetes.client.extended.leaderelection.resourcelock.Lock;
+import io.javaoperatorsdk.operator.api.config.ConfigurationServiceProvider;
 import io.javaoperatorsdk.operator.api.config.LeaderElectionConfiguration;
 
 public class LeaderElectionManager {
 
   private static final Logger log = LoggerFactory.getLogger(LeaderElectionManager.class);
 
-  private LeaderElector<NamespacedKubernetesClient> leaderElector = null;
+  private LeaderElector leaderElector = null;
   private ControllerManager controllerManager;
 
   public LeaderElectionManager(ControllerManager controllerManager) {
@@ -28,8 +28,10 @@ public class LeaderElectionManager {
 
   public void init(LeaderElectionConfiguration config, KubernetesClient client) {
     Lock lock = new LeaseLock(config.getLeaseNamespace(), config.getLeaseName(), identity(config));
-    // the releaseOnCancel does not seem to be used anywhere
-    leaderElector = new LeaderElectorBuilder<>((NamespacedKubernetesClient) client)
+    // todo releaseOnCancel
+    // todo use this executor service?
+    leaderElector = new LeaderElectorBuilder(client,
+        ConfigurationServiceProvider.instance().getExecutorService())
         .withConfig(
             new LeaderElectionConfig(lock, config.getLeaseDuration(), config.getRenewDeadline(),
                 config.getRetryPeriod(), leaderCallbacks(), true, config.getLeaseName()))
