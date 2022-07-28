2db80acf367428d35b9f79cd50d80e3938cfa207
csviri <csviri@gmail.com>
2022-07-28 14:58:09 +0200

    IT fix

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/api/reconciler/ResourceDiscriminator.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/api/reconciler/ResourceDiscriminator.java
@@ -8,7 +8,7 @@ import io.javaoperatorsdk.operator.processing.event.EventSourceRetriever;
 // todo is discriminator a good name? it not just discriminates but also reads from cache
 public interface ResourceDiscriminator<R, P extends HasMetadata> {
 
-  Optional<R> distinguish(Class<R> resource, Context<P> context,
+  Optional<R> distinguish(Class<R> resource, P primary, Context<P> context,
       EventSourceRetriever<P> eventSourceManager);
 
 }
