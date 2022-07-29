5fe2013ed37a0e7e7d18186c4badfc8e6fce563a
csviri <csviri@gmail.com>
2022-07-29 09:31:48 +0200

    fixed ITs

---
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/api/reconciler/ResourceIDMatcherDiscriminator.java
@@ -0,0 +1,30 @@
+package io.javaoperatorsdk.operator.api.reconciler;
+
+import java.util.Optional;
+import java.util.function.Function;
+
+import io.fabric8.kubernetes.api.model.HasMetadata;
+import io.javaoperatorsdk.operator.processing.event.EventSourceRetriever;
+import io.javaoperatorsdk.operator.processing.event.ResourceID;
+
+public class ResourceIDMatcherDiscriminator<R extends HasMetadata, P extends HasMetadata>
+    implements ResourceDiscriminator<R, P> {
+
+  private final Function<P, ResourceID> mapper;
+
+  public ResourceIDMatcherDiscriminator(Function<P, ResourceID> mapper) {
+    this.mapper = mapper;
+  }
+
+  @Override
+  public Optional<R> distinguish(Class<R> resource, P primary, Context<P> context,
+      EventSourceRetriever<P> eventSourceManager) {
+    var resourceID = mapper.apply(primary);
+    return context.getSecondaryResources(resource).stream()
+        .filter(r -> r.getMetadata().getName()
+            .equals(resourceID.getName()) &&
+            resourceID.getNamespace().map(ns -> ns.equals(r.getMetadata().getNamespace()))
+                .orElse(true))
+        .findFirst();
+  }
+}
--- operator-framework/src/test/java/io/javaoperatorsdk/operator/sample/orderedmanageddependent/ConfigMapDependentResource2.java
+++ operator-framework/src/test/java/io/javaoperatorsdk/operator/sample/orderedmanageddependent/ConfigMapDependentResource2.java
@@ -2,16 +2,15 @@ package io.javaoperatorsdk.operator.sample.orderedmanageddependent;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
-import java.util.Set;
 
 import io.fabric8.kubernetes.api.model.ConfigMap;
 import io.fabric8.kubernetes.api.model.ObjectMeta;
 import io.javaoperatorsdk.operator.api.reconciler.Context;
-import io.javaoperatorsdk.operator.api.reconciler.ResourceListDiscriminator;
+import io.javaoperatorsdk.operator.api.reconciler.ResourceIDMatcherDiscriminator;
 import io.javaoperatorsdk.operator.api.reconciler.dependent.ReconcileResult;
 import io.javaoperatorsdk.operator.processing.dependent.kubernetes.CRUDKubernetesDependentResource;
 import io.javaoperatorsdk.operator.processing.dependent.kubernetes.KubernetesDependent;
+import io.javaoperatorsdk.operator.processing.event.ResourceID;
 
 @KubernetesDependent(labelSelector = "dependent = cm2",
     resourceDiscriminator = ConfigMapDependentResource2.CM2ResourceDiscriminator.class)
@@ -47,12 +46,9 @@ public class ConfigMapDependentResource2 extends
   }
 
   public static class CM2ResourceDiscriminator
-      extends ResourceListDiscriminator<ConfigMap, OrderedManagedDependentCustomResource> {
-    @Override
-    protected Optional<ConfigMap> distinguish(OrderedManagedDependentCustomResource primary,
-        Set<ConfigMap> resourceList) {
-      return resourceList.stream().filter(cm -> cm.getMetadata().getName()
-          .equals(primary.getMetadata().getName() + "2")).findFirst();
+      extends ResourceIDMatcherDiscriminator<ConfigMap, OrderedManagedDependentCustomResource> {
+    public CM2ResourceDiscriminator() {
+      super(p -> new ResourceID(p.getMetadata().getName() + "2", p.getMetadata().getNamespace()));
     }
   }
 
