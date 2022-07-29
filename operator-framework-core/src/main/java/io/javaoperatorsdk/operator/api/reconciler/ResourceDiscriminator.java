6b392e81965d6e6dce26396b504b0ec73f85dc30
csviri <csviri@gmail.com>
2022-07-29 16:03:01 +0200

    wip

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/api/reconciler/ResourceDiscriminator.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/api/reconciler/ResourceDiscriminator.java
@@ -6,6 +6,7 @@ import io.fabric8.kubernetes.api.model.HasMetadata;
 import io.javaoperatorsdk.operator.processing.event.EventSourceRetriever;
 
 // todo is discriminator a good name? it not just discriminates but also reads from cache
+// todo discuss a List version of this (for reconciler but also for batch processing?)
 public interface ResourceDiscriminator<R, P extends HasMetadata> {
 
   Optional<R> distinguish(Class<R> resource, P primary, Context<P> context,
