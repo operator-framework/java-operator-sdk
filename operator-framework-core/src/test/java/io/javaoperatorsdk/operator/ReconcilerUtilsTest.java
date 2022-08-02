f8d5e5df77e00ca7cd0b5d38e6b926df8a29d37e
csviri <csviri@gmail.com>
2022-08-02 11:37:10 +0200

    fix: test on fabric v6

--- operator-framework-core/src/test/java/io/javaoperatorsdk/operator/ReconcilerUtilsTest.java
+++ operator-framework-core/src/test/java/io/javaoperatorsdk/operator/ReconcilerUtilsTest.java
@@ -1,7 +1,5 @@
 package io.javaoperatorsdk.operator;
 
-import java.net.URI;
-
 import org.junit.jupiter.api.Test;
 
 import io.fabric8.kubernetes.api.model.ContainerBuilder;
@@ -15,7 +13,6 @@ import io.fabric8.kubernetes.api.model.apps.DeploymentBuilder;
 import io.fabric8.kubernetes.api.model.apps.DeploymentSpec;
 import io.fabric8.kubernetes.client.CustomResource;
 import io.fabric8.kubernetes.client.KubernetesClientException;
-import io.fabric8.kubernetes.client.http.HttpRequest;
 import io.fabric8.kubernetes.model.annotation.Group;
 import io.fabric8.kubernetes.model.annotation.ShortNames;
 import io.fabric8.kubernetes.model.annotation.Version;
@@ -31,8 +28,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 class ReconcilerUtilsTest {
 
@@ -121,13 +116,10 @@ class ReconcilerUtilsTest {
 
   @Test
   void handleKubernetesExceptionShouldThrowMissingCRDExceptionWhenAppropriate() {
-    var request = mock(HttpRequest.class);
-    when(request.uri()).thenReturn(URI
-        .create(RESOURCE_URI));
     assertThrows(MissingCRDException.class, () -> handleKubernetesClientException(
         new KubernetesClientException(
-            "Failure executing: GET at: " + RESOURCE_URI + ". Message: Not Found.",
-            null, 404, null, request),
+            "Failure executing: GET at: https://kubernetes.docker.internal:6443/apis/tomcatoperator.io/v1/tomcats. Message: Not Found.",
+            404, null),
         HasMetadata.getFullResourceName(Tomcat.class)));
   }
 
