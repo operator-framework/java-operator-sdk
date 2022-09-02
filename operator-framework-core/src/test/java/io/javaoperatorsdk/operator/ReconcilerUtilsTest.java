4727366aba5a217081e25c8fd7bfa58bf4c4b040
csviri <csviri@gmail.com>
2022-09-02 11:05:19 +0200

    fix format

--- operator-framework-core/src/test/java/io/javaoperatorsdk/operator/ReconcilerUtilsTest.java
+++ operator-framework-core/src/test/java/io/javaoperatorsdk/operator/ReconcilerUtilsTest.java
@@ -34,7 +34,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-
 class ReconcilerUtilsTest {
 
   public static final String RESOURCE_URI =
