b9553967f7d536713e4053a1efb4a1f37d23ae62
csviri <csviri@gmail.com>
2022-09-20 16:23:31 +0200

    wip

---
+++ operator-framework/src/test/java/io/javaoperatorsdk/operator/sample/bulkdependent/ManagedBulkDependentReconciler.java
@@ -0,0 +1,25 @@
+package io.javaoperatorsdk.operator.sample.bulkdependent;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import io.javaoperatorsdk.operator.api.reconciler.Context;
+import io.javaoperatorsdk.operator.api.reconciler.ControllerConfiguration;
+import io.javaoperatorsdk.operator.api.reconciler.Reconciler;
+import io.javaoperatorsdk.operator.api.reconciler.UpdateControl;
+
+@ControllerConfiguration
+public class ManagedBulkDependentReconciler
+    implements Reconciler<StandaloneBulkDependentTestCustomResource> {
+
+  private final AtomicInteger numberOfExecutions = new AtomicInteger(0);
+
+  @Override
+  public UpdateControl<StandaloneBulkDependentTestCustomResource> reconcile(
+      StandaloneBulkDependentTestCustomResource resource,
+      Context<StandaloneBulkDependentTestCustomResource> context) throws Exception {
+
+    numberOfExecutions.addAndGet(1);
+
+    return UpdateControl.noUpdate();
+  }
+}
