60ca8667f885b3b004aa12883d02a8c349795de5
Attila Mészáros <csviri@gmail.com>
2022-09-20 10:40:01 +0200

    docs: javadoc for CRUDNoGCKubernetesDependentResource (#1483)

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/dependent/kubernetes/CRUDNoGCKubernetesDependentResource.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/dependent/kubernetes/CRUDNoGCKubernetesDependentResource.java
@@ -6,6 +6,17 @@ import io.javaoperatorsdk.operator.api.reconciler.dependent.Deleter;
 import io.javaoperatorsdk.operator.processing.dependent.Creator;
 import io.javaoperatorsdk.operator.processing.dependent.Updater;
 
+/**
+ *
+ * Adaptor class resources that manage Create, Read and Update operations, however resource is NOT
+ * garbage collected by Kubernetes when the associated primary resource is destroyed, instead
+ * explicitly deleted. This is useful when resource needs to be deleted before another one in a
+ * workflow, in other words an ordering matters during a cleanup. See also:
+ * <a href="https://github.com/java-operator-sdk/java-operator-sdk/issues/1127">Related issue</a>
+ *
+ * @param <R> the type of the managed dependent resource
+ * @param <P> the type of the associated primary resource
+ */
 @Ignore
 public class CRUDNoGCKubernetesDependentResource<R extends HasMetadata, P extends HasMetadata>
     extends KubernetesDependentResource<R, P>
