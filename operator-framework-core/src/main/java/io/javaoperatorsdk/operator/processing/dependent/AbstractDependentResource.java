3d07b2e1c7eb1e321e5b95e6aa0b8f481e583abe
csviri <csviri@gmail.com>
2022-09-22 14:55:47 +0200

    docs

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/dependent/AbstractDependentResource.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/dependent/AbstractDependentResource.java
@@ -82,7 +82,8 @@ public abstract class AbstractDependentResource<R, P extends HasMetadata>
   }
 
   protected ReconcileResult<R> reconcileIndexAware(P primary, int i, Context<P> context) {
-    Optional<R> maybeActual = getSecondaryResourceIndexAware(primary, i, context);
+    Optional<R> maybeActual = bulk ? getSecondaryResourceIndexAware(primary, i, context)
+        : getSecondaryResource(primary, context);
     if (creatable || updatable) {
       if (maybeActual.isEmpty()) {
         if (creatable) {
@@ -126,7 +127,6 @@ public abstract class AbstractDependentResource<R, P extends HasMetadata>
         : desired(primary, context);
   }
 
-  // todo check
   protected Optional<R> getSecondaryResource(P primary, Context<P> context) {
     if (resourceDiscriminator.isEmpty()) {
       return context.getSecondaryResource(resourceType());
@@ -136,14 +136,6 @@ public abstract class AbstractDependentResource<R, P extends HasMetadata>
   }
 
   protected Optional<R> getSecondaryResourceIndexAware(P primary, int index, Context<P> context) {
-    if (index > 0 && resourceDiscriminator.isEmpty()) {
-      throw new IllegalStateException(
-          "Handling resources in bulk bot no resource discriminators set.");
-    }
-    if (!bulk) {
-      return getSecondaryResource(primary, context);
-    }
-
     return context.getSecondaryResource(resourceType(), resourceDiscriminator.get(index));
   }
 
--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/dependent/BulkUpdater.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/dependent/BulkUpdater.java
@@ -3,6 +3,13 @@ package io.javaoperatorsdk.operator.processing.dependent;
 import io.fabric8.kubernetes.api.model.HasMetadata;
 import io.javaoperatorsdk.operator.api.reconciler.Context;
 
+/**
+ * Helper for the Bulk Dependent Resources to make it more explicit that bulk needs to only
+ * implement the index aware match method.
+ *
+ * @param <R> secondary resource type
+ * @param <P> primary resource type
+ */
 public interface BulkUpdater<R, P extends HasMetadata> extends Updater<R, P> {
 
   default Matcher.Result<R> match(R actualResource, P primary, Context<P> context) {
