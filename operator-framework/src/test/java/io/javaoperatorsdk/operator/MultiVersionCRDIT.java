a7877616852164d14494af6812f7e94d1d938ec1
csviri <csviri@gmail.com>
2022-09-30 14:12:07 +0200

    docs: introduce ADRs

--- operator-framework/src/test/java/io/javaoperatorsdk/operator/MultiVersionCRDIT.java
+++ operator-framework/src/test/java/io/javaoperatorsdk/operator/MultiVersionCRDIT.java
@@ -3,6 +3,7 @@ package io.javaoperatorsdk.operator;
 import java.time.Duration;
 import java.util.HashMap;
 
+import io.fabric8.kubernetes.client.utils.Serialization;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
