120d3a0705441523b4d2892b0a16b05d3be36b93
csviri <csviri@gmail.com>
2022-10-07 14:19:50 +0200

    wip

--- operator-framework/src/test/java/io/javaoperatorsdk/operator/ExternalStateIT.java
+++ operator-framework/src/test/java/io/javaoperatorsdk/operator/ExternalStateIT.java
@@ -1,12 +1,20 @@
 package io.javaoperatorsdk.operator;
 
+import io.javaoperatorsdk.operator.junit.LocallyRunOperatorExtension;
+import io.javaoperatorsdk.operator.sample.externalstate.ExternalStateReconciler;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
 class ExternalStateIT {
 
-  // @RegisterExtension
-  // LocallyRunOperatorExtension operator =
-  // LocallyRunOperatorExtension.builder().withReconciler(ExternalStateReconciler.class)
-  // .build();
+    @RegisterExtension
+    LocallyRunOperatorExtension operator =
+            LocallyRunOperatorExtension.builder().withReconciler(ExternalStateReconciler.class)
+                    .build();
 
+    @Test
+    public void reconcilesResourceWithPersistentState() {
 
+    }
 
 }
