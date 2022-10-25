62d0f405eec29b9d13c4c85cfd3f09d4fc044bc8
Chris Laprun <metacosm@users.noreply.github.com>
2022-10-25 16:31:02 +0200

    feat: introduce AnnotationDependentResourceConfigurator concept (#1554)
    The idea is to be able to configure dependents using annotations
    directly so that we can remove the special handling of
    KubernetesDependentResource from AnnotationControllerConfiguration. This
    results in dependent resources being instantiated and configured
    directly when processed from the annotations in the managed case, thus
    rendering the DependentResourceFactory concept obsolete. This should
    also lead to further simplification later.

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/api/reconciler/dependent/managed/DependentResourceConfigurator.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/api/reconciler/dependent/managed/DependentResourceConfigurator.java
@@ -1,5 +1,9 @@
 package io.javaoperatorsdk.operator.api.reconciler.dependent.managed;
 
+import java.util.Optional;
+
 public interface DependentResourceConfigurator<C> {
   void configureWith(C config);
+
+  Optional<C> configuration();
 }
