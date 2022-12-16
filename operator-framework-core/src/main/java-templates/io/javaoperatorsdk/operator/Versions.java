73889f9e380bdb869b9af170889a832261dda49f
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2022-12-16 16:37:25 +0000

    Generate a Versions file to be consumed at runtime

---
+++ operator-framework-core/src/main/java-templates/io/javaoperatorsdk/operator/Versions.java
@@ -0,0 +1,8 @@
+package io.javaoperatorsdk.operator;
+
+public final class Versions {
+
+  public static final String JOSDK = "${project.version}";
+  public static final String KUBERNETES_CLIENT = "${fabric8-client.version}";
+
+}
