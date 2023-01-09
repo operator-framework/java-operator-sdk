4107952a15e10c9404cf3b84aedba3c4162d74f0
Attila Mészáros <csviri@gmail.com>
2023-01-09 10:56:17 +0100

    feat: sample with multiple informers for same type dependent resources (#1698)

---
+++ operator-framework/src/test/java/io/javaoperatorsdk/operator/sample/multipledependentsametypemultiinformer/MultipleManagedDependentResourceMultiInformerCustomResource.java
@@ -0,0 +1,16 @@
+package io.javaoperatorsdk.operator.sample.multipledependentsametypemultiinformer;
+
+import io.fabric8.kubernetes.api.model.Namespaced;
+import io.fabric8.kubernetes.client.CustomResource;
+import io.fabric8.kubernetes.model.annotation.Group;
+import io.fabric8.kubernetes.model.annotation.ShortNames;
+import io.fabric8.kubernetes.model.annotation.Version;
+
+@Group("sample.javaoperatorsdk")
+@Version("v1")
+@ShortNames("mmi")
+public class MultipleManagedDependentResourceMultiInformerCustomResource
+    extends CustomResource<MultipleManagedDependentResourceMultiInformerSpec, Void>
+    implements Namespaced {
+
+}
