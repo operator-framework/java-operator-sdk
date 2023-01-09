faccb94e597910f1ab7544037642f726855699df
Chris Laprun <metacosm@users.noreply.github.com>
2023-01-09 17:09:02 +0100

    feat: provide Kubernetes client version (#1706)
    Generate a Versions file to be consumed at runtime
    
    Co-authored-by: Andrea Peruffo <andrea.peruffo1982@gmail.com>

---
+++ operator-framework-core/src/main/java-templates/io/javaoperatorsdk/operator/api/config/Versions.java
@@ -0,0 +1,10 @@
+package io.javaoperatorsdk.operator.api.config;
+
+public final class Versions {
+
+  private Versions() {}
+
+  protected static final String JOSDK = "${project.version}";
+  protected static final String KUBERNETES_CLIENT = "${fabric8-client.version}";
+
+}
