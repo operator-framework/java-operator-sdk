73861cd96ec46300880c9aaece82d15e0f7a2b54
Attila Mészáros <csviri@gmail.com>
2023-01-10 09:52:56 +0100

    feat: dependent resource in the condition instead of resource (#1690)

--- operator-framework/src/test/java/io/javaoperatorsdk/operator/sample/workflowallfeature/ConfigMapReconcileCondition.java
+++ operator-framework/src/test/java/io/javaoperatorsdk/operator/sample/workflowallfeature/ConfigMapReconcileCondition.java
@@ -2,14 +2,18 @@ package io.javaoperatorsdk.operator.sample.workflowallfeature;
 
 import io.fabric8.kubernetes.api.model.ConfigMap;
 import io.javaoperatorsdk.operator.api.reconciler.Context;
+import io.javaoperatorsdk.operator.api.reconciler.dependent.DependentResource;
 import io.javaoperatorsdk.operator.processing.dependent.workflow.Condition;
 
 public class ConfigMapReconcileCondition
     implements Condition<ConfigMap, WorkflowAllFeatureCustomResource> {
 
   @Override
-  public boolean isMet(WorkflowAllFeatureCustomResource primary, ConfigMap secondary,
+  public boolean isMet(
+      DependentResource<ConfigMap, WorkflowAllFeatureCustomResource> dependentResource,
+      WorkflowAllFeatureCustomResource primary,
       Context<WorkflowAllFeatureCustomResource> context) {
+
     return primary.getSpec().isCreateConfigMap();
   }
 }
