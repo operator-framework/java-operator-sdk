8fa5d5c1bc4d423b19160fff26973e11914f844d
Attila Mészáros <csviri@gmail.com>
2023-01-10 09:52:56 +0100

    feat: dependent resource in the condition instead of resource (#1690)

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/dependent/workflow/Condition.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/dependent/workflow/Condition.java
@@ -2,6 +2,7 @@ package io.javaoperatorsdk.operator.processing.dependent.workflow;
 
 import io.fabric8.kubernetes.api.model.HasMetadata;
 import io.javaoperatorsdk.operator.api.reconciler.Context;
+import io.javaoperatorsdk.operator.api.reconciler.dependent.DependentResource;
 
 public interface Condition<R, P extends HasMetadata> {
 
@@ -10,12 +11,10 @@ public interface Condition<R, P extends HasMetadata> {
    * {@link io.javaoperatorsdk.operator.api.reconciler.dependent.DependentResource} based on the
    * observed cluster state.
    *
+   * @param dependentResource for which the condition applies to
    * @param primary the primary resource being considered
-   * @param secondary the secondary resource associated with the specified primary resource or
-   *        {@code null} if no such secondary resource exists (for example because it's been
-   *        deleted)
    * @param context the current reconciliation {@link Context}
    * @return {@code true} if the condition holds, {@code false} otherwise
    */
-  boolean isMet(P primary, R secondary, Context<P> context);
+  boolean isMet(DependentResource<R, P> dependentResource, P primary, Context<P> context);
 }
--- operator-framework/src/test/java/io/javaoperatorsdk/operator/sample/complexdependent/dependent/StatefulSetReadyCondition.java
+++ operator-framework/src/test/java/io/javaoperatorsdk/operator/sample/complexdependent/dependent/StatefulSetReadyCondition.java
@@ -2,6 +2,7 @@ package io.javaoperatorsdk.operator.sample.complexdependent.dependent;
 
 import io.fabric8.kubernetes.api.model.apps.StatefulSet;
 import io.javaoperatorsdk.operator.api.reconciler.Context;
+import io.javaoperatorsdk.operator.api.reconciler.dependent.DependentResource;
 import io.javaoperatorsdk.operator.processing.dependent.workflow.Condition;
 import io.javaoperatorsdk.operator.sample.complexdependent.ComplexDependentCustomResource;
 
@@ -9,10 +10,15 @@ public class StatefulSetReadyCondition
     implements Condition<StatefulSet, ComplexDependentCustomResource> {
 
   @Override
-  public boolean isMet(ComplexDependentCustomResource primary, StatefulSet secondary,
+  public boolean isMet(
+      DependentResource<StatefulSet, ComplexDependentCustomResource> dependentResource,
+      ComplexDependentCustomResource primary,
       Context<ComplexDependentCustomResource> context) {
 
-    var readyReplicas = secondary.getStatus().getReadyReplicas();
-    return readyReplicas != null && readyReplicas > 0;
+    return dependentResource.getSecondaryResource(primary, context).map(secondary -> {
+      var readyReplicas = secondary.getStatus().getReadyReplicas();
+      return readyReplicas != null && readyReplicas > 0;
+    })
+        .orElse(false);
   }
 }
