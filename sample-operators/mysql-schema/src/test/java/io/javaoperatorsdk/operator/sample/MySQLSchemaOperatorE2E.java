c9f0f41f01ca07bbcef556fffad89a3504d6b0c6
csviri <csviri@gmail.com>
2023-01-19 19:08:18 +0100

    fix regarding api change

--- sample-operators/mysql-schema/src/test/java/io/javaoperatorsdk/operator/sample/MySQLSchemaOperatorE2E.java
+++ sample-operators/mysql-schema/src/test/java/io/javaoperatorsdk/operator/sample/MySQLSchemaOperatorE2E.java
@@ -43,8 +43,8 @@ class MySQLSchemaOperatorE2E {
     infrastructure.add(
         new NamespaceBuilder().withNewMetadata().withName(MY_SQL_NS).endMetadata().build());
     try {
-      infrastructure.addAll(client.load(new FileInputStream("k8s/mysql-deployment.yaml")).get());
-      infrastructure.addAll(client.load(new FileInputStream("k8s/mysql-service.yaml")).get());
+      infrastructure.addAll(client.load(new FileInputStream("k8s/mysql-deployment.yaml")).items());
+      infrastructure.addAll(client.load(new FileInputStream("k8s/mysql-service.yaml")).items());
     } catch (FileNotFoundException e) {
       e.printStackTrace();
     }
