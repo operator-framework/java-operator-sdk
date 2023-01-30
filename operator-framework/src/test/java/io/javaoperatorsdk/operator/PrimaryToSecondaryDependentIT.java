02e34ff74e541b5338fa1b4bae22b02c094f8421
csviri <csviri@gmail.com>
2023-01-30 15:36:03 +0100

    feat: integration test showcasing how to use primary to secondary mapper with dependent resources

---
+++ operator-framework/src/test/java/io/javaoperatorsdk/operator/PrimaryToSecondaryDependentIT.java
@@ -0,0 +1,76 @@
+package io.javaoperatorsdk.operator;
+
+import java.time.Duration;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.fabric8.kubernetes.api.model.ConfigMap;
+import io.fabric8.kubernetes.api.model.ObjectMetaBuilder;
+import io.fabric8.kubernetes.api.model.Secret;
+import io.javaoperatorsdk.operator.junit.LocallyRunOperatorExtension;
+import io.javaoperatorsdk.operator.sample.primarytosecondaydependent.PrimaryToSecondaryDependentCustomResource;
+import io.javaoperatorsdk.operator.sample.primarytosecondaydependent.PrimaryToSecondaryDependentReconciler;
+import io.javaoperatorsdk.operator.sample.primarytosecondaydependent.PrimaryToSecondaryDependentSpec;
+
+import static io.javaoperatorsdk.operator.sample.primarytosecondaydependent.ConfigMapReconcilePrecondition.DO_NOT_RECONCILE;
+import static io.javaoperatorsdk.operator.sample.primarytosecondaydependent.PrimaryToSecondaryDependentReconciler.DATA_KEY;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
+
+class PrimaryToSecondaryDependentIT {
+
+  public static final String TEST_CONFIG_MAP_NAME = "testconfigmap";
+  public static final String TEST_CR_NAME = "test1";
+  public static final String TEST_DATA = "testData";
+  public
+
+  @RegisterExtension LocallyRunOperatorExtension operator =
+      LocallyRunOperatorExtension.builder()
+          .withReconciler(new PrimaryToSecondaryDependentReconciler())
+          .build();
+
+  @Test
+  void testPrimaryToSecondaryInDependentResources() {
+    var reconciler = operator.getReconcilerOfType(PrimaryToSecondaryDependentReconciler.class);
+    var cm = operator.create(configMap(DO_NOT_RECONCILE));
+    var cr = operator.create(testCustomResource());
+
+    await().pollDelay(Duration.ofMillis(250)).untilAsserted(() -> {
+      assertThat(reconciler.getNumberOfExecutions()).isPositive();
+      assertThat(operator.get(Secret.class, TEST_CR_NAME)).isNull();
+    });
+
+    cm.setData(Map.of(DATA_KEY, TEST_DATA));
+    operator.replace(cm);
+    var executions = reconciler.getNumberOfExecutions();
+
+    await().pollDelay(Duration.ofMillis(250)).untilAsserted(() -> {
+      assertThat(reconciler.getNumberOfExecutions()).isGreaterThan(executions);
+      var secret = operator.get(Secret.class, TEST_CR_NAME);
+      assertThat(secret).isNotNull();
+      assertThat(secret.getData().get(DATA_KEY)).isEqualTo(TEST_DATA);
+    });
+  }
+
+  PrimaryToSecondaryDependentCustomResource testCustomResource() {
+    var res = new PrimaryToSecondaryDependentCustomResource();
+    res.setMetadata(new ObjectMetaBuilder()
+        .withName(TEST_CR_NAME)
+        .build());
+    res.setSpec(new PrimaryToSecondaryDependentSpec());
+    res.getSpec().setConfigMapName(TEST_CONFIG_MAP_NAME);
+    return res;
+  }
+
+  ConfigMap configMap(String data) {
+    var cm = new ConfigMap();
+    cm.setMetadata(new ObjectMetaBuilder()
+        .withName(TEST_CONFIG_MAP_NAME)
+        .build());
+    cm.setData(Map.of(DATA_KEY, data));
+    return cm;
+  }
+
+}
---
+++ operator-framework/src/test/java/io/javaoperatorsdk/operator/sample/primarytosecondaydependent/PrimaryToSecondaryDependentReconciler.java
@@ -0,0 +1,74 @@
+package io.javaoperatorsdk.operator.sample.primarytosecondaydependent;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+import io.fabric8.kubernetes.api.model.ConfigMap;
+import io.javaoperatorsdk.operator.api.config.informer.InformerConfiguration;
+import io.javaoperatorsdk.operator.api.reconciler.*;
+import io.javaoperatorsdk.operator.api.reconciler.dependent.Dependent;
+import io.javaoperatorsdk.operator.processing.event.ResourceID;
+import io.javaoperatorsdk.operator.processing.event.source.EventSource;
+import io.javaoperatorsdk.operator.processing.event.source.PrimaryToSecondaryMapper;
+import io.javaoperatorsdk.operator.processing.event.source.informer.InformerEventSource;
+import io.javaoperatorsdk.operator.support.TestExecutionInfoProvider;
+
+import static io.javaoperatorsdk.operator.sample.primarytosecondaydependent.PrimaryToSecondaryDependentReconciler.CONFIG_MAP;
+import static io.javaoperatorsdk.operator.sample.primarytosecondaydependent.PrimaryToSecondaryDependentReconciler.CONFIG_MAP_EVENT_SOURCE;
+
+@ControllerConfiguration(dependents = {@Dependent(type = ConfigMapDependent.class,
+    name = CONFIG_MAP,
+    reconcilePrecondition = ConfigMapReconcilePrecondition.class,
+    useEventSourceWithName = CONFIG_MAP_EVENT_SOURCE),
+    @Dependent(type = SecretDependent.class, dependsOn = CONFIG_MAP)})
+public class PrimaryToSecondaryDependentReconciler
+    implements Reconciler<PrimaryToSecondaryDependentCustomResource>, TestExecutionInfoProvider,
+    EventSourceInitializer<PrimaryToSecondaryDependentCustomResource> {
+
+  public static final String DATA_KEY = "data";
+  public static final String CONFIG_MAP = "ConfigMap";
+  public static final String CONFIG_MAP_INDEX = "ConfigMapIndex";
+  public static final String CONFIG_MAP_EVENT_SOURCE = "ConfigMapEventSource";
+
+  private final AtomicInteger numberOfExecutions = new AtomicInteger(0);
+
+  @Override
+  public UpdateControl<PrimaryToSecondaryDependentCustomResource> reconcile(
+      PrimaryToSecondaryDependentCustomResource resource,
+      Context<PrimaryToSecondaryDependentCustomResource> context) {
+    numberOfExecutions.addAndGet(1);
+    return UpdateControl.noUpdate();
+  }
+
+  public int getNumberOfExecutions() {
+    return numberOfExecutions.get();
+  }
+
+  @Override
+  public Map<String, EventSource> prepareEventSources(
+      EventSourceContext<PrimaryToSecondaryDependentCustomResource> context) {
+    context.getPrimaryCache().addIndexer(CONFIG_MAP_INDEX, (primary -> List
+        .of(indexKey(primary.getSpec().getConfigMapName(), primary.getMetadata().getNamespace()))));
+
+    var cmES = new InformerEventSource<>(InformerConfiguration
+        .from(ConfigMap.class, context)
+        .withPrimaryToSecondaryMapper(
+            (PrimaryToSecondaryMapper<PrimaryToSecondaryDependentCustomResource>) p -> Set
+                .of(new ResourceID(p.getSpec().getConfigMapName(), p.getMetadata().getNamespace())))
+        .withSecondaryToPrimaryMapper(cm -> context.getPrimaryCache()
+            .byIndex(CONFIG_MAP_INDEX, indexKey(cm.getMetadata().getName(),
+                cm.getMetadata().getNamespace()))
+            .stream().map(ResourceID::fromResource).collect(Collectors.toSet()))
+        .build(),
+        context);
+
+    return Map.of(CONFIG_MAP_EVENT_SOURCE, cmES);
+  }
+
+  private String indexKey(String clusterName, String namespace) {
+    return clusterName + "#" + namespace;
+  }
+}
