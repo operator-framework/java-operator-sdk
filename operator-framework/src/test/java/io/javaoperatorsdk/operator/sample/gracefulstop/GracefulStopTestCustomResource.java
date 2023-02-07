77f5571fbcd3ead88d9ac2e6c5c74654c3023471
Attila Mészáros <csviri@gmail.com>
2023-02-07 14:50:17 +0100

    feat: graceful shutdown (#1735)

---
+++ operator-framework/src/test/java/io/javaoperatorsdk/operator/sample/gracefulstop/GracefulStopTestCustomResource.java
@@ -0,0 +1,16 @@
+package io.javaoperatorsdk.operator.sample.gracefulstop;
+
+import io.fabric8.kubernetes.api.model.Namespaced;
+import io.fabric8.kubernetes.client.CustomResource;
+import io.fabric8.kubernetes.model.annotation.Group;
+import io.fabric8.kubernetes.model.annotation.ShortNames;
+import io.fabric8.kubernetes.model.annotation.Version;
+
+@Group("sample.javaoperatorsdk")
+@Version("v1")
+@ShortNames("gst")
+public class GracefulStopTestCustomResource
+    extends CustomResource<GracefulStopTestCustomResourceSpec, GracefulStopTestCustomResourceStatus>
+    implements Namespaced {
+
+}
