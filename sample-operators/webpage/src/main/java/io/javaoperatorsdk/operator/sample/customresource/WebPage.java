dfb0f6fdb0f4c24366f26e733cdf71751ccfa9c3
Attila Mészáros <csviri@gmail.com>
2023-02-21 09:44:17 +0100

    refactor: Webpage sample better readability (#1771)

--- sample-operators/webpage/src/main/java/io/javaoperatorsdk/operator/sample/customresource/WebPage.java
+++ sample-operators/webpage/src/main/java/io/javaoperatorsdk/operator/sample/customresource/WebPage.java
@@ -1,4 +1,4 @@
-package io.javaoperatorsdk.operator.sample;
+package io.javaoperatorsdk.operator.sample.customresource;
 
 import io.fabric8.kubernetes.api.model.Namespaced;
 import io.fabric8.kubernetes.client.CustomResource;
