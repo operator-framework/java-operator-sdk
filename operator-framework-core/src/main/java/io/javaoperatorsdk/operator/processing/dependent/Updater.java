c973cdd983c56125a9a64a8e3d069f7aa07e3105
Chris Laprun <metacosm@gmail.com>
2023-02-27 14:37:10 +0100

    fix: make explicit that Updater extends Matcher

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/dependent/Updater.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/dependent/Updater.java
@@ -2,10 +2,7 @@ package io.javaoperatorsdk.operator.processing.dependent;
 
 import io.fabric8.kubernetes.api.model.HasMetadata;
 import io.javaoperatorsdk.operator.api.reconciler.Context;
-import io.javaoperatorsdk.operator.processing.dependent.Matcher.Result;
 
-public interface Updater<R, P extends HasMetadata> {
+public interface Updater<R, P extends HasMetadata> extends Matcher<R, P> {
   R update(R actual, R desired, P primary, Context<P> context);
-
-  Result<R> match(R actualResource, P primary, Context<P> context);
 }
