7239c5a36975c7119d8af1a0501620466ac83bd6
Attila Mészáros <csviri@gmail.com>
2023-03-28 13:49:19 +0200

    fix: special IT failing test case (#1836)

--- operator-framework/src/test/java/io/javaoperatorsdk/operator/sample/informerrelatedbehavior/InformerRelatedBehaviorTestReconciler.java
+++ operator-framework/src/test/java/io/javaoperatorsdk/operator/sample/informerrelatedbehavior/InformerRelatedBehaviorTestReconciler.java
@@ -2,12 +2,16 @@ package io.javaoperatorsdk.operator.sample.informerrelatedbehavior;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.javaoperatorsdk.operator.api.reconciler.Context;
 import io.javaoperatorsdk.operator.api.reconciler.ControllerConfiguration;
 import io.javaoperatorsdk.operator.api.reconciler.Reconciler;
 import io.javaoperatorsdk.operator.api.reconciler.UpdateControl;
 import io.javaoperatorsdk.operator.api.reconciler.dependent.Dependent;
+import io.javaoperatorsdk.operator.processing.event.ResourceID;
 import io.javaoperatorsdk.operator.support.TestExecutionInfoProvider;
 
 @ControllerConfiguration(
@@ -17,6 +21,9 @@ import io.javaoperatorsdk.operator.support.TestExecutionInfoProvider;
         type = ConfigMapDependentResource.class))
 public class InformerRelatedBehaviorTestReconciler
     implements Reconciler<InformerRelatedBehaviorTestCustomResource>, TestExecutionInfoProvider {
+
+  private static final Logger log =
+      LoggerFactory.getLogger(InformerRelatedBehaviorTestReconciler.class);
 
   public static final String INFORMER_RELATED_BEHAVIOR_TEST_RECONCILER =
       "InformerRelatedBehaviorTestReconciler";
@@ -30,6 +37,7 @@ public class InformerRelatedBehaviorTestReconciler
       InformerRelatedBehaviorTestCustomResource resource,
       Context<InformerRelatedBehaviorTestCustomResource> context) {
     numberOfExecutions.addAndGet(1);
+    log.info("Reconciled for: {}", ResourceID.fromResource(resource));
     return UpdateControl.noUpdate();
   }
 
