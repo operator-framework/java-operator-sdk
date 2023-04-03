afb8f86a9ef2a2ce578dbdaf8f8e81fd21d4c091
csviri <csviri@gmail.com>
2023-04-03 12:33:30 +0200

    no line in github link

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/event/source/PrimaryToSecondaryMapper.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/event/source/PrimaryToSecondaryMapper.java
@@ -9,7 +9,7 @@ import io.javaoperatorsdk.operator.processing.event.ResourceID;
  * Primary to Secondary mapper only needed in some cases, typically when there is many-to-one or
  * many-to-many relation between primary and secondary resources. If there is owner reference (or
  * reference with annotations) from secondary to primary this is not needed. See <a href=
- * "https://github.com/java-operator-sdk/java-operator-sdk/blob/main/operator-framework/src/test/java/io/javaoperatorsdk/operator/sample/primarytosecondary/JobReconciler.java#L45-L45">Reconciler
+ * "https://github.com/java-operator-sdk/java-operator-sdk/blob/main/operator-framework/src/test/java/io/javaoperatorsdk/operator/sample/primarytosecondary/JobReconciler.java">Reconciler
  * in PrimaryToSecondaryIT</a> integration tests that handles many-to-many relationship. In other
  * words it is needed when the secondary is not referencing the primary, but other way around. The
  * issue is with a use case when a primary resource references a secondary resource, and the primary
