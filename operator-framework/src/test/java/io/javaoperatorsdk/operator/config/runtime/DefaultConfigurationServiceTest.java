0dbc048bcc1ba8c43dcb491a5253631c3e95f290
csviri <csviri@gmail.com>
2023-04-14 10:58:54 +0200

    chore: log4j 2.20.0

--- operator-framework/src/test/java/io/javaoperatorsdk/operator/config/runtime/DefaultConfigurationServiceTest.java
+++ operator-framework/src/test/java/io/javaoperatorsdk/operator/config/runtime/DefaultConfigurationServiceTest.java
@@ -1,11 +1,5 @@
 package io.javaoperatorsdk.operator.config.runtime;
 
-import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.core.LoggerContext;
-import org.apache.logging.log4j.core.config.AppenderRef;
-import org.apache.logging.log4j.core.config.LoggerConfig;
-import org.apache.logging.log4j.core.layout.PatternLayout;
-import org.apache.logging.log4j.test.appender.ListAppender;
 import org.junit.jupiter.api.Test;
 
 import io.fabric8.kubernetes.client.CustomResource;
@@ -17,59 +11,12 @@ import io.javaoperatorsdk.operator.api.reconciler.ControllerConfiguration;
 import io.javaoperatorsdk.operator.api.reconciler.Reconciler;
 import io.javaoperatorsdk.operator.api.reconciler.UpdateControl;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.*;
 
 class DefaultConfigurationServiceTest {
 
   public static final String CUSTOM_FINALIZER_NAME = "a.custom/finalizer";
   final DefaultConfigurationService configurationService = DefaultConfigurationService.instance();
-
-  @Test
-  void attemptingToRetrieveAnUnknownControllerShouldLogWarning() {
-    final LoggerContext context = LoggerContext.getContext(false);
-    final PatternLayout layout = PatternLayout.createDefaultLayout(context.getConfiguration());
-    final ListAppender appender = new ListAppender("list", null, layout, false, false);
-
-    appender.start();
-
-    context.getConfiguration().addAppender(appender);
-
-    AppenderRef ref = AppenderRef.createAppenderRef("list", null, null);
-    final var loggerName = configurationService.getLoggerName();
-    LoggerConfig loggerConfig =
-        LoggerConfig.createLogger(
-            false,
-            Level.valueOf("info"),
-            loggerName,
-            "false",
-            new AppenderRef[] {ref},
-            null,
-            context.getConfiguration(),
-            null);
-    loggerConfig.addAppender(appender, null, null);
-
-    context.getConfiguration().addLogger(loggerName, loggerConfig);
-    context.updateLoggers();
-
-    try {
-      final var config =
-          configurationService
-              .getConfigurationFor(new NotAutomaticallyCreated(), false);
-
-      assertThat(config).isNull();
-      assertThat(appender.getMessages())
-          .hasSize(1)
-          .allMatch(m -> m.contains(NotAutomaticallyCreated.NAME) && m.contains("not found"));
-    } finally {
-      appender.stop();
-
-      context.getConfiguration().removeLogger(loggerName);
-      context.updateLoggers();
-    }
-  }
 
   @Test
   void returnsValuesFromControllerAnnotationFinalizer() {
