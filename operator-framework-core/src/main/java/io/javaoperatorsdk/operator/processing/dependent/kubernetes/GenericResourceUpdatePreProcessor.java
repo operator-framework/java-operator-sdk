e95f9c8a8b8a8561c9a735e60fc5d82b7758df8e
Attila Mészáros <csviri@gmail.com>
2023-06-16 12:31:54 +0200

    fix: remove dead code (#1943)

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/dependent/kubernetes/GenericResourceUpdatePreProcessor.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/dependent/kubernetes/GenericResourceUpdatePreProcessor.java
@@ -1,46 +1,12 @@
 package io.javaoperatorsdk.operator.processing.dependent.kubernetes;
 
-import io.fabric8.kubernetes.api.model.ConfigMap;
 import io.fabric8.kubernetes.api.model.HasMetadata;
-import io.fabric8.kubernetes.api.model.Secret;
-import io.javaoperatorsdk.operator.ReconcilerUtils;
 import io.javaoperatorsdk.operator.api.reconciler.Context;
 
 public abstract class GenericResourceUpdatePreProcessor<R extends HasMetadata> implements
     ResourceUpdatePreProcessor<R> {
 
   private GenericResourceUpdatePreProcessor() {}
-
-  @SuppressWarnings("unchecked")
-  public static <R extends HasMetadata> ResourceUpdatePreProcessor<R> processorFor(
-      Class<R> resourceType) {
-    if (Secret.class.isAssignableFrom(resourceType)) {
-      return (ResourceUpdatePreProcessor<R>) new GenericResourceUpdatePreProcessor<Secret>() {
-        @Override
-        protected void updateClonedActual(Secret actual, Secret desired) {
-          actual.setData(desired.getData());
-          actual.setStringData(desired.getStringData());
-        }
-      };
-    } else if (ConfigMap.class.isAssignableFrom(resourceType)) {
-      return (ResourceUpdatePreProcessor<R>) new GenericResourceUpdatePreProcessor<ConfigMap>() {
-
-        @Override
-        protected void updateClonedActual(ConfigMap actual, ConfigMap desired) {
-          actual.setData(desired.getData());
-          actual.setBinaryData((desired.getBinaryData()));
-        }
-      };
-    } else {
-      return new GenericResourceUpdatePreProcessor<>() {
-        @Override
-        protected void updateClonedActual(R actual, R desired) {
-          var desiredSpec = ReconcilerUtils.getSpec(desired);
-          ReconcilerUtils.setSpec(actual, desiredSpec);
-        }
-      };
-    }
-  }
 
   public R replaceSpecOnActual(R actual, R desired, Context<?> context) {
     var clonedActual = context.getControllerConfiguration().getConfigurationService()
