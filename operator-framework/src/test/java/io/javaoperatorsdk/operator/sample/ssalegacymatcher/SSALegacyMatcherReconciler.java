4df205bc455c3d9fa7f724ea052a399402c5e7b7
Attila Mészáros <csviri@gmail.com>
2023-06-23 16:49:54 +0200

    feat: showcase sample for matcher with ssa in DR (#1964)
    * feat: shocase sample for matcher with ssa in DR
    
    * docs update

---
+++ operator-framework/src/test/java/io/javaoperatorsdk/operator/sample/ssalegacymatcher/SSALegacyMatcherReconciler.java
@@ -0,0 +1,28 @@
+package io.javaoperatorsdk.operator.sample.ssalegacymatcher;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import io.javaoperatorsdk.operator.api.reconciler.Context;
+import io.javaoperatorsdk.operator.api.reconciler.ControllerConfiguration;
+import io.javaoperatorsdk.operator.api.reconciler.Reconciler;
+import io.javaoperatorsdk.operator.api.reconciler.UpdateControl;
+import io.javaoperatorsdk.operator.api.reconciler.dependent.Dependent;
+
+@ControllerConfiguration(dependents = {@Dependent(type = ServiceDependentResource.class)})
+public class SSALegacyMatcherReconciler
+    implements Reconciler<SSALegacyMatcherCustomResource> {
+
+  private final AtomicInteger numberOfExecutions = new AtomicInteger(0);
+
+  @Override
+  public UpdateControl<SSALegacyMatcherCustomResource> reconcile(
+      SSALegacyMatcherCustomResource resource,
+      Context<SSALegacyMatcherCustomResource> context) {
+    numberOfExecutions.addAndGet(1);
+    return UpdateControl.noUpdate();
+  }
+
+  public int getNumberOfExecutions() {
+    return numberOfExecutions.get();
+  }
+}
---
+++ operator-framework/src/test/java/io/javaoperatorsdk/operator/sample/ssalegacymatcher/SSALegacyMatcherSpec.java
@@ -0,0 +1,15 @@
+package io.javaoperatorsdk.operator.sample.ssalegacymatcher;
+
+public class SSALegacyMatcherSpec {
+
+  private String value;
+
+  public String getValue() {
+    return value;
+  }
+
+  public SSALegacyMatcherSpec setValue(String value) {
+    this.value = value;
+    return this;
+  }
+}
