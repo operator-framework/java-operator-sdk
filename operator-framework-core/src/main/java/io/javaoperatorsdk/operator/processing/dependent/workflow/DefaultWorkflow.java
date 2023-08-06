17353876f881b6ff4b3dd5c7771c9184f9d50571
yarinkos <yarinkos@gmail.com>
2023-08-06 21:05:21 +0300

    feat(): validate toplevel size

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/dependent/workflow/DefaultWorkflow.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/dependent/workflow/DefaultWorkflow.java
@@ -79,6 +79,9 @@ public class DefaultWorkflow<P extends HasMetadata> implements Workflow<P> {
       }
       map.put(node.getName(), node);
     }
+    if (topLevelResources.size() <= 0 ){
+      throw new IllegalStateException("Top level dependent resources set size must be bigger than 0 .");
+    }
     return map;
   }
 
