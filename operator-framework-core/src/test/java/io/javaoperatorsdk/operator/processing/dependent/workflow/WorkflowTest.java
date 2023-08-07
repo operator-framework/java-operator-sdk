f5e68598e989f129dca9d54711b486523322b7cb
yarinkos <yarinkos@gmail.com>
2023-08-07 14:36:20 +0300

    Update WorkflowTest.java

--- operator-framework-core/src/test/java/io/javaoperatorsdk/operator/processing/dependent/workflow/WorkflowTest.java
+++ operator-framework-core/src/test/java/io/javaoperatorsdk/operator/processing/dependent/workflow/WorkflowTest.java
@@ -14,6 +14,7 @@ import io.javaoperatorsdk.operator.processing.dependent.kubernetes.KubernetesDep
 import io.javaoperatorsdk.operator.sample.simple.TestCustomResource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
@@ -23,6 +24,22 @@ import static org.mockito.Mockito.withSettings;
 class WorkflowTest {
 
   ExecutorService executorService = Executors.newCachedThreadPool();
+
+  @Test
+  void zeroTopLevelDRShouldThrowException() {
+    var dr1 = mock(DependentResource.class);
+    var dr2 = mock(DependentResource.class);
+    var dr3 = mock(DependentResource.class);
+
+    var cyclicWorkflowBuilderSetup = new WorkflowBuilder<TestCustomResource>()
+        .addDependentResource(dr1).dependsOn()
+        .addDependentResource(dr2).dependsOn(dr1)
+        .addDependentResource(dr3).dependsOn(dr2)
+        .addDependentResource(dr1).dependsOn(dr2);
+
+    assertThrows(IllegalStateException.class,
+        cyclicWorkflowBuilderSetup::build);
+  }
 
   @Test
   void calculatesTopLevelResources() {
