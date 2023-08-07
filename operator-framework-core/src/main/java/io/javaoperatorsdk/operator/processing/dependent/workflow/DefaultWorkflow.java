d2fdf384fd8933b3dfef986b08f5f6cf61b65da2
yarinkos <yarinkos@gmail.com>
2023-08-07 17:55:51 +0300

    fix: standalone workflows now fail on top-level cycles (#1997)

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/dependent/workflow/DefaultWorkflow.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/dependent/workflow/DefaultWorkflow.java
@@ -79,6 +79,10 @@ public class DefaultWorkflow<P extends HasMetadata> implements Workflow<P> {
       }
       map.put(node.getName(), node);
     }
+    if (topLevelResources.size() == 0) {
+      throw new IllegalStateException(
+          "No top-level dependent resources found. This might indicate a cyclic Set of DependentResourceNode has been provided.");
+    }
     return map;
   }
 
