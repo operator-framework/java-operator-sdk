ceb1cd95ca0f1e84f7a5e55dee3c185ca6bfeef9
Attila Mészáros <csviri@gmail.com>
2023-08-08 09:34:00 +0200

    feat: create resource only if not exists (#2001)
    Signed-off-by: csviri <csviri@gmail.com>
    Signed-off-by: Attila Mészáros <csviri@gmail.com>

---
+++ operator-framework/src/test/java/io/javaoperatorsdk/operator/CreateOnlyIfNotExistingDependentWithSSA.java
@@ -0,0 +1,57 @@
+package io.javaoperatorsdk.operator;
+
+import java.time.Duration;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.fabric8.kubernetes.api.model.ConfigMap;
+import io.fabric8.kubernetes.api.model.ConfigMapBuilder;
+import io.fabric8.kubernetes.api.model.ObjectMetaBuilder;
+import io.javaoperatorsdk.operator.junit.LocallyRunOperatorExtension;
+import io.javaoperatorsdk.operator.sample.createonlyifnotexistsdependentwithssa.CreateOnlyIfNotExistingDependentWithSSACustomResource;
+import io.javaoperatorsdk.operator.sample.createonlyifnotexistsdependentwithssa.CreateOnlyIfNotExistingDependentWithSSAReconciler;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
+
+class CreateOnlyIfNotExistingDependentWithSSA {
+
+  public static final String TEST_RESOURCE_NAME = "test1";
+  public static final String KEY = "key";
+
+  @RegisterExtension
+  LocallyRunOperatorExtension extension =
+      LocallyRunOperatorExtension.builder()
+          .withReconciler(new CreateOnlyIfNotExistingDependentWithSSAReconciler())
+          .build();
+
+
+  @Test
+  void createsResourceOnlyIfNotExisting() {
+    var cm = new ConfigMapBuilder().withMetadata(new ObjectMetaBuilder()
+        .withName(TEST_RESOURCE_NAME)
+        .build())
+        .withData(Map.of(KEY, "val"))
+        .build();
+
+    extension.create(cm);
+    extension.create(testResource());
+
+    await().pollDelay(Duration.ofMillis(200)).untilAsserted(() -> {
+      var currentCM = extension.get(ConfigMap.class, TEST_RESOURCE_NAME);
+      assertThat(currentCM.getData()).containsKey(KEY);
+    });
+  }
+
+  CreateOnlyIfNotExistingDependentWithSSACustomResource testResource() {
+    var res = new CreateOnlyIfNotExistingDependentWithSSACustomResource();
+    res.setMetadata(new ObjectMetaBuilder()
+        .withName(TEST_RESOURCE_NAME)
+        .build());
+
+    return res;
+  }
+
+}
