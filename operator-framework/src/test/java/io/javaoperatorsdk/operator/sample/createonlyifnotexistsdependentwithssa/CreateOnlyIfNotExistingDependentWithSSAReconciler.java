4110c8b8b0b9aed91e7fd1200efa76ff3daf781f
Attila Mészáros <csviri@gmail.com>
2023-08-08 09:34:00 +0200

    feat: create resource only if not exists (#2001)

---
+++ operator-framework/src/test/java/io/javaoperatorsdk/operator/sample/createonlyifnotexistsdependentwithssa/CreateOnlyIfNotExistingDependentWithSSAReconciler.java
@@ -0,0 +1,32 @@
+package io.javaoperatorsdk.operator.sample.createonlyifnotexistsdependentwithssa;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import io.javaoperatorsdk.operator.api.reconciler.Context;
+import io.javaoperatorsdk.operator.api.reconciler.ControllerConfiguration;
+import io.javaoperatorsdk.operator.api.reconciler.Reconciler;
+import io.javaoperatorsdk.operator.api.reconciler.UpdateControl;
+import io.javaoperatorsdk.operator.api.reconciler.dependent.Dependent;
+
+@ControllerConfiguration(dependents = {
+    @Dependent(type = ConfigMapDependentResource.class)})
+public class CreateOnlyIfNotExistingDependentWithSSAReconciler
+    implements Reconciler<CreateOnlyIfNotExistingDependentWithSSACustomResource> {
+
+  private final AtomicInteger numberOfExecutions = new AtomicInteger(0);
+
+  @Override
+  public UpdateControl<CreateOnlyIfNotExistingDependentWithSSACustomResource> reconcile(
+      CreateOnlyIfNotExistingDependentWithSSACustomResource resource,
+      Context<CreateOnlyIfNotExistingDependentWithSSACustomResource> context) {
+    numberOfExecutions.addAndGet(1);
+    return UpdateControl.noUpdate();
+  }
+
+  public int getNumberOfExecutions() {
+    return numberOfExecutions.get();
+  }
+
+
+
+}
