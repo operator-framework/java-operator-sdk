d44a53c69809747786cb9b2c1ba6e12b05cd12d7
csviri <csviri@gmail.com>
2023-08-23 12:51:23 +0200

    improve: dependent resource can be re-initialized

---
+++ operator-framework/src/test/java/io/javaoperatorsdk/operator/DependentReInitializationIT.java
@@ -0,0 +1,13 @@
+package io.javaoperatorsdk.operator;
+
+import org.junit.jupiter.api.Test;
+
+public class DependentReInitializationIT {
+
+
+    @Test
+    void dependentCanDeReInitialized() {
+        
+    }
+
+}
