5951f66f4f69482fc63255934165a1e950ced740
csviri <csviri@gmail.com>
2023-08-29 14:16:52 +0200

    reproduce ssa issue

---
+++ operator-framework/src/test/java/io/javaoperatorsdk/operator/sample/ssastatefulsetmatcherissue/StatefulSetDependentResource.java
@@ -0,0 +1,27 @@
+package io.javaoperatorsdk.operator.sample.ssastatefulsetmatcherissue;
+
+import io.fabric8.kubernetes.api.model.ObjectMetaBuilder;
+import io.fabric8.kubernetes.api.model.apps.StatefulSet;
+import io.javaoperatorsdk.operator.ReconcilerUtils;
+import io.javaoperatorsdk.operator.api.reconciler.Context;
+import io.javaoperatorsdk.operator.processing.dependent.kubernetes.CRUDKubernetesDependentResource;
+
+public class StatefulSetDependentResource
+    extends CRUDKubernetesDependentResource<StatefulSet, SSAStatefulSetMatcherIssueCustomResource> {
+
+  public StatefulSetDependentResource() {
+
+    super(StatefulSet.class);
+  }
+
+  @Override
+  protected StatefulSet desired(SSAStatefulSetMatcherIssueCustomResource primary,
+      Context<SSAStatefulSetMatcherIssueCustomResource> context) {
+    var template = ReconcilerUtils.loadYaml(StatefulSet.class, getClass(), "statefulset.yaml");
+    template.setMetadata(new ObjectMetaBuilder()
+        .withName(primary.getMetadata().getName())
+        .withNamespace(primary.getMetadata().getNamespace())
+        .build());
+    return template;
+  }
+}
