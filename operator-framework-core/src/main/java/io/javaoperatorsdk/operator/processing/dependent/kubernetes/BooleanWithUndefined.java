a3e2c2487a083caae536963f42828a32f322882d
Attila Mészáros <csviri@gmail.com>
2023-09-08 10:51:08 +0200

    feat: configurable SSA per Dependent Resource (#2045)
    Signed-off-by: Attila Mészáros <csviri@gmail.com>

---
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/dependent/kubernetes/BooleanWithUndefined.java
@@ -0,0 +1,19 @@
+package io.javaoperatorsdk.operator.processing.dependent.kubernetes;
+
+/**
+ * A replacement for {@link Boolean}, which can't be used in annotations.
+ */
+public enum BooleanWithUndefined {
+  TRUE, FALSE, UNDEFINED;
+
+  Boolean asBoolean() {
+    switch (this) {
+      case TRUE:
+        return Boolean.TRUE;
+      case FALSE:
+        return Boolean.FALSE;
+      default:
+        return null;
+    }
+  }
+}
--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/dependent/kubernetes/KubernetesDependent.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/dependent/kubernetes/KubernetesDependent.java
@@ -25,7 +25,7 @@ public @interface KubernetesDependent {
    * namespace is specified then the controller will monitor the namespaces configured for the
    * controller.
    *
-   * @return the list of namespaces this controller monitors
+   * @return the array of namespaces this controller monitors
    */
   String[] namespaces() default {Constants.SAME_AS_CONTROLLER};
 
@@ -76,4 +76,16 @@ public @interface KubernetesDependent {
    * Creates the resource only if did not exist before, this applies only if SSA is used.
    */
   boolean createResourceOnlyIfNotExistingWithSSA() default KubernetesDependentResourceConfig.DEFAULT_CREATE_RESOURCE_ONLY_IF_NOT_EXISTING_WITH_SSA;
+
+  /**
+   * Determines whether to use SSA (Server-Side Apply) for this dependent. If SSA is used, the
+   * dependent resource will only be created if it did not exist before. Default value is
+   * {@link BooleanWithUndefined#UNDEFINED}, which specifies that the behavior with respect to SSA
+   * is inherited from the global configuration.
+   *
+   * @return {@code true} if SSA is enabled, {@code false} if SSA is disabled,
+   *         {@link BooleanWithUndefined#UNDEFINED} if the SSA behavior should be inherited from the
+   *         global configuration
+   */
+  BooleanWithUndefined useSSA() default BooleanWithUndefined.UNDEFINED;
 }
