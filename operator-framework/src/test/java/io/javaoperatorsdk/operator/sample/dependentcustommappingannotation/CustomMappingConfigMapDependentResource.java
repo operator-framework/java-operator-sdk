2016f8eda60a6af96409f5a3eb676e679fafc15f
Attila Mészáros <csviri@gmail.com>
2023-10-03 11:38:39 +0200

    progress
    Signed-off-by: Attila Mészáros <csviri@gmail.com>

---
+++ operator-framework/src/test/java/io/javaoperatorsdk/operator/sample/dependentcustommappingannotation/CustomMappingConfigMapDependentResource.java
@@ -0,0 +1,42 @@
+package io.javaoperatorsdk.operator.sample.dependentcustommappingannotation;
+
+import io.fabric8.kubernetes.api.model.ConfigMap;
+import io.fabric8.kubernetes.api.model.ConfigMapBuilder;
+import io.fabric8.kubernetes.api.model.ObjectMetaBuilder;
+import io.javaoperatorsdk.operator.api.reconciler.Context;
+import io.javaoperatorsdk.operator.processing.dependent.kubernetes.CRUDKubernetesDependentResource;
+import io.javaoperatorsdk.operator.processing.dependent.kubernetes.CRUDNoGCKubernetesDependentResource;
+import io.javaoperatorsdk.operator.processing.event.ResourceID;
+import io.javaoperatorsdk.operator.processing.event.source.SecondaryToPrimaryMapper;
+import io.javaoperatorsdk.operator.processing.event.source.informer.Mappers;
+
+import java.util.Map;
+import java.util.Set;
+
+public class CustomMappingConfigMapDependentResource
+    extends CRUDNoGCKubernetesDependentResource<ConfigMap, DependentCustomMappingCustomResource>
+        implements SecondaryToPrimaryMapper<ConfigMap> {
+
+  private SecondaryToPrimaryMapper<ConfigMap> mapper = Mappers.fromAnnotation("customNameKey","customNamespaceKey");
+
+  public CustomMappingConfigMapDependentResource() {
+    super(ConfigMap.class);
+  }
+
+  @Override
+  protected ConfigMap desired(DependentCustomMappingCustomResource primary,
+                              Context<DependentCustomMappingCustomResource> context) {
+    return new ConfigMapBuilder()
+        .withMetadata(new ObjectMetaBuilder()
+            .withName(primary.getMetadata().getName())
+            .withNamespace(primary.getMetadata().getNamespace())
+            .build())
+        .withData(Map.of("key", "val"))
+        .build();
+  }
+
+  @Override
+  public Set<ResourceID> toPrimaryResourceIDs(ConfigMap resource) {
+    return mapper.toPrimaryResourceIDs(resource);
+  }
+}
