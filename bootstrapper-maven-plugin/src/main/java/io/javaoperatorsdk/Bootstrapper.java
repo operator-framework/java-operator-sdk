9dab736b32d0ed47960e3e7e1a1e7ea69edfb233
csviri <csviri@gmail.com>
2023-10-05 22:12:10 +0200

    feat: project generator maven plugin
    Signed-off-by: Attila Mészáros <csviri@gmail.com>

---
+++ bootstrapper-maven-plugin/src/main/java/io/javaoperatorsdk/Bootstrapper.java
@@ -0,0 +1,12 @@
+package io.javaoperatorsdk;
+
+import java.io.File;
+import java.nio.file.Path;
+
+public class Bootstrapper {
+
+    public void create(File targetDir, String groupId, String artifactId, String version) {
+
+    }
+
+}
---
+++ bootstrapper-maven-plugin/src/main/java/io/javaoperatorsdk/BootstrapperMojo.java
@@ -0,0 +1,60 @@
+package io.javaoperatorsdk;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Mojo;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+@Mojo(name = "create")
+public class BootstrapperMojo
+    extends AbstractMojo
+{
+    /**
+     * Location of the file.
+     * @parameter expression="${project.build.directory}"
+     * @required
+     */
+    private File outputDirectory;
+
+    public void execute()
+        throws MojoExecutionException
+    {
+        File f = outputDirectory;
+
+        if ( !f.exists() )
+        {
+            f.mkdirs();
+        }
+
+        File touch = new File( f, "touch.txt" );
+
+        FileWriter w = null;
+        try
+        {
+            w = new FileWriter( touch );
+
+            w.write( "touch.txt" );
+        }
+        catch ( IOException e )
+        {
+            throw new MojoExecutionException( "Error creating file " + touch, e );
+        }
+        finally
+        {
+            if ( w != null )
+            {
+                try
+                {
+                    w.close();
+                }
+                catch ( IOException e )
+                {
+                    // ignore
+                }
+            }
+        }
+    }
+}
