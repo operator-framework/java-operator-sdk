3900d23f3e896d9ccd77853fa6952de2ef96e035
Attila Mészáros <csviri@gmail.com>
2023-10-10 13:15:51 +0200

    testing if generated project compiles
    Signed-off-by: Attila Mészáros <csviri@gmail.com>

--- bootstrapper-maven-plugin/src/test/java/io/javaoperatorsdk/bootstrapper/BootstrapperTest.java
+++ bootstrapper-maven-plugin/src/test/java/io/javaoperatorsdk/bootstrapper/BootstrapperTest.java
@@ -1,14 +1,25 @@
 package io.javaoperatorsdk.bootstrapper;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.PosixFilePermission;
+import java.util.Set;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import io.javaoperatorsdk.boostrapper.Bootstrapper;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class BootstrapperTest {
+
+  private static final Logger log = LoggerFactory.getLogger(BootstrapperTest.class);
 
   Bootstrapper bootstrapper = new Bootstrapper();
 
@@ -18,6 +29,30 @@ class BootstrapperTest {
 
     var targetDir = new File("target", "test-project");
     assertThat(targetDir.list()).contains("pom.xml");
+    assertProjectCompiles();
   }
 
+  private void assertProjectCompiles() {
+    try {
+      var mvnwPath = "./target/test-project/mvnw";
+
+      Files.setPosixFilePermissions(Path.of(mvnwPath), Set.of(PosixFilePermission.OWNER_READ,
+          PosixFilePermission.OWNER_EXECUTE));
+      var process = Runtime.getRuntime()
+          .exec(mvnwPath + " clean install -f target/test-project/pom.xml");
+
+      BufferedReader stdError = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+      log.info("Maven output:");
+      String logLine;
+      while ((logLine = stdError.readLine()) != null) {
+        System.out.println(logLine);
+      }
+      var res = process.waitFor();
+      log.info("exit code: {}", res);
+      assertThat(res).isZero();
+    } catch (IOException | InterruptedException e) {
+      throw new RuntimeException(e);
+    }
+  }
 }
