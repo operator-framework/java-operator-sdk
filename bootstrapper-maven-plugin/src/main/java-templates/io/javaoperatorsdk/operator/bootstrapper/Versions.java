9485c0ca3e29d0683545935ce5479a845ddcf4d4
Attila Mészáros <csviri@gmail.com>
2023-10-12 13:57:45 +0200

    Pure Java Project Generator (#2083)
    Signed-off-by: Attila Mészáros <csviri@gmail.com>

---
+++ bootstrapper-maven-plugin/src/main/java-templates/io/javaoperatorsdk/operator/bootstrapper/Versions.java
@@ -0,0 +1,10 @@
+package io.javaoperatorsdk.bootstrapper;
+
+public final class Versions {
+
+  private Versions() {}
+
+  public static final String JOSDK = "${project.version}";
+  public static final String KUBERNETES_CLIENT = "${fabric8-client.version}";
+
+}
