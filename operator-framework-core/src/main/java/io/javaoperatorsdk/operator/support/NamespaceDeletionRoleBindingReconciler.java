4a41c88aac57475b6b13a9ff917713b5d6352994
Attila Mészáros <csviri@gmail.com>
2023-12-12 17:09:08 +0100

    wip
    Signed-off-by: Attila Mészáros <csviri@gmail.com>

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/support/NamespaceDeletionRoleBindingReconciler.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/support/NamespaceDeletionRoleBindingReconciler.java
@@ -4,7 +4,8 @@ import io.fabric8.kubernetes.api.model.rbac.RoleBinding;
 import io.javaoperatorsdk.operator.api.reconciler.*;
 
 @ControllerConfiguration(labelSelector = "")
-public class NamespaceDeletionRoleBindingReconciler implements Reconciler<RoleBinding>, Cleaner<RoleBinding> {
+public class NamespaceDeletionRoleBindingReconciler
+    implements Reconciler<RoleBinding>, Cleaner<RoleBinding> {
 
   @Override
   public UpdateControl<RoleBinding> reconcile(RoleBinding resource, Context<RoleBinding> context)
--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/support/NamespaceDeletionRoleReconciler.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/support/NamespaceDeletionRoleReconciler.java
@@ -1,27 +1,30 @@
 package io.javaoperatorsdk.operator.support;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import io.fabric8.kubernetes.api.model.HasMetadata;
 import io.fabric8.kubernetes.api.model.rbac.Role;
 import io.javaoperatorsdk.operator.api.config.informer.InformerConfiguration;
 import io.javaoperatorsdk.operator.api.reconciler.*;
-import io.javaoperatorsdk.operator.processing.event.ResourceID;
 import io.javaoperatorsdk.operator.processing.event.source.EventSource;
-import io.javaoperatorsdk.operator.processing.event.source.PrimaryToSecondaryMapper;
 import io.javaoperatorsdk.operator.processing.event.source.SecondaryToPrimaryMapper;
 import io.javaoperatorsdk.operator.processing.event.source.informer.InformerEventSource;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
+import static io.fabric8.kubernetes.api.model.HasMetadata.getPlural;
 
 @ControllerConfiguration
-public class NamespaceDeletionRoleReconciler implements Reconciler<Role>, Cleaner<Role>, EventSourceInitializer<Role> {
+public class NamespaceDeletionRoleReconciler
+    implements Reconciler<Role>, Cleaner<Role>, EventSourceInitializer<Role> {
 
-  List<Class<? extends HasMetadata>> resourceClasses;
+  final List<Class<? extends HasMetadata>> resourceClasses;
+  final Map<Class<? extends HasMetadata>, String> plurals;
 
   public NamespaceDeletionRoleReconciler(List<Class<? extends HasMetadata>> resourceClasses) {
     this.resourceClasses = resourceClasses;
+    this.plurals =
+        resourceClasses.stream().collect(Collectors.toMap(c -> c, HasMetadata::getPlural));
   }
 
   @Override
@@ -34,17 +37,22 @@ public class NamespaceDeletionRoleReconciler implements Reconciler<Role>, Cleane
     return null;
   }
 
-  @SuppressWarnings({"rawtypes","unchecked"})
+  @SuppressWarnings({"rawtypes", "unchecked"})
   @Override
   public Map<String, EventSource> prepareEventSources(EventSourceContext<Role> context) {
+    // var allPlurals = plurals.values();
+    // context.getPrimaryCache().addIndexer("namespace-target-resource-index", r-> {
+    // r.getRules().stream().map() // todo
+    //
+    // });
 
-    return  resourceClasses.stream().map(c-> new InformerEventSource(InformerConfiguration.from(c,context)
-            .withPrimaryToSecondaryMapper(primary -> {
-
-                 return null;
+    return resourceClasses.stream()
+        .map(c -> new InformerEventSource(InformerConfiguration.from(c, context)
+            .withSecondaryToPrimaryMapper((SecondaryToPrimaryMapper) resource -> {
+              return null;
             })
-            .withSecondaryToPrimaryMapper((SecondaryToPrimaryMapper) resource -> null)
-            .build(),context)).collect(Collectors.toMap(i->i.resourceType().getName(),i->i));
+            .build(), context))
+        .collect(Collectors.toMap(i -> plurals.get(i.resourceType()), i -> i));
 
   }
 }
