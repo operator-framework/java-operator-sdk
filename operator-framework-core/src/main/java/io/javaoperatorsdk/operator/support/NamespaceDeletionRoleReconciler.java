bbce6bc107afd3e03f94b60174c063bd0d3ab815
Attila Mészáros <csviri@gmail.com>
2023-12-13 13:38:56 +0100

    indexes
    Signed-off-by: Attila Mészáros <csviri@gmail.com>

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/support/NamespaceDeletionRoleReconciler.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/support/NamespaceDeletionRoleReconciler.java
@@ -1,22 +1,30 @@
 package io.javaoperatorsdk.operator.support;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import io.fabric8.kubernetes.api.model.HasMetadata;
 import io.fabric8.kubernetes.api.model.rbac.Role;
 import io.javaoperatorsdk.operator.api.config.informer.InformerConfiguration;
 import io.javaoperatorsdk.operator.api.reconciler.*;
+import io.javaoperatorsdk.operator.processing.event.ResourceID;
 import io.javaoperatorsdk.operator.processing.event.source.EventSource;
 import io.javaoperatorsdk.operator.processing.event.source.SecondaryToPrimaryMapper;
 import io.javaoperatorsdk.operator.processing.event.source.informer.InformerEventSource;
 
 import static io.fabric8.kubernetes.api.model.HasMetadata.getPlural;
 
+// todo filters
 @ControllerConfiguration
 public class NamespaceDeletionRoleReconciler
     implements Reconciler<Role>, Cleaner<Role>, EventSourceInitializer<Role> {
+
+  public static final String TARGET_RESOURCES_IN_NAMESPACE_TO_ROLE_INDEX = "target-resources-in-namespace";
+  public static final String RESOURCE_NAMESPACE_INDEX = "resource-namespace-index";
 
   final List<Class<? extends HasMetadata>> resourceClasses;
   final Map<Class<? extends HasMetadata>, String> plurals;
@@ -34,25 +42,46 @@ public class NamespaceDeletionRoleReconciler
 
   @Override
   public DeleteControl cleanup(Role resource, Context<Role> context) {
-    return null;
+      
+
+    return DeleteControl.defaultDelete();
   }
 
   @SuppressWarnings({"rawtypes", "unchecked"})
   @Override
   public Map<String, EventSource> prepareEventSources(EventSourceContext<Role> context) {
-    // var allPlurals = plurals.values();
-    // context.getPrimaryCache().addIndexer("namespace-target-resource-index", r-> {
-    // r.getRules().stream().map() // todo
-    //
-    // });
+     var allPlurals = plurals.values();
+     context.getPrimaryCache().addIndexer(TARGET_RESOURCES_IN_NAMESPACE_TO_ROLE_INDEX, r-> {
+       // resource-plural+namespace -> role (in that namespace)
+       List<String> result = new ArrayList<>();
+       r.getRules().forEach(rule->{
+         rule.getResources().forEach(resource-> {
+           if (allPlurals.contains(resource)) {
+              result.add(keyFor(r.getMetadata().getNamespace(),resource));
+           }
+         });
+       });
+       return result;
+     });
 
     return resourceClasses.stream()
-        .map(c -> new InformerEventSource(InformerConfiguration.from(c, context)
+        .map(c -> { var ies =  new InformerEventSource<HasMetadata,Role>(InformerConfiguration.from(c, context)
             .withSecondaryToPrimaryMapper((SecondaryToPrimaryMapper) resource -> {
-              return null;
+              HasMetadata rm = (HasMetadata) resource;
+              var roles = context.getPrimaryCache().byIndex(TARGET_RESOURCES_IN_NAMESPACE_TO_ROLE_INDEX,
+                      keyFor(rm.getMetadata().getNamespace(),rm.getPlural()));
+              return roles.stream().map(r->new ResourceID(r.getMetadata().getName(),
+                      r.getMetadata().getNamespace())).collect(Collectors.toSet());
             })
-            .build(), context))
+            .build(), context);
+          ies.addIndexer(RESOURCE_NAMESPACE_INDEX, r-> List.of(r.getMetadata().getNamespace()));
+          return ies;
+        })
         .collect(Collectors.toMap(i -> plurals.get(i.resourceType()), i -> i));
-
   }
+
+  public static String keyFor(String namespace, String resourcePlural) {
+     return resourcePlural+"-"+namespace;
+  }
+
 }
