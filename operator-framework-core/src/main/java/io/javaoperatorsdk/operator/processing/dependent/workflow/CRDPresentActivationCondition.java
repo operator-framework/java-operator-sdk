ac5139a9115cc829f50e565e6214c605cb269579
Attila Mészáros <csviri@gmail.com>
2024-02-13 14:31:09 +0100

    wip
    Signed-off-by: Attila Mészáros <csviri@gmail.com>

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/dependent/workflow/CRDPresentActivationCondition.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/dependent/workflow/CRDPresentActivationCondition.java
@@ -1,10 +1,16 @@
 package io.javaoperatorsdk.operator.processing.dependent.workflow;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import io.fabric8.kubernetes.api.model.HasMetadata;
 import io.fabric8.kubernetes.api.model.apiextensions.v1.CustomResourceDefinition;
+import io.fabric8.kubernetes.client.KubernetesClient;
 import io.javaoperatorsdk.operator.api.reconciler.Context;
 import io.javaoperatorsdk.operator.api.reconciler.dependent.DependentResource;
 import io.javaoperatorsdk.operator.processing.GroupVersionKind;
@@ -13,6 +19,13 @@ import io.javaoperatorsdk.operator.processing.event.source.informer.InformerEven
 public class CRDPresentActivationCondition implements Condition<HasMetadata, HasMetadata> {
 
   private static final Logger log = LoggerFactory.getLogger(CRDPresentActivationCondition.class);
+
+  private static final Map<GroupVersionKind, Boolean> crdPresenceCache = new ConcurrentHashMap<>();
+  private static final Map<GroupVersionKind, LocalDateTime> crdPresenceLastRefresh =
+      new ConcurrentHashMap<>();
+
+  // todo configurable, enhanced behavior (exp backoff etc)
+  private int cacheRefreshInterval = 5000;
 
   @Override
   public boolean isMet(DependentResource<HasMetadata, HasMetadata> dependentResource,
@@ -33,12 +46,26 @@ public class CRDPresentActivationCondition implements Condition<HasMetadata, Has
 
     if (crdInformer != null) {
       return crdInformer
-          .list(p -> p.getSpec().getNames().getKind().equals(kind)
-              && p.getSpec().getGroup().equals(gvk.getGroup()))
+          .list(crd -> crd.getSpec().getNames().getKind().equals(kind)
+              && crd.getSpec().getGroup().equals(gvk.getGroup()))
           .findAny().isPresent();
     } else {
-      // todo poll activation condition
+      if (crdPresenceCache.get(gvk) == null ||
+          crdPresenceLastRefresh.get(gvk)
+              .isBefore(LocalDateTime.now().minus(cacheRefreshInterval, ChronoUnit.MILLIS))) {
+        refreshCache(gvk, context.getClient());
+      }
+      return crdPresenceCache.get(gvk);
     }
-    return false;
   }
+
+  private void refreshCache(GroupVersionKind gvk, KubernetesClient client) {
+    boolean found = client.resources(CustomResourceDefinition.class).list().getItems()
+        .stream().anyMatch(crd -> crd.getSpec().getNames().getKind().equals(gvk.getKind())
+            && crd.getSpec().getGroup().equals(gvk.getGroup()));
+    crdPresenceCache.put(gvk, found);
+    crdPresenceLastRefresh.put(gvk, LocalDateTime.now());
+  }
+
+
 }
