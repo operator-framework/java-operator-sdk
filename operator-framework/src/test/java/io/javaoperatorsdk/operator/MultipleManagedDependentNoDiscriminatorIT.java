10cc13e90db4225b5572b06204971345d7814a77
Attila Mészáros <csviri@gmail.com>
2024-02-27 15:38:08 +0100

    Integration Test
    Signed-off-by: Attila Mészáros <csviri@gmail.com>

---
+++ operator-framework/src/test/java/io/javaoperatorsdk/operator/MultipleManagedDependentNoDiscriminatorIT.java
@@ -0,0 +1,79 @@
+package io.javaoperatorsdk.operator;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.fabric8.kubernetes.api.model.ConfigMap;
+import io.fabric8.kubernetes.api.model.ObjectMetaBuilder;
+import io.javaoperatorsdk.operator.junit.LocallyRunOperatorExtension;
+import io.javaoperatorsdk.operator.sample.multipledrsametypenodiscriminator.*;
+
+import static io.javaoperatorsdk.operator.sample.multipledrsametypenodiscriminator.MultipleManagedDependentSameTypeNoDiscriminatorReconciler.DATA_KEY;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
+
+public class MultipleManagedDependentNoDiscriminatorIT {
+
+  public static final String RESOURCE_NAME = "test1";
+  public static final String INITIAL_VALUE = "initial_value";
+  public static final String CHANGED_VALUE = "changed_value";
+
+  @RegisterExtension
+  LocallyRunOperatorExtension extension =
+      LocallyRunOperatorExtension.builder()
+          .withReconciler(new MultipleManagedDependentSameTypeNoDiscriminatorReconciler())
+          .build();
+
+  @Test
+  void handlesCRUDOperations() {
+    var res = extension.create(testResource());
+
+    await().untilAsserted(() -> {
+      var cm1 = extension.get(ConfigMap.class,
+          RESOURCE_NAME + MultipleManagedDependentNoDiscriminatorConfigMap1.NAME_SUFFIX);
+      var cm2 = extension.get(ConfigMap.class,
+          RESOURCE_NAME + MultipleManagedDependentNoDiscriminatorConfigMap2.NAME_SUFFIX);
+
+      assertThat(cm1).isNotNull();
+      assertThat(cm2).isNotNull();
+      assertThat(cm1.getData()).containsEntry(DATA_KEY, INITIAL_VALUE);
+      assertThat(cm2.getData()).containsEntry(DATA_KEY, INITIAL_VALUE);
+    });
+
+    res.getSpec().setValue(CHANGED_VALUE);
+    res = extension.replace(res);
+
+    await().untilAsserted(() -> {
+      var cm1 = extension.get(ConfigMap.class,
+          RESOURCE_NAME + MultipleManagedDependentNoDiscriminatorConfigMap1.NAME_SUFFIX);
+      var cm2 = extension.get(ConfigMap.class,
+          RESOURCE_NAME + MultipleManagedDependentNoDiscriminatorConfigMap2.NAME_SUFFIX);
+
+      assertThat(cm1.getData()).containsEntry(DATA_KEY, CHANGED_VALUE);
+      assertThat(cm2.getData()).containsEntry(DATA_KEY, CHANGED_VALUE);
+    });
+
+    extension.delete(res);
+
+    await().untilAsserted(() -> {
+      var cm1 = extension.get(ConfigMap.class,
+          RESOURCE_NAME + MultipleManagedDependentNoDiscriminatorConfigMap1.NAME_SUFFIX);
+      var cm2 = extension.get(ConfigMap.class,
+          RESOURCE_NAME + MultipleManagedDependentNoDiscriminatorConfigMap2.NAME_SUFFIX);
+
+      assertThat(cm1).isNull();
+      assertThat(cm2).isNull();
+    });
+  }
+
+  MultipleManagedDependentNoDiscriminatorCustomResource testResource() {
+    var res = new MultipleManagedDependentNoDiscriminatorCustomResource();
+    res.setMetadata(new ObjectMetaBuilder()
+        .withName(RESOURCE_NAME)
+        .build());
+    res.setSpec(new MultipleManagedDependentNoDiscriminatorSpec());
+    res.getSpec().setValue(INITIAL_VALUE);
+    return res;
+  }
+
+}
---
+++ operator-framework/src/test/java/io/javaoperatorsdk/operator/sample/multipledrsametypenodiscriminator/MultipleManagedDependentSameTypeNoDiscriminatorReconciler.java
@@ -0,0 +1,57 @@
+package io.javaoperatorsdk.operator.sample.multipledrsametypenodiscriminator;
+
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import io.fabric8.kubernetes.api.model.ConfigMap;
+import io.javaoperatorsdk.operator.api.config.informer.InformerConfiguration;
+import io.javaoperatorsdk.operator.api.reconciler.*;
+import io.javaoperatorsdk.operator.api.reconciler.dependent.Dependent;
+import io.javaoperatorsdk.operator.processing.event.source.EventSource;
+import io.javaoperatorsdk.operator.processing.event.source.informer.InformerEventSource;
+import io.javaoperatorsdk.operator.support.TestExecutionInfoProvider;
+
+import static io.javaoperatorsdk.operator.sample.multiplemanageddependentsametype.MultipleManagedDependentResourceReconciler.CONFIG_MAP_EVENT_SOURCE;
+
+@ControllerConfiguration(dependents = {
+    @Dependent(type = MultipleManagedDependentNoDiscriminatorConfigMap1.class,
+        useEventSourceWithName = CONFIG_MAP_EVENT_SOURCE),
+    @Dependent(type = MultipleManagedDependentNoDiscriminatorConfigMap2.class,
+        useEventSourceWithName = CONFIG_MAP_EVENT_SOURCE)
+})
+public class MultipleManagedDependentSameTypeNoDiscriminatorReconciler
+    implements Reconciler<MultipleManagedDependentNoDiscriminatorCustomResource>,
+    TestExecutionInfoProvider,
+    EventSourceInitializer<MultipleManagedDependentNoDiscriminatorCustomResource> {
+
+  public static final String CONFIG_MAP_EVENT_SOURCE = "ConfigMapEventSource";
+  public static final String DATA_KEY = "key";
+
+  private final AtomicInteger numberOfExecutions = new AtomicInteger(0);
+
+  public MultipleManagedDependentSameTypeNoDiscriminatorReconciler() {}
+
+  @Override
+  public UpdateControl<MultipleManagedDependentNoDiscriminatorCustomResource> reconcile(
+      MultipleManagedDependentNoDiscriminatorCustomResource resource,
+      Context<MultipleManagedDependentNoDiscriminatorCustomResource> context) {
+    numberOfExecutions.getAndIncrement();
+
+    return UpdateControl.noUpdate();
+  }
+
+
+  public int getNumberOfExecutions() {
+    return numberOfExecutions.get();
+  }
+
+  @Override
+  public Map<String, EventSource> prepareEventSources(
+      EventSourceContext<MultipleManagedDependentNoDiscriminatorCustomResource> context) {
+    InformerEventSource<ConfigMap, MultipleManagedDependentNoDiscriminatorCustomResource> ies =
+        new InformerEventSource<>(InformerConfiguration.from(ConfigMap.class, context)
+            .build(), context);
+
+    return Map.of(CONFIG_MAP_EVENT_SOURCE, ies);
+  }
+}
