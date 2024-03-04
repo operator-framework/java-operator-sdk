c0b61fb7a018818797d2afa57ba08960afb5bf6a
Attila Mészáros <csviri@gmail.com>
2024-03-04 09:56:45 +0100

    fix docs
    Signed-off-by: Attila Mészáros <csviri@gmail.com>

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/event/source/EventSource.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/event/source/EventSource.java
@@ -6,9 +6,8 @@ import io.javaoperatorsdk.operator.processing.LifecycleAware;
 import io.javaoperatorsdk.operator.processing.event.EventHandler;
 
 /**
- * Creates an event source to trigger your reconciler whenever something happens to a secondary or
- * external resource that would not normally trigger your reconciler (as the primary resources are
- * not changed). To register EventSources with so that your reconciler is triggered.
+ * Creates an event source to trigger your reconciler. EventSource is generalized concept of
+ * Informer to cover also external resources.
  */
 public interface EventSource extends LifecycleAware, EventSourceHealthIndicator {
 
