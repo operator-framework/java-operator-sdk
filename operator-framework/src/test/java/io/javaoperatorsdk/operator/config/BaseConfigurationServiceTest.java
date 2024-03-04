cc3d839a7625b25c7e4bcb134af3da76b8572399
Attila Mészáros <csviri@gmail.com>
2024-03-04 17:11:03 +0100

    feat: move name is directly to dependent resource (#2250)
    * feat: move name is directly to dependent resource
    
    - use this name when throwing aggregate exception
    
    Signed-off-by: Attila Mészáros <csviri@gmail.com>
    
    * refactor to use a dedicated interface for setting the name
    
    Signed-off-by: Attila Mészáros <csviri@gmail.com>
    
    * refactor: add default implementation for name() (#2255)
    
    Signed-off-by: Chris Laprun <claprun@redhat.com>
    Signed-off-by: Attila Mészáros <csviri@gmail.com>
    
    ---------
    
    Signed-off-by: Attila Mészáros <csviri@gmail.com>
    Signed-off-by: Chris Laprun <claprun@redhat.com>
    Co-authored-by: Chris Laprun <metacosm@users.noreply.github.com>
    Signed-off-by: Attila Mészáros <csviri@gmail.com>

--- operator-framework/src/test/java/io/javaoperatorsdk/operator/config/BaseConfigurationServiceTest.java
+++ operator-framework/src/test/java/io/javaoperatorsdk/operator/config/BaseConfigurationServiceTest.java
@@ -46,13 +46,7 @@ import io.javaoperatorsdk.operator.sample.dependentssa.DependentSSAReconciler;
 import io.javaoperatorsdk.operator.sample.readonly.ConfigMapReader;
 import io.javaoperatorsdk.operator.sample.readonly.ReadOnlyDependent;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertInstanceOf;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 class BaseConfigurationServiceTest {
 
