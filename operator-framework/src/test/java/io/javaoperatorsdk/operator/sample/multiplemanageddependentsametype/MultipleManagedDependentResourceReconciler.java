d3c299b060ec858355b0de022c8af30dde8a4fb7
Attila Mészáros <csviri@gmail.com>
2024-03-11 20:16:14 +0100

    feat: Workflow extracted to a separate annotation (#2274)
    Signed-off-by: Attila Mészáros <csviri@gmail.com>

--- operator-framework/src/test/java/io/javaoperatorsdk/operator/sample/multiplemanageddependentsametype/MultipleManagedDependentResourceReconciler.java
+++ operator-framework/src/test/java/io/javaoperatorsdk/operator/sample/multiplemanageddependentsametype/MultipleManagedDependentResourceReconciler.java
@@ -13,12 +13,13 @@ import io.javaoperatorsdk.operator.support.TestExecutionInfoProvider;
 
 import static io.javaoperatorsdk.operator.sample.multiplemanageddependentsametype.MultipleManagedDependentResourceReconciler.CONFIG_MAP_EVENT_SOURCE;
 
-@ControllerConfiguration(dependents = {
+@Workflow(dependents = {
     @Dependent(type = MultipleManagedDependentResourceConfigMap1.class,
         useEventSourceWithName = CONFIG_MAP_EVENT_SOURCE),
     @Dependent(type = MultipleManagedDependentResourceConfigMap2.class,
         useEventSourceWithName = CONFIG_MAP_EVENT_SOURCE)
 })
+@ControllerConfiguration
 public class MultipleManagedDependentResourceReconciler
     implements Reconciler<MultipleManagedDependentResourceCustomResource>,
     TestExecutionInfoProvider,
