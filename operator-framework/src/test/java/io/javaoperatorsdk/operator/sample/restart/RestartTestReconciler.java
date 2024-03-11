9b1310bd9b062f3c8ed35fea6170c77ed0afc027
Attila Mészáros <csviri@gmail.com>
2024-03-11 20:16:14 +0100

    feat: Workflow extracted to a separate annotation (#2274)
    Signed-off-by: Attila Mészáros <csviri@gmail.com>

--- operator-framework/src/test/java/io/javaoperatorsdk/operator/sample/restart/RestartTestReconciler.java
+++ operator-framework/src/test/java/io/javaoperatorsdk/operator/sample/restart/RestartTestReconciler.java
@@ -2,15 +2,12 @@ package io.javaoperatorsdk.operator.sample.restart;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
-import io.javaoperatorsdk.operator.api.reconciler.Context;
-import io.javaoperatorsdk.operator.api.reconciler.ControllerConfiguration;
-import io.javaoperatorsdk.operator.api.reconciler.Reconciler;
-import io.javaoperatorsdk.operator.api.reconciler.UpdateControl;
+import io.javaoperatorsdk.operator.api.reconciler.*;
 import io.javaoperatorsdk.operator.api.reconciler.dependent.Dependent;
 import io.javaoperatorsdk.operator.support.TestExecutionInfoProvider;
 
-@ControllerConfiguration(
-    dependents = @Dependent(type = ConfigMapDependentResource.class))
+@Workflow(dependents = @Dependent(type = ConfigMapDependentResource.class))
+@ControllerConfiguration
 public class RestartTestReconciler
     implements Reconciler<RestartTestCustomResource>, TestExecutionInfoProvider {
 
