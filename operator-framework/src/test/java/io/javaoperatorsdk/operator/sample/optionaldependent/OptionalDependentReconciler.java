daf226219b1e434b14daa70b2fb285ed4313afaa
Attila Mészáros <csviri@gmail.com>
2024-03-12 10:18:42 +0100

    rebase
    Signed-off-by: Attila Mészáros <csviri@gmail.com>

--- operator-framework/src/test/java/io/javaoperatorsdk/operator/sample/optionaldependent/OptionalDependentReconciler.java
+++ operator-framework/src/test/java/io/javaoperatorsdk/operator/sample/optionaldependent/OptionalDependentReconciler.java
@@ -3,12 +3,12 @@ package io.javaoperatorsdk.operator.sample.optionaldependent;
 import io.javaoperatorsdk.operator.api.reconciler.*;
 import io.javaoperatorsdk.operator.api.reconciler.dependent.Dependent;
 
+@Workflow(dependents = {
+    @Dependent(type = OptionalDependent.class, optional = true),
+})
 @ControllerConfiguration(
     generationAwareEventProcessing = false, // to easily trigger reconciliation with metadata update
-    namespaces = Constants.WATCH_CURRENT_NAMESPACE,
-    dependents = {
-        @Dependent(type = OptionalDependent.class, optional = true),
-    })
+    namespaces = Constants.WATCH_CURRENT_NAMESPACE)
 public class OptionalDependentReconciler
     implements Reconciler<OptionalDependentCustomResource> {
 
