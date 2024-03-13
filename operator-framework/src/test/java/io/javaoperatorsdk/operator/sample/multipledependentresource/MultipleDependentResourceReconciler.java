bbf0fbea4dadc0751fe0b31c30ea944f9be9e348
Attila Mészáros <csviri@gmail.com>
2024-03-13 17:11:44 +0100

    feat: distinguish resources based on desired state (#2252)
    Signed-off-by: Attila Mészáros <csviri@gmail.com>
    Signed-off-by: Attila Mészáros <csviri@gmail.com>

--- operator-framework/src/test/java/io/javaoperatorsdk/operator/sample/multipledependentresource/MultipleDependentResourceReconciler.java
+++ operator-framework/src/test/java/io/javaoperatorsdk/operator/sample/multipledependentresource/MultipleDependentResourceReconciler.java
@@ -1,58 +1,35 @@
 package io.javaoperatorsdk.operator.sample.multipledependentresource;
 
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import io.fabric8.kubernetes.api.model.ConfigMap;
 import io.javaoperatorsdk.operator.api.config.informer.InformerConfiguration;
 import io.javaoperatorsdk.operator.api.reconciler.*;
-import io.javaoperatorsdk.operator.processing.event.ResourceID;
 import io.javaoperatorsdk.operator.processing.event.source.EventSource;
 import io.javaoperatorsdk.operator.processing.event.source.informer.InformerEventSource;
-import io.javaoperatorsdk.operator.support.TestExecutionInfoProvider;
 
 @ControllerConfiguration
 public class MultipleDependentResourceReconciler
-    implements Reconciler<MultipleDependentResourceCustomResource>,
-    TestExecutionInfoProvider {
+    implements Reconciler<MultipleDependentResourceCustomResource> {
 
-  public static final int FIRST_CONFIG_MAP_ID = 1;
-  public static final int SECOND_CONFIG_MAP_ID = 2;
-  private final AtomicInteger numberOfExecutions = new AtomicInteger(0);
+  public static final String FIRST_CONFIG_MAP_ID = "1";
+  public static final String SECOND_CONFIG_MAP_ID = "2";
 
   private final MultipleDependentResourceConfigMap firstDependentResourceConfigMap;
   private final MultipleDependentResourceConfigMap secondDependentResourceConfigMap;
 
   public MultipleDependentResourceReconciler() {
     firstDependentResourceConfigMap = new MultipleDependentResourceConfigMap(FIRST_CONFIG_MAP_ID);
-
     secondDependentResourceConfigMap = new MultipleDependentResourceConfigMap(SECOND_CONFIG_MAP_ID);
-
-    firstDependentResourceConfigMap
-        .setResourceDiscriminator(
-            new ResourceIDMatcherDiscriminator<>(
-                p -> new ResourceID(p.getConfigMapName(FIRST_CONFIG_MAP_ID),
-                    p.getMetadata().getNamespace())));
-    secondDependentResourceConfigMap
-        .setResourceDiscriminator(
-            new ResourceIDMatcherDiscriminator<>(
-                p -> new ResourceID(p.getConfigMapName(SECOND_CONFIG_MAP_ID),
-                    p.getMetadata().getNamespace())));
   }
 
   @Override
   public UpdateControl<MultipleDependentResourceCustomResource> reconcile(
       MultipleDependentResourceCustomResource resource,
       Context<MultipleDependentResourceCustomResource> context) {
-    numberOfExecutions.getAndIncrement();
     firstDependentResourceConfigMap.reconcile(resource, context);
     secondDependentResourceConfigMap.reconcile(resource, context);
     return UpdateControl.noUpdate();
-  }
-
-
-  public int getNumberOfExecutions() {
-    return numberOfExecutions.get();
   }
 
   @Override
