d24d4ca5f62a7c0f1d64c043681b1e725638908e
Attila Mészáros <csviri@gmail.com>
2024-03-13 17:11:44 +0100

    feat: distinguish resources based on desired state (#2252)
    Signed-off-by: Attila Mészáros <csviri@gmail.com>
    Signed-off-by: Attila Mészáros <csviri@gmail.com>

---
+++ operator-framework/src/test/java/io/javaoperatorsdk/operator/sample/multipledependentresource/MultipleDependentResourceSpec.java
@@ -0,0 +1,14 @@
+package io.javaoperatorsdk.operator.sample.multipledependentresource;
+
+public class MultipleDependentResourceSpec {
+
+  private String value;
+
+  public String getValue() {
+    return value;
+  }
+
+  public void setValue(String value) {
+    this.value = value;
+  }
+}
