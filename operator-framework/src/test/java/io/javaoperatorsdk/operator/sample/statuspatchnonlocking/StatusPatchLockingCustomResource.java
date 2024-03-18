2c6609399f77e3cc9370c0e04db2d205481f0671
Attila Mészáros <csviri@gmail.com>
2024-03-18 14:32:26 +0100

    feat: use ssa for status update by default from `UpdateControl` (#2278)
    Signed-off-by: Attila Mészáros <csviri@gmail.com>

--- operator-framework/src/test/java/io/javaoperatorsdk/operator/sample/statuspatchnonlocking/StatusPatchLockingCustomResource.java
+++ operator-framework/src/test/java/io/javaoperatorsdk/operator/sample/statuspatchnonlocking/StatusPatchLockingCustomResource.java
@@ -3,14 +3,12 @@ package io.javaoperatorsdk.operator.sample.statuspatchnonlocking;
 import io.fabric8.kubernetes.api.model.Namespaced;
 import io.fabric8.kubernetes.client.CustomResource;
 import io.fabric8.kubernetes.model.annotation.Group;
-import io.fabric8.kubernetes.model.annotation.Kind;
 import io.fabric8.kubernetes.model.annotation.ShortNames;
 import io.fabric8.kubernetes.model.annotation.Version;
 
 @Group("sample.javaoperatorsdk")
 @Version("v1")
-@Kind("StatusUpdateLockingCustomResource")
-@ShortNames("sul")
+@ShortNames("spl")
 public class StatusPatchLockingCustomResource
     extends
     CustomResource<StatusPatchLockingCustomResourceSpec, StatusPatchLockingCustomResourceStatus>
