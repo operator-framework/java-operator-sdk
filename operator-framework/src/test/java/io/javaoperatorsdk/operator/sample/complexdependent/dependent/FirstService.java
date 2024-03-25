b409b864fd9eac0b33a0e2cfdaa09d8052c4d6b5
Attila Mészáros <csviri@gmail.com>
2024-03-25 10:51:26 +0100

    feat: remove resource discriminator (#2299)
    Signed-off-by: Attila Mészáros <csviri@gmail.com>

--- operator-framework/src/test/java/io/javaoperatorsdk/operator/sample/complexdependent/dependent/FirstService.java
+++ operator-framework/src/test/java/io/javaoperatorsdk/operator/sample/complexdependent/dependent/FirstService.java
@@ -1,20 +1,13 @@
 package io.javaoperatorsdk.operator.sample.complexdependent.dependent;
 
-import io.fabric8.kubernetes.api.model.Service;
 import io.javaoperatorsdk.operator.processing.dependent.kubernetes.KubernetesDependent;
 
-@KubernetesDependent(resourceDiscriminator = FirstService.Discriminator.class)
+@KubernetesDependent
 public class FirstService extends BaseService {
   public static final String DISCRIMINATOR_PREFIX = "first";
 
   public FirstService() {
     super(DISCRIMINATOR_PREFIX);
-  }
-
-  public static class Discriminator extends NamePrefixResourceDiscriminator<Service> {
-    protected Discriminator() {
-      super(DISCRIMINATOR_PREFIX);
-    }
   }
 
 }
