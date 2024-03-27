ac14a7f77c8e4522c30e64dbf6b464594c39cca0
Chris Laprun <metacosm@users.noreply.github.com>
2024-03-27 18:20:48 +0100

    feat: handle clustered resource on secondary to primary mapper init (#2321)
    Fixes #2311
    
    Overriding getPrimaryResourceType should allow to make things work even
    in deeper hierarchies.
    
    Signed-off-by: Chris Laprun <claprun@redhat.com>
    Signed-off-by: Attila Mészáros <csviri@gmail.com>

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/dependent/kubernetes/GenericKubernetesDependentResource.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/dependent/kubernetes/GenericKubernetesDependentResource.java
@@ -2,13 +2,14 @@ package io.javaoperatorsdk.operator.processing.dependent.kubernetes;
 
 import io.fabric8.kubernetes.api.model.GenericKubernetesResource;
 import io.fabric8.kubernetes.api.model.HasMetadata;
+import io.javaoperatorsdk.operator.api.config.Utils;
 import io.javaoperatorsdk.operator.api.config.informer.InformerConfiguration;
 import io.javaoperatorsdk.operator.processing.GroupVersionKind;
 
 public class GenericKubernetesDependentResource<P extends HasMetadata>
     extends KubernetesDependentResource<GenericKubernetesResource, P> {
 
-  private GroupVersionKind groupVersionKind;
+  private final GroupVersionKind groupVersionKind;
 
   public GenericKubernetesDependentResource(GroupVersionKind groupVersionKind) {
     super(GenericKubernetesResource.class);
@@ -19,6 +20,13 @@ public class GenericKubernetesDependentResource<P extends HasMetadata>
     return InformerConfiguration.from(groupVersionKind);
   }
 
+  @SuppressWarnings("unchecked")
+  @Override
+  protected Class<P> getPrimaryResourceType() {
+    return (Class<P>) Utils.getFirstTypeArgumentFromExtendedClass(getClass());
+  }
+
+  @SuppressWarnings("unused")
   public GroupVersionKind getGroupVersionKind() {
     return groupVersionKind;
   }
