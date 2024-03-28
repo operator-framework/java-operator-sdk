1253093f85b0195603a5c4c249b917fcd5da9da6
Attila Mészáros <csviri@gmail.com>
2024-03-28 15:35:21 +0100

    ITs
    Signed-off-by: Attila Mészáros <csviri@gmail.com>

--- operator-framework/src/test/java/io/javaoperatorsdk/operator/PatchResourceWithSSAIT.java
+++ operator-framework/src/test/java/io/javaoperatorsdk/operator/PatchResourceWithSSAIT.java
@@ -1,51 +1,15 @@
 package io.javaoperatorsdk.operator;
 
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.RegisterExtension;
 
-import io.fabric8.kubernetes.api.model.ObjectMetaBuilder;
-import io.javaoperatorsdk.operator.junit.LocallyRunOperatorExtension;
-import io.javaoperatorsdk.operator.sample.patchresourcewithssa.PatchResourceWithSSACustomResource;
+import io.javaoperatorsdk.operator.api.reconciler.Reconciler;
 import io.javaoperatorsdk.operator.sample.patchresourcewithssa.PatchResourceWithSSAReconciler;
-import io.javaoperatorsdk.operator.sample.patchresourcewithssa.PatchResourceWithSSASpec;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.awaitility.Awaitility.await;
 
-public class PatchResourceWithSSAIT {
+public class PatchResourceWithSSAIT extends PatchWithSSAITBase {
 
-  public static final String RESOURCE_NAME = "test1";
-  public static final String INIT_VALUE = "init value";
-
-  @RegisterExtension
-  LocallyRunOperatorExtension extension =
-      LocallyRunOperatorExtension.builder()
-          .withReconciler(new PatchResourceWithSSAReconciler())
-          .build();
-
-  @Test
-  void reconcilerPatchesResourceWithSSA() {
-    extension.create(testResource());
-
-    await().untilAsserted(() -> {
-      var actualResource = extension.get(PatchResourceWithSSACustomResource.class, RESOURCE_NAME);
-
-      assertThat(actualResource.getSpec().getInitValue()).isEqualTo(INIT_VALUE);
-      assertThat(actualResource.getSpec().getControllerManagedValue())
-          .isEqualTo(PatchResourceWithSSAReconciler.ADDED_VALUE);
-      // finalizer is added to the SSA patch in the background by the framework
-      assertThat(actualResource.getMetadata().getFinalizers()).isNotEmpty();
-      assertThat(actualResource.getStatus().isSuccessfullyReconciled()).isTrue();
-    });
+  @Override
+  protected Reconciler<?> reconciler() {
+    return new PatchResourceWithSSAReconciler();
   }
 
-  PatchResourceWithSSACustomResource testResource() {
-    var res = new PatchResourceWithSSACustomResource();
-    res.setMetadata(new ObjectMetaBuilder()
-        .withName(RESOURCE_NAME)
-        .build());
-    res.setSpec(new PatchResourceWithSSASpec());
-    res.getSpec().setInitValue(INIT_VALUE);
-    return res;
-  }
 }
