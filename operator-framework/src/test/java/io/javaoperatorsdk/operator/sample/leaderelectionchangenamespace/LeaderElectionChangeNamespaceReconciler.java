90ea84e31623a5c74decdc9d94a5a364da09287f
Attila Mészáros <csviri@gmail.com>
2024-04-15 13:10:57 +0200

    fix: change namespace starts processor on namespace change even if not leader (#2344)
    Signed-off-by: Attila Mészáros <csviri@gmail.com>

---
+++ operator-framework/src/test/java/io/javaoperatorsdk/operator/sample/leaderelectionchangenamespace/LeaderElectionChangeNamespaceReconciler.java
@@ -0,0 +1,29 @@
+package io.javaoperatorsdk.operator.sample.leaderelectionchangenamespace;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import io.javaoperatorsdk.operator.api.reconciler.Context;
+import io.javaoperatorsdk.operator.api.reconciler.ControllerConfiguration;
+import io.javaoperatorsdk.operator.api.reconciler.Reconciler;
+import io.javaoperatorsdk.operator.api.reconciler.UpdateControl;
+import io.javaoperatorsdk.operator.support.TestExecutionInfoProvider;
+
+@ControllerConfiguration()
+public class LeaderElectionChangeNamespaceReconciler
+    implements Reconciler<LeaderElectionChangeNamespaceCustomResource>, TestExecutionInfoProvider {
+
+  private final AtomicInteger numberOfExecutions = new AtomicInteger(0);
+
+  @Override
+  public UpdateControl<LeaderElectionChangeNamespaceCustomResource> reconcile(
+      LeaderElectionChangeNamespaceCustomResource resource,
+      Context<LeaderElectionChangeNamespaceCustomResource> context) {
+    numberOfExecutions.addAndGet(1);
+    return UpdateControl.noUpdate();
+  }
+
+  public int getNumberOfExecutions() {
+    return numberOfExecutions.get();
+  }
+
+}
