f8f7b63951a2492de99ab62b8deb22d7dc7034ed
Attila Mészáros <csviri@gmail.com>
2024-04-25 19:58:40 +0200

    improve: samples correctly create status object for SSA (#2365)
    Signed-off-by: Attila Mészáros <csviri@gmail.com>

--- sample-operators/tomcat-operator/src/main/java/io/javaoperatorsdk/operator/sample/TomcatReconciler.java
+++ sample-operators/tomcat-operator/src/main/java/io/javaoperatorsdk/operator/sample/TomcatReconciler.java
@@ -5,6 +5,7 @@ import java.util.Objects;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import io.fabric8.kubernetes.api.model.ObjectMetaBuilder;
 import io.fabric8.kubernetes.api.model.apps.Deployment;
 import io.fabric8.kubernetes.api.model.apps.DeploymentStatus;
 import io.javaoperatorsdk.operator.api.reconciler.*;
@@ -26,23 +27,28 @@ public class TomcatReconciler implements Reconciler<Tomcat> {
   @Override
   public UpdateControl<Tomcat> reconcile(Tomcat tomcat, Context<Tomcat> context) {
     return context.getSecondaryResource(Deployment.class).map(deployment -> {
-      Tomcat updatedTomcat = updateTomcatStatus(tomcat, deployment);
+      Tomcat updatedTomcat = createTomcatForStatusUpdate(tomcat, deployment);
       log.info(
           "Updating status of Tomcat {} in namespace {} to {} ready replicas",
           tomcat.getMetadata().getName(),
           tomcat.getMetadata().getNamespace(),
-          tomcat.getStatus().getReadyReplicas());
+          tomcat.getStatus() == null ? 0 : tomcat.getStatus().getReadyReplicas());
       return UpdateControl.patchStatus(updatedTomcat);
     }).orElseGet(UpdateControl::noUpdate);
   }
 
-  private Tomcat updateTomcatStatus(Tomcat tomcat, Deployment deployment) {
+  private Tomcat createTomcatForStatusUpdate(Tomcat tomcat, Deployment deployment) {
+    Tomcat res = new Tomcat();
+    res.setMetadata(new ObjectMetaBuilder()
+        .withName(tomcat.getMetadata().getName())
+        .withNamespace(tomcat.getMetadata().getNamespace())
+        .build());
     DeploymentStatus deploymentStatus =
         Objects.requireNonNullElse(deployment.getStatus(), new DeploymentStatus());
     int readyReplicas = Objects.requireNonNullElse(deploymentStatus.getReadyReplicas(), 0);
     TomcatStatus status = new TomcatStatus();
     status.setReadyReplicas(readyReplicas);
-    tomcat.setStatus(status);
-    return tomcat;
+    res.setStatus(status);
+    return res;
   }
 }
