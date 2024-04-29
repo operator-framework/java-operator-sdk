89b9fb806957b1325e3bd3ea5e65d82f761f04c9
Attila Mészáros <csviri@gmail.com>
2024-04-29 13:41:53 +0200

    feat: silent exception handling in managed workflows (#2363)
    Signed-off-by: Attila Mészáros <csviri@gmail.com>
    Signed-off-by: Chris Laprun <claprun@redhat.com>
    Co-authored-by: Chris Laprun <claprun@redhat.com>
    Signed-off-by: Attila Mészáros <csviri@gmail.com>

---
+++ operator-framework/src/test/java/io/javaoperatorsdk/operator/WorkflowSilentExceptionHandlingIT.java
@@ -0,0 +1,47 @@
+package io.javaoperatorsdk.operator;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.fabric8.kubernetes.api.model.ObjectMetaBuilder;
+import io.javaoperatorsdk.operator.junit.LocallyRunOperatorExtension;
+import io.javaoperatorsdk.operator.sample.workflowsilentexceptionhandling.HandleWorkflowExceptionsInReconcilerCustomResource;
+import io.javaoperatorsdk.operator.sample.workflowsilentexceptionhandling.HandleWorkflowExceptionsInReconcilerReconciler;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
+
+public class WorkflowSilentExceptionHandlingIT {
+
+  @RegisterExtension
+  LocallyRunOperatorExtension extension =
+      LocallyRunOperatorExtension.builder()
+          .withReconciler(HandleWorkflowExceptionsInReconcilerReconciler.class)
+          .build();
+
+  @Test
+  void handleExceptionsInReconciler() {
+    extension.create(testResource());
+    var reconciler =
+        extension.getReconcilerOfType(HandleWorkflowExceptionsInReconcilerReconciler.class);
+
+    await().untilAsserted(() -> {
+      assertThat(reconciler.isErrorsFoundInReconcilerResult()).isTrue();
+    });
+
+    extension.delete(testResource());
+
+    await().untilAsserted(() -> {
+      assertThat(reconciler.isErrorsFoundInCleanupResult()).isTrue();
+    });
+  }
+
+  HandleWorkflowExceptionsInReconcilerCustomResource testResource() {
+    var res = new HandleWorkflowExceptionsInReconcilerCustomResource();
+    res.setMetadata(new ObjectMetaBuilder()
+        .withName("test1")
+        .build());
+    return res;
+  }
+
+}
