b77905e1bcfb74fd7c0439d7390c9429957ac0de
Attila Mészáros <csviri@gmail.com>
2024-04-29 13:41:53 +0200

    feat: silent exception handling in managed workflows (#2363)
    Signed-off-by: Attila Mészáros <csviri@gmail.com>
    Signed-off-by: Chris Laprun <claprun@redhat.com>
    Co-authored-by: Chris Laprun <claprun@redhat.com>

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/api/reconciler/Workflow.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/api/reconciler/Workflow.java
@@ -1,6 +1,10 @@
 package io.javaoperatorsdk.operator.api.reconciler;
 
-import java.lang.annotation.*;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 import io.fabric8.kubernetes.api.model.HasMetadata;
 import io.javaoperatorsdk.operator.api.reconciler.dependent.Dependent;
@@ -13,10 +17,25 @@ public @interface Workflow {
   Dependent[] dependents();
 
   /**
-   * If true, managed workflow should be explicitly invoked within the reconciler implementation. If
-   * false workflow is invoked just before the {@link Reconciler#reconcile(HasMetadata, Context)}
-   * method.
+   * If {@code true}, the managed workflow should be explicitly invoked within the reconciler
+   * implementation. If {@code false}, the workflow is invoked just before the
+   * {@link Reconciler#reconcile(HasMetadata, Context)} method.
    */
   boolean explicitInvocation() default false;
+
+  /**
+   * If {@code true} and exceptions are thrown during the workflow's execution, the reconciler won't
+   * throw an {@link io.javaoperatorsdk.operator.AggregatedOperatorException} at the end of the
+   * execution as would normally be the case. Instead, it will proceed to its
+   * {@link Reconciler#reconcile(HasMetadata, Context)} method as if no error occurred. It is then
+   * up to the developer to decide how to proceed by retrieving the errored dependents (and their
+   * associated exception) via
+   * {@link io.javaoperatorsdk.operator.processing.dependent.workflow.WorkflowResult#erroredDependents},
+   * the workflow result itself being accessed from
+   * {@link Context#managedWorkflowAndDependentResourceContext()}. If {@code false}, an exception
+   * will be automatically thrown at the end of the workflow execution, presenting an aggregated
+   * view of what happened.
+   */
+  boolean handleExceptionsInReconciler() default false;
 
 }
--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/Controller.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/Controller.java
@@ -71,6 +71,7 @@ public class Controller<P extends HasMetadata>
   private final boolean isCleaner;
   private final Metrics metrics;
   private final Workflow<P> managedWorkflow;
+  private final boolean explicitWorkflowInvocation;
 
   private final GroupVersionKind associatedGVK;
   private final EventProcessor<P> eventProcessor;
@@ -93,6 +94,9 @@ public class Controller<P extends HasMetadata>
 
     final var managed = configurationService.getWorkflowFactory().workflowFor(configuration);
     managedWorkflow = managed.resolve(kubernetesClient, configuration);
+    explicitWorkflowInvocation =
+        configuration.getWorkflowSpec().map(WorkflowSpec::isExplicitInvocation)
+            .orElse(false);
 
     eventSourceManager = new EventSourceManager<>(this);
     eventProcessor = new EventProcessor<>(eventSourceManager, configurationService);
@@ -144,7 +148,7 @@ public class Controller<P extends HasMetadata>
           public UpdateControl<P> execute() throws Exception {
             initContextIfNeeded(resource, context);
             configuration.getWorkflowSpec().ifPresent(ws -> {
-              if (!isWorkflowExplicitInvocation()) {
+              if (!explicitWorkflowInvocation) {
                 reconcileManagedWorkflow(resource, context);
               }
             });
@@ -190,7 +194,7 @@ public class Controller<P extends HasMetadata>
 
               // The cleanup is called also when explicit invocation is true, but the cleaner is not
               // implemented
-              if (!isCleaner || !isWorkflowExplicitInvocation()) {
+              if (!isCleaner || !explicitWorkflowInvocation) {
                 workflowCleanupResult = cleanupManagedWorkflow(resource, context);
               }
 
@@ -443,7 +447,6 @@ public class Controller<P extends HasMetadata>
       ((DefaultManagedWorkflowAndDependentResourceContext) context
           .managedWorkflowAndDependentResourceContext())
           .setWorkflowExecutionResult(res);
-      res.throwAggregateExceptionIfErrorsPresent();
     }
   }
 
@@ -453,7 +456,7 @@ public class Controller<P extends HasMetadata>
       ((DefaultManagedWorkflowAndDependentResourceContext) context
           .managedWorkflowAndDependentResourceContext())
           .setWorkflowCleanupResult(workflowCleanupResult);
-      workflowCleanupResult.throwAggregateExceptionIfErrorsPresent();
+
       return workflowCleanupResult;
     } else {
       return null;
@@ -461,7 +464,6 @@ public class Controller<P extends HasMetadata>
   }
 
   public boolean isWorkflowExplicitInvocation() {
-    return configuration.getWorkflowSpec().map(WorkflowSpec::isExplicitInvocation)
-        .orElse(false);
+    return explicitWorkflowInvocation;
   }
 }
