1c7a094b646a061cd6e0928a5ee140d3dd0dbe87
Attila Mészáros <csviri@gmail.com>
2024-05-14 16:22:48 +0200

    feat: remove automatic observed generation handling (#2382)
    Signed-off-by: Attila Mészáros <csviri@gmail.com>
    Signed-off-by: Chris Laprun <claprun@redhat.com>
    Co-authored-by: Chris Laprun <claprun@redhat.com>
    Signed-off-by: Attila Mészáros <csviri@gmail.com>

--- operator-framework-core/src/test/java/io/javaoperatorsdk/operator/sample/observedgeneration/ObservedGenStatus.java
+++ operator-framework-core/src/test/java/io/javaoperatorsdk/operator/sample/observedgeneration/ObservedGenStatus.java
@@ -1,7 +1,5 @@
 package io.javaoperatorsdk.operator.sample.observedgeneration;
 
-import io.javaoperatorsdk.operator.api.ObservedGenerationAwareStatus;
-
-public class ObservedGenStatus extends ObservedGenerationAwareStatus {
+public class ObservedGenStatus {
 
 }
