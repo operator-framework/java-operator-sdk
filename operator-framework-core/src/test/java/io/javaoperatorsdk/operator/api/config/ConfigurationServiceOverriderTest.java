2e72bbec2a1afded1063fb0045006e8d821c514b
Attila Mészáros <csviri@gmail.com>
2024-05-16 10:01:46 +0200

    improve: remove deprecated APIs (#2375)
    Signed-off-by: Attila Mészáros <csviri@gmail.com>

--- operator-framework-core/src/test/java/io/javaoperatorsdk/operator/api/config/ConfigurationServiceOverriderTest.java
+++ operator-framework-core/src/test/java/io/javaoperatorsdk/operator/api/config/ConfigurationServiceOverriderTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Test;
 import io.fabric8.kubernetes.api.model.HasMetadata;
 import io.javaoperatorsdk.operator.api.monitoring.Metrics;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class ConfigurationServiceOverriderTest {
@@ -78,26 +77,6 @@ class ConfigurationServiceOverriderTest {
         overridden.getLeaderElectionConfiguration());
     assertNotEquals(config.getInformerStoppedHandler(),
         overridden.getLeaderElectionConfiguration());
-  }
-
-  @Test
-  void shouldReplaceInvalidValues() {
-    final var original = new BaseConfigurationService();
-
-    final var service = ConfigurationService.newOverriddenConfigurationService(original,
-        o -> o
-            .withConcurrentReconciliationThreads(0)
-            .withMinConcurrentReconciliationThreads(-1)
-            .withConcurrentWorkflowExecutorThreads(2)
-            .withMinConcurrentWorkflowExecutorThreads(3));
-
-    assertEquals(original.minConcurrentReconciliationThreads(),
-        service.minConcurrentReconciliationThreads());
-    assertEquals(original.concurrentReconciliationThreads(),
-        service.concurrentReconciliationThreads());
-    assertEquals(3, service.minConcurrentWorkflowExecutorThreads());
-    assertEquals(original.concurrentWorkflowExecutorThreads(),
-        service.concurrentWorkflowExecutorThreads());
   }
 
 }
