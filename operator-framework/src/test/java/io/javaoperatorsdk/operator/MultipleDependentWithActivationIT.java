362d1d235e35ba7143c9ded33a1fb84078614a19
Attila Mészáros <csviri@gmail.com>
2024-06-21 17:33:01 +0200

    feat: integration test to show mulitple dependents with activation condition
    Signed-off-by: Attila Mészáros <csviri@gmail.com>

---
+++ operator-framework/src/test/java/io/javaoperatorsdk/operator/MultipleDependentWithActivationIT.java
@@ -0,0 +1,60 @@
+package io.javaoperatorsdk.operator;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.fabric8.kubernetes.api.model.ConfigMap;
+import io.fabric8.kubernetes.api.model.ObjectMetaBuilder;
+import io.fabric8.kubernetes.api.model.Secret;
+import io.javaoperatorsdk.operator.junit.LocallyRunOperatorExtension;
+import io.javaoperatorsdk.operator.sample.multipledependentwithactivation.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
+
+public class MultipleDependentWithActivationIT {
+
+  public static final String INITIAL_VALUE = "initial_value";
+  public static final String TEST_RESOURCE_NAME = "test1";
+
+  @RegisterExtension
+  LocallyRunOperatorExtension extension =
+      LocallyRunOperatorExtension.builder()
+          .withReconciler(new MultipleDependentActivationReconciler())
+          .build();
+
+  @Test
+  void bothDependentsWithActivationAreHandled() {
+    var resource = extension.create(testResource());
+
+    await().untilAsserted(() -> {
+      var cm1 =
+          extension.get(ConfigMap.class, TEST_RESOURCE_NAME + ConfigMapDependentResource1.SUFFIX);
+      var cm2 =
+          extension.get(ConfigMap.class, TEST_RESOURCE_NAME + ConfigMapDependentResource2.SUFFIX);
+      var secret = extension.get(Secret.class, TEST_RESOURCE_NAME);
+
+      assertThat(secret).isNotNull();
+      assertThat(cm1).isNotNull();
+      assertThat(cm2).isNotNull();
+      assertThat(cm1.getData()).containsEntry(ConfigMapDependentResource1.DATA_KEY,
+          INITIAL_VALUE + ConfigMapDependentResource1.SUFFIX);
+      assertThat(cm2.getData()).containsEntry(ConfigMapDependentResource2.DATA_KEY,
+          INITIAL_VALUE + ConfigMapDependentResource2.SUFFIX);
+    });
+
+  }
+
+  MultipleDependentActivationCustomResource testResource() {
+    var res = new MultipleDependentActivationCustomResource();
+    res.setMetadata(new ObjectMetaBuilder()
+        .withName(TEST_RESOURCE_NAME)
+        .build());
+    res.setSpec(new MultipleDependentActivationSpec());
+    res.getSpec().setValue(INITIAL_VALUE);
+
+    return res;
+  }
+
+
+}
