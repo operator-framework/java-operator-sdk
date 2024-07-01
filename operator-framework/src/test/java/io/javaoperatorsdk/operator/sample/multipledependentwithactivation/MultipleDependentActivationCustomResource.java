20e7980619399ed8d8affe40b1fff4468f148136
Attila Mészáros <csviri@gmail.com>
2024-07-01 09:12:34 +0200

    feat: integration test to show multiple dependents with activation condition (#2454)
    Signed-off-by: Attila Mészáros <csviri@gmail.com>

---
+++ operator-framework/src/test/java/io/javaoperatorsdk/operator/sample/multipledependentwithactivation/MultipleDependentActivationCustomResource.java
@@ -0,0 +1,17 @@
+package io.javaoperatorsdk.operator.sample.multipledependentwithactivation;
+
+import io.fabric8.kubernetes.api.model.Namespaced;
+import io.fabric8.kubernetes.client.CustomResource;
+import io.fabric8.kubernetes.model.annotation.Group;
+import io.fabric8.kubernetes.model.annotation.ShortNames;
+import io.fabric8.kubernetes.model.annotation.Version;
+
+@Group("sample.javaoperatorsdk")
+@Version("v1")
+@ShortNames("mdar")
+public class MultipleDependentActivationCustomResource
+    extends CustomResource<MultipleDependentActivationSpec, Void>
+    implements Namespaced {
+
+
+}
