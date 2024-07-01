5dc01322148af40274d591a0820d0bc5b860746b
Attila Mészáros <csviri@gmail.com>
2024-07-01 09:12:34 +0200

    feat: integration test to show multiple dependents with activation condition (#2454)
    Signed-off-by: Attila Mészáros <csviri@gmail.com>

---
+++ operator-framework/src/test/java/io/javaoperatorsdk/operator/sample/multipledependentwithactivation/SecretDependentResource.java
@@ -0,0 +1,31 @@
+package io.javaoperatorsdk.operator.sample.multipledependentwithactivation;
+
+import java.util.Base64;
+import java.util.Map;
+
+import io.fabric8.kubernetes.api.model.ObjectMetaBuilder;
+import io.fabric8.kubernetes.api.model.Secret;
+import io.javaoperatorsdk.operator.api.reconciler.Context;
+import io.javaoperatorsdk.operator.processing.dependent.kubernetes.CRUDKubernetesDependentResource;
+
+public class SecretDependentResource
+    extends CRUDKubernetesDependentResource<Secret, MultipleDependentActivationCustomResource> {
+
+  public SecretDependentResource() {
+    super(Secret.class);
+  }
+
+  @Override
+  protected Secret desired(MultipleDependentActivationCustomResource primary,
+      Context<MultipleDependentActivationCustomResource> context) {
+    // basically does not matter since this should not be called
+    Secret secret = new Secret();
+    secret.setMetadata(new ObjectMetaBuilder()
+        .withName(primary.getMetadata().getName())
+        .withNamespace(primary.getMetadata().getNamespace())
+        .build());
+    secret.setData(Map.of("data",
+        Base64.getEncoder().encodeToString(primary.getSpec().getValue().getBytes())));
+    return secret;
+  }
+}
