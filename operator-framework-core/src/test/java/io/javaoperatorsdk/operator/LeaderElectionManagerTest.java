0efdf855f0c9fcfd9f9ec455b80e150dde6ea429
Michael Edgar <michael@xlate.io>
2024-07-02 12:03:29 -0400

    Update lease access check to account for lease name and multiple rules (#2456)
    Signed-off-by: Michael Edgar <medgar@redhat.com>
    Signed-off-by: Chris Laprun <claprun@redhat.com>
    Co-authored-by: Chris Laprun <claprun@redhat.com>

--- operator-framework-core/src/test/java/io/javaoperatorsdk/operator/LeaderElectionManagerTest.java
+++ operator-framework-core/src/test/java/io/javaoperatorsdk/operator/LeaderElectionManagerTest.java
@@ -3,11 +3,15 @@ package io.javaoperatorsdk.operator;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
+import io.fabric8.kubernetes.api.model.authorization.v1.ResourceRule;
+import io.fabric8.kubernetes.api.model.authorization.v1.SelfSubjectRulesReview;
+import io.fabric8.kubernetes.api.model.authorization.v1.SubjectRulesReviewStatus;
 import io.fabric8.kubernetes.api.model.coordination.v1.Lease;
 import io.fabric8.kubernetes.client.Config;
 import io.javaoperatorsdk.operator.api.config.ConfigurationService;
@@ -15,6 +19,8 @@ import io.javaoperatorsdk.operator.api.config.LeaderElectionConfiguration;
 
 import static io.fabric8.kubernetes.client.Config.KUBERNETES_AUTH_TRYKUBECONFIG_SYSTEM_PROPERTY;
 import static io.fabric8.kubernetes.client.Config.KUBERNETES_NAMESPACE_FILE;
+import static io.javaoperatorsdk.operator.LeaderElectionManager.COORDINATION_GROUP;
+import static io.javaoperatorsdk.operator.LeaderElectionManager.LEASES_RESOURCE;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
@@ -22,9 +28,9 @@ import static org.mockito.Mockito.when;
 
 class LeaderElectionManagerTest {
 
-  private LeaderElectionManager leaderElectionManager() {
+  private LeaderElectionManager leaderElectionManager(Object selfSubjectReview) {
     ControllerManager controllerManager = mock(ControllerManager.class);
-    final var kubernetesClient = MockKubernetesClient.client(Lease.class);
+    final var kubernetesClient = MockKubernetesClient.client(Lease.class, selfSubjectReview);
     when(kubernetesClient.getConfiguration()).thenReturn(Config.autoConfigure(null));
     var configurationService =
         ConfigurationService.newOverriddenConfigurationService(
@@ -48,7 +54,7 @@ class LeaderElectionManagerTest {
     System.setProperty(KUBERNETES_AUTH_TRYKUBECONFIG_SYSTEM_PROPERTY, "false");
     System.setProperty(KUBERNETES_NAMESPACE_FILE, namespacePath.toString());
 
-    final var leaderElectionManager = leaderElectionManager();
+    final var leaderElectionManager = leaderElectionManager(null);
     leaderElectionManager.start();
     assertTrue(leaderElectionManager.isLeaderElectionEnabled());
   }
@@ -56,6 +62,64 @@ class LeaderElectionManagerTest {
   @Test
   void testFailedToInitInferLeaseNamespace() {
     System.setProperty(KUBERNETES_AUTH_TRYKUBECONFIG_SYSTEM_PROPERTY, "false");
-    assertThrows(IllegalArgumentException.class, () -> leaderElectionManager().start());
+    final var leaderElectionManager = leaderElectionManager(null);
+    assertThrows(IllegalArgumentException.class, leaderElectionManager::start);
+  }
+
+  @Test
+  void testInitPermissionsMultipleRulesWithResourceName(@TempDir Path tempDir) throws IOException {
+    var namespace = "foo";
+    var namespacePath = tempDir.resolve("namespace");
+    Files.writeString(namespacePath, namespace);
+
+    System.setProperty(KUBERNETES_AUTH_TRYKUBECONFIG_SYSTEM_PROPERTY, "false");
+    System.setProperty(KUBERNETES_NAMESPACE_FILE, namespacePath.toString());
+
+    SelfSubjectRulesReview review = new SelfSubjectRulesReview();
+    review.setStatus(new SubjectRulesReviewStatus());
+    var resourceRule1 = new ResourceRule();
+    resourceRule1.setApiGroups(Arrays.asList(COORDINATION_GROUP));
+    resourceRule1.setResources(Arrays.asList(LEASES_RESOURCE));
+    resourceRule1.setResourceNames(Arrays.asList("test"));
+    resourceRule1.setVerbs(Arrays.asList("get", "update"));
+    var resourceRule2 = new ResourceRule();
+    resourceRule2.setApiGroups(Arrays.asList(COORDINATION_GROUP));
+    resourceRule2.setResources(Arrays.asList(LEASES_RESOURCE));
+    resourceRule2.setVerbs(Arrays.asList("create"));
+    review.getStatus().setResourceRules(Arrays.asList(resourceRule1, resourceRule2));
+
+    final var leaderElectionManager = leaderElectionManager(review);
+    leaderElectionManager.start();
+    assertTrue(leaderElectionManager.isLeaderElectionEnabled());
+  }
+
+  @Test
+  void testFailedToInitMissingPermission(@TempDir Path tempDir) throws IOException {
+    var namespace = "foo";
+    var namespacePath = tempDir.resolve("namespace");
+    Files.writeString(namespacePath, namespace);
+
+    System.setProperty(KUBERNETES_AUTH_TRYKUBECONFIG_SYSTEM_PROPERTY, "false");
+    System.setProperty(KUBERNETES_NAMESPACE_FILE, namespacePath.toString());
+
+    SelfSubjectRulesReview review = new SelfSubjectRulesReview();
+    review.setStatus(new SubjectRulesReviewStatus());
+    var resourceRule1 = new ResourceRule();
+    resourceRule1.setApiGroups(Arrays.asList(COORDINATION_GROUP));
+    resourceRule1.setResources(Arrays.asList(LEASES_RESOURCE));
+    resourceRule1.setVerbs(Arrays.asList("get"));
+    var resourceRule2 = new ResourceRule();
+    resourceRule2.setApiGroups(Arrays.asList(COORDINATION_GROUP));
+    resourceRule2.setResources(Arrays.asList(LEASES_RESOURCE));
+    resourceRule2.setVerbs(Arrays.asList("update"));
+    var resourceRule3 = new ResourceRule();
+    resourceRule3.setApiGroups(Arrays.asList(COORDINATION_GROUP));
+    resourceRule3.setResources(Arrays.asList(LEASES_RESOURCE));
+    resourceRule3.setResourceNames(Arrays.asList("some-other-lease"));
+    resourceRule3.setVerbs(Arrays.asList("create"));
+    review.getStatus().setResourceRules(Arrays.asList(resourceRule1, resourceRule2, resourceRule3));
+
+    final var leaderElectionManager = leaderElectionManager(review);
+    assertThrows(OperatorException.class, leaderElectionManager::start);
   }
 }
