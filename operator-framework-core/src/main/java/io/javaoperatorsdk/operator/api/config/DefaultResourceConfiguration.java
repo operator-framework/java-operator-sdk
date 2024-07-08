df51036a6d9d524476b0b798714646e1f5af1bea
Chris Laprun <metacosm@users.noreply.github.com>
2024-07-08 15:56:34 +0200

    feat: move controller informer-related configuration to InformerConfig (#2455)
    * feat: move controller informer-related configuration to InformerConfig
    
    Signed-off-by: Chris Laprun <claprun@redhat.com>
    
    * refactor: start isolating ResourceConfiguration
    
    Signed-off-by: Chris Laprun <claprun@redhat.com>
    
    * fix: initFromAnnotation now properly inits the current instance
    
    Signed-off-by: Chris Laprun <claprun@redhat.com>
    
    * fix: default onDeleteFilter implementation
    
    Signed-off-by: Chris Laprun <claprun@redhat.com>
    
    * fix: properly set default namespaces in controller case
    
    Signed-off-by: Chris Laprun <claprun@redhat.com>
    
    * refactor: remove KubernetesDependentInformerConfigBuilder
    
    Signed-off-by: Chris Laprun <claprun@redhat.com>
    
    * refactor: use InformerConfigHolder in more places, unifying handling
    
    Signed-off-by: Chris Laprun <claprun@redhat.com>
    
    * fix: properly propagate name to informer config
    
    Signed-off-by: Chris Laprun <claprun@redhat.com>
    
    * feat: add factory method to init builder from an extising configuation
    
    Signed-off-by: Chris Laprun <claprun@redhat.com>
    
    * fix: remove potentially problematic default implementation
    
    Signed-off-by: Chris Laprun <claprun@redhat.com>
    
    ---------
    
    Signed-off-by: Chris Laprun <claprun@redhat.com>

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/api/config/DefaultResourceConfiguration.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/api/config/DefaultResourceConfiguration.java
@@ -1,47 +1,27 @@
 package io.javaoperatorsdk.operator.api.config;
 
-import java.util.Optional;
-import java.util.Set;
 
 import io.fabric8.kubernetes.api.model.GenericKubernetesResource;
 import io.fabric8.kubernetes.api.model.HasMetadata;
-import io.fabric8.kubernetes.client.informers.cache.ItemStore;
 import io.javaoperatorsdk.operator.ReconcilerUtils;
-import io.javaoperatorsdk.operator.processing.event.source.filter.GenericFilter;
-import io.javaoperatorsdk.operator.processing.event.source.filter.OnAddFilter;
-import io.javaoperatorsdk.operator.processing.event.source.filter.OnUpdateFilter;
+import io.javaoperatorsdk.operator.processing.dependent.kubernetes.InformerConfigHolder;
 
 public class DefaultResourceConfiguration<R extends HasMetadata>
     implements ResourceConfiguration<R> {
 
   private final Class<R> resourceClass;
   private final String resourceTypeName;
-  private final OnAddFilter<? super R> onAddFilter;
-  private final OnUpdateFilter<? super R> onUpdateFilter;
-  private final GenericFilter<? super R> genericFilter;
-  private final String labelSelector;
-  private final Set<String> namespaces;
-  private final ItemStore<R> itemStore;
-  private final Long informerListLimit;
+  private final InformerConfigHolder<R> informerConfig;
 
   protected DefaultResourceConfiguration(Class<R> resourceClass,
-      Set<String> namespaces, String labelSelector, OnAddFilter<? super R> onAddFilter,
-      OnUpdateFilter<? super R> onUpdateFilter, GenericFilter<? super R> genericFilter,
-      ItemStore<R> itemStore, Long informerListLimit) {
+      InformerConfigHolder<R> informerConfig) {
     this.resourceClass = resourceClass;
     this.resourceTypeName = resourceClass.isAssignableFrom(GenericKubernetesResource.class)
         // in general this is irrelevant now for secondary resources it is used just by controller
         // where GenericKubernetesResource now does not apply
         ? GenericKubernetesResource.class.getSimpleName()
         : ReconcilerUtils.getResourceTypeName(resourceClass);
-    this.onAddFilter = onAddFilter;
-    this.onUpdateFilter = onUpdateFilter;
-    this.genericFilter = genericFilter;
-
-    this.namespaces = ResourceConfiguration.ensureValidNamespaces(namespaces);
-    this.labelSelector = ResourceConfiguration.ensureValidLabelSelector(labelSelector);
-    this.itemStore = itemStore;
-    this.informerListLimit = informerListLimit;
+    this.informerConfig = informerConfig;
   }
 
   @Override
@@ -50,43 +30,12 @@ public class DefaultResourceConfiguration<R extends HasMetadata>
   }
 
   @Override
-  public String getLabelSelector() {
-    return labelSelector;
-  }
-
-  @Override
-  public Set<String> getNamespaces() {
-    return namespaces;
-  }
-
-  @Override
   public Class<R> getResourceClass() {
     return resourceClass;
   }
 
   @Override
-  public Optional<OnAddFilter<? super R>> onAddFilter() {
-    return Optional.ofNullable(onAddFilter);
+  public InformerConfigHolder<R> getInformerConfig() {
+    return informerConfig;
   }
-
-  @Override
-  public Optional<OnUpdateFilter<? super R>> onUpdateFilter() {
-    return Optional.ofNullable(onUpdateFilter);
-  }
-
-  @Override
-  public Optional<GenericFilter<? super R>> genericFilter() {
-    return Optional.ofNullable(genericFilter);
-  }
-
-  @Override
-  public Optional<ItemStore<R>> getItemStore() {
-    return Optional.ofNullable(itemStore);
-  }
-
-  @Override
-  public Optional<Long> getInformerListLimit() {
-    return Optional.ofNullable(informerListLimit);
-  }
-
 }
--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/dependent/kubernetes/InformerConfig.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/dependent/kubernetes/InformerConfig.java
@@ -5,13 +5,16 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import io.fabric8.kubernetes.client.informers.cache.ItemStore;
 import io.javaoperatorsdk.operator.api.reconciler.Constants;
+import io.javaoperatorsdk.operator.processing.event.source.cache.BoundedItemStore;
 import io.javaoperatorsdk.operator.processing.event.source.filter.GenericFilter;
 import io.javaoperatorsdk.operator.processing.event.source.filter.OnAddFilter;
 import io.javaoperatorsdk.operator.processing.event.source.filter.OnDeleteFilter;
 import io.javaoperatorsdk.operator.processing.event.source.filter.OnUpdateFilter;
 
 import static io.javaoperatorsdk.operator.api.config.informer.InformerConfiguration.DEFAULT_FOLLOW_CONTROLLER_NAMESPACES_ON_CHANGE;
+import static io.javaoperatorsdk.operator.api.reconciler.Constants.NO_LONG_VALUE_SET;
 import static io.javaoperatorsdk.operator.api.reconciler.Constants.NO_VALUE_SET;
 
 @Retention(RetentionPolicy.RUNTIME)
@@ -21,32 +24,36 @@ public @interface InformerConfig {
   String name() default NO_VALUE_SET;
 
   /**
-   * Specified which namespaces this Controller monitors for custom resources events. If no
-   * namespace is specified then the controller will monitor the namespaces configured for the
-   * controller.
-   *
-   * You can set a list of namespaces or also constants:
+   * Specified which namespaces the associated informer monitors for custom resources events. If no
+   * namespace is specified then which namespaces the informer will monitor will depend on the
+   * context in which the informer is configured:
    * <ul>
-   * <li>{@link Constants#WATCH_ALL_NAMESPACE_SET}</li>
+   * <li>all namespaces if configuring a controller informer</li>
+   * <li>the namespaces configured for the associated controller if configuring an event source</li>
+   * </ul>
+   *
+   * You can set a list of namespaces or use the following constants:
+   * <ul>
+   * <li>{@link Constants#WATCH_ALL_NAMESPACES}</li>
    * <li>{@link Constants#WATCH_CURRENT_NAMESPACE}</li>
    * <li>{@link Constants#SAME_AS_CONTROLLER}</li>
    * </ul>
    * 
-   * @return the array of namespaces this controller monitors
+   * @return the array of namespaces the associated informer monitors
    */
   String[] namespaces() default {Constants.SAME_AS_CONTROLLER};
 
   /**
-   * Optional label selector used to identify the set of custom resources the controller will act
-   * upon. The label selector can be made of multiple comma separated requirements that acts as a
-   * logical AND operator.
+   * Optional label selector used to identify the set of custom resources the associated informer
+   * will act upon. The label selector can be made of multiple comma separated requirements that
+   * acts as a logical AND operator.
    *
    * @return the label selector
    */
   String labelSelector() default NO_VALUE_SET;
 
   /**
-   * Optional {@link OnAddFilter} to filter events sent to this KubernetesDependent
+   * Optional {@link OnAddFilter} to filter add events sent to the associated informer
    *
    * @return the {@link OnAddFilter} filter implementation to use, defaulting to the interface
    *         itself if no value is set
@@ -54,7 +61,7 @@ public @interface InformerConfig {
   Class<? extends OnAddFilter> onAddFilter() default OnAddFilter.class;
 
   /**
-   * Optional {@link OnUpdateFilter} to filter events sent to this KubernetesDependent
+   * Optional {@link OnUpdateFilter} to filter update events sent to the associated informer
    *
    * @return the {@link OnUpdateFilter} filter implementation to use, defaulting to the interface
    *         itself if no value is set
@@ -62,7 +69,7 @@ public @interface InformerConfig {
   Class<? extends OnUpdateFilter> onUpdateFilter() default OnUpdateFilter.class;
 
   /**
-   * Optional {@link OnDeleteFilter} to filter events sent to this KubernetesDependent
+   * Optional {@link OnDeleteFilter} to filter delete events sent to the associated informer
    *
    * @return the {@link OnDeleteFilter} filter implementation to use, defaulting to the interface
    *         itself if no value is set
@@ -70,7 +77,7 @@ public @interface InformerConfig {
   Class<? extends OnDeleteFilter> onDeleteFilter() default OnDeleteFilter.class;
 
   /**
-   * Optional {@link GenericFilter} to filter events sent to this KubernetesDependent
+   * Optional {@link GenericFilter} to filter events sent to the associated informer
    *
    * @return the {@link GenericFilter} filter implementation to use, defaulting to the interface
    *         itself if no value is set
@@ -82,5 +89,33 @@ public @interface InformerConfig {
    * new namespace set.
    */
   boolean followControllerNamespacesOnChange() default DEFAULT_FOLLOW_CONTROLLER_NAMESPACES_ON_CHANGE;
+
+  /**
+   * Replaces the item store used by the informer for the associated primary resource controller.
+   * See underlying <a href=
+   * "https://github.com/fabric8io/kubernetes-client/blob/43b67939fde91046ab7fb0c362f500c2b46eb59e/kubernetes-client/src/main/java/io/fabric8/kubernetes/client/informers/impl/DefaultSharedIndexInformer.java#L273">
+   * method</a> in fabric8 client informer implementation.
+   *
+   * <p>
+   * The main goal, is to be able to use limited caches or provide any custom implementation.
+   * </p>
+   *
+   * <p>
+   * See {@link BoundedItemStore} and <a href=
+   * "https://github.com/operator-framework/java-operator-sdk/blob/main/caffeine-bounded-cache-support/src/main/java/io/javaoperatorsdk/operator/processing/event/source/cache/CaffeineBoundedCache.java">CaffeinBoundedCache</a>
+   * </p>
+   *
+   * @return the class of the {@link ItemStore} implementation to use
+   */
+  // todo: check javadoc
+  Class<? extends ItemStore> itemStore() default ItemStore.class;
+
+  /**
+   * The maximum amount of items to return for a single list call when starting the primary resource
+   * related informers. If this is a not null it will result in paginating for the initial load of
+   * the informer cache.
+   */
+  // todo: check javadoc
+  long informerListLimit() default NO_LONG_VALUE_SET;
 
 }
