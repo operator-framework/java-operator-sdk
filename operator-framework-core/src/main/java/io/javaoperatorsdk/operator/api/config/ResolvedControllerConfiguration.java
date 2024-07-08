aaeb978fb2d7c3b06d2e403a02577dc5a75ff75a
Chris Laprun <metacosm@users.noreply.github.com>
2024-07-08 15:56:34 +0200

    feat: move controller informer-related configuration to InformerConfig (#2455)
    * feat: move controller informer-related configuration to InformerConfig
    
    Signed-off-by: Chris Laprun <claprun@redhat.com>
    
    * refactor: start isolating ResourceConfiguration
    
    Signed-off-by: Chris Laprun <claprun@redhat.com>
    
    * fix: initFromAnnotation now properly inits the current instance
    
    Signed-off-by: Chris Laprun <claprun@redhat.com>
    
    * fix: default onDeleteFilter implementation
    
    Signed-off-by: Chris Laprun <claprun@redhat.com>
    
    * fix: properly set default namespaces in controller case
    
    Signed-off-by: Chris Laprun <claprun@redhat.com>
    
    * refactor: remove KubernetesDependentInformerConfigBuilder
    
    Signed-off-by: Chris Laprun <claprun@redhat.com>
    
    * refactor: use InformerConfigHolder in more places, unifying handling
    
    Signed-off-by: Chris Laprun <claprun@redhat.com>
    
    * fix: properly propagate name to informer config
    
    Signed-off-by: Chris Laprun <claprun@redhat.com>
    
    * feat: add factory method to init builder from an extising configuation
    
    Signed-off-by: Chris Laprun <claprun@redhat.com>
    
    * fix: remove potentially problematic default implementation
    
    Signed-off-by: Chris Laprun <claprun@redhat.com>
    
    ---------
    
    Signed-off-by: Chris Laprun <claprun@redhat.com>

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/api/config/ResolvedControllerConfiguration.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/api/config/ResolvedControllerConfiguration.java
@@ -4,18 +4,14 @@ import java.time.Duration;
 import java.util.Collections;
 import java.util.Map;
 import java.util.Optional;
-import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import io.fabric8.kubernetes.api.model.HasMetadata;
-import io.fabric8.kubernetes.client.informers.cache.ItemStore;
 import io.javaoperatorsdk.operator.api.config.dependent.DependentResourceSpec;
 import io.javaoperatorsdk.operator.api.config.workflow.WorkflowSpec;
 import io.javaoperatorsdk.operator.api.reconciler.Reconciler;
+import io.javaoperatorsdk.operator.processing.dependent.kubernetes.InformerConfigHolder;
 import io.javaoperatorsdk.operator.processing.event.rate.RateLimiter;
-import io.javaoperatorsdk.operator.processing.event.source.filter.GenericFilter;
-import io.javaoperatorsdk.operator.processing.event.source.filter.OnAddFilter;
-import io.javaoperatorsdk.operator.processing.event.source.filter.OnUpdateFilter;
 import io.javaoperatorsdk.operator.processing.retry.Retry;
 
 @SuppressWarnings("rawtypes")
@@ -31,7 +27,6 @@ public class ResolvedControllerConfiguration<P extends HasMetadata>
   private final Duration maxReconciliationInterval;
   private final String finalizer;
   private final Map<DependentResourceSpec, Object> configurations;
-  private final ItemStore<P> itemStore;
   private final ConfigurationService configurationService;
   private final String fieldManager;
   private WorkflowSpec workflowSpec;
@@ -40,13 +35,53 @@ public class ResolvedControllerConfiguration<P extends HasMetadata>
     this(resourceClass, other.getName(), other.isGenerationAware(),
         other.getAssociatedReconcilerClassName(), other.getRetry(), other.getRateLimiter(),
         other.maxReconciliationInterval().orElse(null),
-        other.onAddFilter().orElse(null), other.onUpdateFilter().orElse(null),
-        other.genericFilter().orElse(null),
-        other.getNamespaces(),
-        other.getFinalizerName(), other.getLabelSelector(), Collections.emptyMap(),
-        other.getItemStore().orElse(null), other.fieldManager(),
+        other.getFinalizerName(), Collections.emptyMap(),
+        other.fieldManager(),
         other.getConfigurationService(),
-        other.getInformerListLimit().orElse(null), other.getWorkflowSpec().orElse(null));
+        other.getInformerConfig(),
+        other.getWorkflowSpec().orElse(null));
+  }
+
+  public ResolvedControllerConfiguration(Class<P> resourceClass, String name,
+      boolean generationAware, String associatedReconcilerClassName, Retry retry,
+      RateLimiter rateLimiter, Duration maxReconciliationInterval,
+      String finalizer,
+      Map<DependentResourceSpec, Object> configurations,
+      String fieldManager,
+      ConfigurationService configurationService,
+      InformerConfigHolder<P> informerConfig,
+      WorkflowSpec workflowSpec) {
+    this(resourceClass, name, generationAware, associatedReconcilerClassName, retry, rateLimiter,
+        maxReconciliationInterval, finalizer, configurations, fieldManager,
+        configurationService, informerConfig);
+    setWorkflowSpec(workflowSpec);
+  }
+
+  protected ResolvedControllerConfiguration(Class<P> resourceClass, String name,
+      boolean generationAware, String associatedReconcilerClassName, Retry retry,
+      RateLimiter rateLimiter, Duration maxReconciliationInterval, String finalizer,
+      Map<DependentResourceSpec, Object> configurations,
+      String fieldManager,
+      ConfigurationService configurationService, InformerConfigHolder<P> informerConfig) {
+    super(resourceClass, informerConfig);
+    this.configurationService = configurationService;
+    this.name = ControllerConfiguration.ensureValidName(name, associatedReconcilerClassName);
+    this.generationAware = generationAware;
+    this.associatedReconcilerClassName = associatedReconcilerClassName;
+    this.retry = ensureRetry(retry);
+    this.rateLimiter = ensureRateLimiter(rateLimiter);
+    this.maxReconciliationInterval = maxReconciliationInterval;
+    this.configurations = configurations != null ? configurations : Collections.emptyMap();
+    this.finalizer =
+        ControllerConfiguration.ensureValidFinalizerName(finalizer, getResourceTypeName());
+    this.fieldManager = fieldManager;
+  }
+
+  protected ResolvedControllerConfiguration(Class<P> resourceClass, String name,
+      Class<? extends Reconciler> reconcilerClas, ConfigurationService configurationService) {
+    this(resourceClass, name, false, getAssociatedReconcilerClassName(reconcilerClas), null, null,
+        null, null, null, null, configurationService,
+        InformerConfigHolder.builder(resourceClass).buildForController());
   }
 
   public static Duration getMaxReconciliationInterval(long interval, TimeUnit timeUnit) {
@@ -64,55 +99,6 @@ public class ResolvedControllerConfiguration<P extends HasMetadata>
 
   protected RateLimiter ensureRateLimiter(RateLimiter given) {
     return given == null ? ControllerConfiguration.super.getRateLimiter() : given;
-  }
-
-  public ResolvedControllerConfiguration(Class<P> resourceClass, String name,
-      boolean generationAware, String associatedReconcilerClassName, Retry retry,
-      RateLimiter rateLimiter, Duration maxReconciliationInterval,
-      OnAddFilter<? super P> onAddFilter, OnUpdateFilter<? super P> onUpdateFilter,
-      GenericFilter<? super P> genericFilter,
-      Set<String> namespaces, String finalizer, String labelSelector,
-      Map<DependentResourceSpec, Object> configurations, ItemStore<P> itemStore,
-      String fieldManager,
-      ConfigurationService configurationService, Long informerListLimit,
-      WorkflowSpec workflowSpec) {
-    this(resourceClass, name, generationAware, associatedReconcilerClassName, retry, rateLimiter,
-        maxReconciliationInterval, onAddFilter, onUpdateFilter, genericFilter,
-        namespaces, finalizer, labelSelector, configurations, itemStore, fieldManager,
-        configurationService, informerListLimit);
-    setWorkflowSpec(workflowSpec);
-  }
-
-  protected ResolvedControllerConfiguration(Class<P> resourceClass, String name,
-      boolean generationAware, String associatedReconcilerClassName, Retry retry,
-      RateLimiter rateLimiter, Duration maxReconciliationInterval,
-      OnAddFilter<? super P> onAddFilter, OnUpdateFilter<? super P> onUpdateFilter,
-      GenericFilter<? super P> genericFilter,
-      Set<String> namespaces, String finalizer, String labelSelector,
-      Map<DependentResourceSpec, Object> configurations, ItemStore<P> itemStore,
-      String fieldManager,
-      ConfigurationService configurationService, Long informerListLimit) {
-    super(resourceClass, namespaces, labelSelector, onAddFilter, onUpdateFilter, genericFilter,
-        itemStore, informerListLimit);
-    this.configurationService = configurationService;
-    this.name = ControllerConfiguration.ensureValidName(name, associatedReconcilerClassName);
-    this.generationAware = generationAware;
-    this.associatedReconcilerClassName = associatedReconcilerClassName;
-    this.retry = ensureRetry(retry);
-    this.rateLimiter = ensureRateLimiter(rateLimiter);
-    this.maxReconciliationInterval = maxReconciliationInterval;
-    this.configurations = configurations != null ? configurations : Collections.emptyMap();
-    this.itemStore = itemStore;
-    this.finalizer =
-        ControllerConfiguration.ensureValidFinalizerName(finalizer, getResourceTypeName());
-    this.fieldManager = fieldManager;
-  }
-
-  protected ResolvedControllerConfiguration(Class<P> resourceClass, String name,
-      Class<? extends Reconciler> reconcilerClas, ConfigurationService configurationService) {
-    this(resourceClass, name, false, getAssociatedReconcilerClassName(reconcilerClas), null, null,
-        null, null, null, null, null,
-        null, null, null, null, null, configurationService, null);
   }
 
   @Override
@@ -175,11 +161,6 @@ public class ResolvedControllerConfiguration<P extends HasMetadata>
       config = spec.getConfiguration().orElse(null);
     }
     return (C) config;
-  }
-
-  @Override
-  public Optional<ItemStore<P>> getItemStore() {
-    return Optional.ofNullable(itemStore);
   }
 
   @Override
