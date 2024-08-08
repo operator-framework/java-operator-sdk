c62e234d5fcace4404c6f9baf1350ec182ac77b8
Attila Mészáros <a_meszaros@apple.com>
2024-08-08 18:09:37 +0200

    old service
    Signed-off-by: Attila Mészáros <a_meszaros@apple.com>

--- operator-framework/src/test/java/io/javaoperatorsdk/operator/workflow/complexdependent/dependent/BaseService.java
+++ operator-framework/src/test/java/io/javaoperatorsdk/operator/workflow/complexdependent/dependent/BaseService.java
@@ -18,7 +18,7 @@ public abstract class BaseService extends BaseDependentResource<Service> {
   protected Service desired(ComplexWorkflowCustomResource primary,
       Context<ComplexWorkflowCustomResource> context) {
     var template = ReconcilerUtils.loadYaml(Service.class, getClass(),
-        "/io/javaoperatorsdk/operator/service.yaml");
+        "/io/javaoperatorsdk/operator/workflow/complexdependent/service.yaml");
 
     return new ServiceBuilder(template)
         .withMetadata(createMeta(primary).build())
