872400cc9f9cd0c7f22e231259f3ad092b834928
Chris Laprun <metacosm@users.noreply.github.com>
2024-08-19 15:26:24 +0200

    refactor: clean up ResourceConfiguration & rename to Informable (#2486)
    ---------
    
    Signed-off-by: Attila Mészáros <csviri@gmail.com>
    Signed-off-by: Chris Laprun <claprun@redhat.com>
    Signed-off-by: Attila Mészáros <a_meszaros@apple.com>
    Co-authored-by: Attila Mészáros <csviri@gmail.com>
    Co-authored-by: Attila Mészáros <a_meszaros@apple.com>

---
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/api/config/Informable.java
@@ -0,0 +1,18 @@
+package io.javaoperatorsdk.operator.api.config;
+
+
+import io.fabric8.kubernetes.api.model.HasMetadata;
+import io.javaoperatorsdk.operator.api.config.informer.InformerConfiguration;
+
+public interface Informable<R extends HasMetadata> {
+
+  default String getResourceTypeName() {
+    return getInformerConfig().getResourceTypeName();
+  }
+
+  InformerConfiguration<R> getInformerConfig();
+
+  default Class<R> getResourceClass() {
+    return getInformerConfig().getResourceClass();
+  }
+}
