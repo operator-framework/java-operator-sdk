59ff1d83445399081deaa798d789cb2f765cf950
Chris Laprun <claprun@redhat.com>
2024-10-16 17:21:53 +0200

    refactor: simplify workflow handling
    Signed-off-by: Chris Laprun <claprun@redhat.com>

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/dependent/workflow/DefaultWorkflow.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/dependent/workflow/DefaultWorkflow.java
@@ -1,5 +1,6 @@
 package io.javaoperatorsdk.operator.processing.dependent.workflow;
 
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -29,11 +30,11 @@ class DefaultWorkflow<P extends HasMetadata> implements Workflow<P> {
   private final boolean throwExceptionAutomatically;
   private final boolean hasCleaner;
 
-  DefaultWorkflow(Set<DependentResourceNode> dependentResourceNodes) {
+  DefaultWorkflow(Collection<DependentResourceNode> dependentResourceNodes) {
     this(dependentResourceNodes, THROW_EXCEPTION_AUTOMATICALLY_DEFAULT, false);
   }
 
-  DefaultWorkflow(Set<DependentResourceNode> dependentResourceNodes,
+  DefaultWorkflow(Collection<DependentResourceNode> dependentResourceNodes,
       boolean throwExceptionAutomatically,
       boolean hasCleaner) {
     this.throwExceptionAutomatically = throwExceptionAutomatically;
@@ -62,7 +63,7 @@ class DefaultWorkflow<P extends HasMetadata> implements Workflow<P> {
   }
 
   @SuppressWarnings("unchecked")
-  private Map<String, DependentResourceNode> toMap(Set<DependentResourceNode> nodes) {
+  private Map<String, DependentResourceNode> toMap(Collection<DependentResourceNode> nodes) {
     if (nodes == null || nodes.isEmpty()) {
       return Collections.emptyMap();
     }
@@ -77,7 +78,7 @@ class DefaultWorkflow<P extends HasMetadata> implements Workflow<P> {
           bottomLevelResource.remove(dependsOn);
         }
       }
-      map.put(node.getDependentResource().name(), node);
+      map.put(node.name(), node);
     }
     if (topLevelResources.isEmpty()) {
       throw new IllegalStateException(
@@ -156,5 +157,9 @@ class DefaultWorkflow<P extends HasMetadata> implements Workflow<P> {
         .filter(n -> n.getActivationCondition().isEmpty())
         .map(DependentResourceNode::getDependentResource)
         .toList();
+  }
+
+  Map<String, DependentResourceNode> getDependentResourceNodes() {
+    return dependentResourceNodes;
   }
 }
