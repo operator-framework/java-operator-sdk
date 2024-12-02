1cb59d5f339e80de73cea8dd25758110cb3c656a
Chris Laprun <metacosm@users.noreply.github.com>
2024-12-02 16:01:54 +0100

    chore: update to Fabric8 7.0-SNAPSHOT & replace APT by maven plugin (#2613)
    * chore: update to Fabric8 7.0-SNAPSHOT & replace APT by maven plugin
    
    Signed-off-by: Bernhard Strähle <bernhard.straehle@gmail.com>
    Signed-off-by: Chris Laprun <claprun@redhat.com>
    
    * chore(docs): add Fabric8 7 migration information
    
    Signed-off-by: Chris Laprun <claprun@redhat.com>
    
    ---------
    
    Signed-off-by: Bernhard Strähle <bernhard.straehle@gmail.com>
    Signed-off-by: Chris Laprun <claprun@redhat.com>

--- operator-framework-core/src/test/java/io/javaoperatorsdk/operator/api/config/VersionTest.java
+++ operator-framework-core/src/test/java/io/javaoperatorsdk/operator/api/config/VersionTest.java
@@ -2,7 +2,7 @@ package io.javaoperatorsdk.operator.api.config;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class VersionTest {
 
