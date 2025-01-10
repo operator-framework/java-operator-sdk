67993de79856850092931b6a081c0ea7cdf700ff
Chris Laprun <metacosm@users.noreply.github.com>
2025-01-10 11:48:20 +0100

    fix: ensure manually defined CRDs are considered before generated ones (#2662)
    * fix: ensure manually defined CRDs are considered before generated ones
    
    Fixes #2658
    
    Signed-off-by: Chris Laprun <claprun@redhat.com>
    
    * fix: output more details in logs on errors / failures
    
    Signed-off-by: Chris Laprun <claprun@redhat.com>
    
    * wip: add more logging
    
    Signed-off-by: Chris Laprun <claprun@redhat.com>
    
    * fix: add missing file
    
    Signed-off-by: Chris Laprun <claprun@redhat.com>
    
    * fix: make sure generated CRDs are applied
    
    Signed-off-by: Chris Laprun <claprun@redhat.com>
    
    ---------
    
    Signed-off-by: Chris Laprun <claprun@redhat.com>

---
+++ operator-framework-junit5/src/test/java/io/javaoperatorsdk/operator/junit/LocallyRunOperatorExtensionTest.java
@@ -0,0 +1,26 @@
+package io.javaoperatorsdk.operator.junit;
+
+import java.nio.file.Path;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import io.fabric8.kubernetes.client.KubernetesClientBuilder;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LocallyRunOperatorExtensionTest {
+
+  @Test
+  void getAdditionalCRDsFromFiles() {
+    System.out.println(Path.of("").toAbsolutePath());
+    System.out.println(Path.of("src/test/crd/test.crd").toAbsolutePath());
+    final var crds = LocallyRunOperatorExtension.getAdditionalCRDsFromFiles(
+        List.of("src/test/resources/crd/test.crd", "src/test/crd/test.crd"),
+        new KubernetesClientBuilder().build());
+    assertNotNull(crds);
+    assertEquals(2, crds.size());
+    assertEquals("src/test/crd/test.crd", crds.get("externals.crd.example"));
+    assertEquals("src/test/resources/crd/test.crd", crds.get("tests.crd.example"));
+  }
+}
