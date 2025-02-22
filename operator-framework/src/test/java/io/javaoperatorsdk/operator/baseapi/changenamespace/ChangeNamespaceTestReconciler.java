691e0228d27e84cd9e829b1274cf1ecfb300905b
Attila Mészáros <a_meszaros@apple.com>
2025-02-22 10:16:27 +0100

    fix
    Signed-off-by: Attila Mészáros <a_meszaros@apple.com>

--- operator-framework/src/test/java/io/javaoperatorsdk/operator/baseapi/changenamespace/ChangeNamespaceTestReconciler.java
+++ operator-framework/src/test/java/io/javaoperatorsdk/operator/baseapi/changenamespace/ChangeNamespaceTestReconciler.java
@@ -46,12 +46,16 @@ public class ChangeNamespaceTestReconciler
     }
 
     increaseNumberOfResourceExecutions(primary);
-    if (primary.getStatus() == null) {
-      primary.setStatus(new ChangeNamespaceTestCustomResourceStatus());
-    }
+
+    var statusPatchResource = new ChangeNamespaceTestCustomResource();
+    statusPatchResource.setMetadata(new ObjectMetaBuilder()
+                    .withName(primary.getMetadata().getName())
+                    .withNamespace(primary.getMetadata().getNamespace())
+            .build());
+    statusPatchResource.setStatus(new ChangeNamespaceTestCustomResourceStatus());
     var statusUpdates = primary.getStatus().getNumberOfStatusUpdates();
-    primary.getStatus().setNumberOfStatusUpdates(statusUpdates + 1);
-    return UpdateControl.patchStatus(primary);
+    statusPatchResource.getStatus().setNumberOfStatusUpdates(statusUpdates + 1);
+    return UpdateControl.patchStatus(statusPatchResource);
   }
 
   private void increaseNumberOfResourceExecutions(ChangeNamespaceTestCustomResource primary) {
