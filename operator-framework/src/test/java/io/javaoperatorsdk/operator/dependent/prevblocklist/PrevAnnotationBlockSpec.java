74c13ffc4765c4ffbfbc2ff6fac683ec16dc55dd
Attila Mészáros <a_meszaros@apple.com>
2025-04-29 14:59:11 +0200

    Add integration test
    Signed-off-by: Attila Mészáros <a_meszaros@apple.com>

---
+++ operator-framework/src/test/java/io/javaoperatorsdk/operator/dependent/prevblocklist/PrevAnnotationBlockSpec.java
@@ -0,0 +1,15 @@
+package io.javaoperatorsdk.operator.dependent.prevblocklist;
+
+public class PrevAnnotationBlockSpec {
+
+  private String value;
+
+  public String getValue() {
+    return value;
+  }
+
+  public PrevAnnotationBlockSpec setValue(String value) {
+    this.value = value;
+    return this;
+  }
+}
