456c68d9ee0b8a22905c93d92704c63e22be4e90
Attila Mészáros <a_meszaros@apple.com>
2025-06-27 14:43:03 +0200

    wip
    Signed-off-by: Attila Mészáros <a_meszaros@apple.com>

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/api/reconciler/expectation/ExpectationResult.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/api/reconciler/expectation/ExpectationResult.java
@@ -1,8 +1,12 @@
 package io.javaoperatorsdk.operator.api.reconciler.expectation;
 
-public class ExpectationResult {
+import io.fabric8.kubernetes.api.model.HasMetadata;
+
+public class ExpectationResult<P extends HasMetadata> {
 
   private ExpectationStatus status;
+
+  private Expectation<P> expectation;
 
   public ExpectationResult(ExpectationStatus status) {
     this.status = status;
@@ -10,5 +14,9 @@ public class ExpectationResult {
 
   public ExpectationStatus getStatus() {
     return status;
+  }
+
+  public Expectation<P> getExpectation() {
+    return expectation;
   }
 }
