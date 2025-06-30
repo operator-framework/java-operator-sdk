d9f299aa2b77cbef04b9ac8fee50d64b01476ba4
Attila Mészáros <a_meszaros@apple.com>
2025-06-30 16:05:03 +0200

    ssa issue reproducer
    Signed-off-by: Attila Mészáros <a_meszaros@apple.com>

---
+++ operator-framework/src/test/java/io/javaoperatorsdk/operator/baseapi/ssaissue/SSAFinalizerIssueSpec.java
@@ -0,0 +1,26 @@
+package io.javaoperatorsdk.operator.baseapi.ssaissue;
+
+import java.util.List;
+
+public class SSAFinalizerIssueSpec {
+
+  private String value;
+
+  private List<String> list;
+
+  public String getValue() {
+    return value;
+  }
+
+  public void setValue(String value) {
+    this.value = value;
+  }
+
+  public List<String> getList() {
+    return list;
+  }
+
+  public void setList(List<String> list) {
+    this.list = list;
+  }
+}
