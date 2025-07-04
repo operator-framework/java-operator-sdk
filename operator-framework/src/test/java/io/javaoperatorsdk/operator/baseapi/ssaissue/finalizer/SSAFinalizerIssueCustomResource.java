3442e49b6cb5e7023c31637c1e107476f88773c5
Attila Mészáros <a_meszaros@apple.com>
2025-07-04 13:52:12 +0200

    feat: showcase initilized list cleanup when adding finalizer with SSA
    Signed-off-by: Attila Mészáros <a_meszaros@apple.com>

--- operator-framework/src/test/java/io/javaoperatorsdk/operator/baseapi/ssaissue/finalizer/SSAFinalizerIssueCustomResource.java
+++ operator-framework/src/test/java/io/javaoperatorsdk/operator/baseapi/ssaissue/finalizer/SSAFinalizerIssueCustomResource.java
@@ -1,4 +1,4 @@
-package io.javaoperatorsdk.operator.baseapi.ssaissue;
+package io.javaoperatorsdk.operator.baseapi.ssaissue.finalizer;
 
 import io.fabric8.kubernetes.api.model.Namespaced;
 import io.fabric8.kubernetes.client.CustomResource;
