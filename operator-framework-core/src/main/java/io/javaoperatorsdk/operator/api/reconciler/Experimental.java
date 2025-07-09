8f47e51886f6124243f2ecd19146795366dda5a8
Attila Mészáros <a_meszaros@apple.com>
2025-07-09 12:58:11 +0200

    format
    Signed-off-by: Attila Mészáros <a_meszaros@apple.com>

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/api/reconciler/Experimental.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/api/reconciler/Experimental.java
@@ -5,15 +5,12 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-
 /**
  * Marks experimental features.
  *
- * Experimental features are not yet stable and may change in future releases. Usually based
- * on the feedback of the users.
- *
- * */
+ * <p>Experimental features are not yet stable and may change in future releases. Usually based on
+ * the feedback of the users.
+ */
 @Retention(RetentionPolicy.SOURCE)
 @Target({ElementType.METHOD, ElementType.TYPE})
-public @interface Experimental {
-}
+public @interface Experimental {}
