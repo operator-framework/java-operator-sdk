58bab122abd642425ea0e38b84c04b303cea6997
Attila Mészáros <a_meszaros@apple.com>
2025-08-15 11:10:41 +0200

    fix: create state only on resource event
    In general we cleanup caches on delete event, so although in practice this probably not causing
    issues in theory it can happen that we receive events where there is no related custom resource.
    In that case we would create state, what can lead to a memory leak.
    
    Signed-off-by: Attila Mészáros <a_meszaros@apple.com>

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/event/ResourceStateManager.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/event/ResourceStateManager.java
@@ -2,14 +2,32 @@ package io.javaoperatorsdk.operator.processing.event;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
+
+import io.javaoperatorsdk.operator.processing.event.source.controller.ResourceEvent;
 
 class ResourceStateManager {
   // maybe we should have a way for users to specify a hint on the amount of CRs their reconciler
   // will process to avoid under- or over-sizing the state maps and avoid too many resizing that
   // take time and memory?
   private final Map<ResourceID, ResourceState> states = new ConcurrentHashMap<>(100);
+
+  public Optional<ResourceState> getOrCreateOnResourceEvent(Event event) {
+    var resourceId = event.getRelatedCustomResourceID();
+    var state = states.get(event.getRelatedCustomResourceID());
+    if (state != null) {
+      return Optional.of(state);
+    }
+    if (event instanceof ResourceEvent) {
+      state = new ResourceState(resourceId);
+      states.put(resourceId, state);
+      return Optional.of(state);
+    } else {
+      return Optional.empty();
+    }
+  }
 
   public ResourceState getOrCreate(ResourceID resourceID) {
     return states.computeIfAbsent(resourceID, ResourceState::new);
