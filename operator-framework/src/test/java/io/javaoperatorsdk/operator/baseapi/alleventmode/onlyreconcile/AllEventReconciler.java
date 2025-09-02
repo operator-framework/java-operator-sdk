3c9ed9d601d0b591004110c6314af4ddca449ac7
Attila Mészáros <a_meszaros@apple.com>
2025-09-02 13:29:45 +0200

    wip
    Signed-off-by: Attila Mészáros <a_meszaros@apple.com>

--- operator-framework/src/test/java/io/javaoperatorsdk/operator/baseapi/alleventmode/onlyreconcile/AllEventReconciler.java
+++ operator-framework/src/test/java/io/javaoperatorsdk/operator/baseapi/alleventmode/onlyreconcile/AllEventReconciler.java
@@ -19,6 +19,15 @@ public class AllEventReconciler extends AbstractAllEventReconciler
 
   private static final Logger log = LoggerFactory.getLogger(AllEventReconciler.class);
 
+  private volatile boolean throwExceptionOnFirstDeleteEvent = false;
+  private volatile boolean throwExceptionIfNoAnnotation = false;
+
+  private volatile boolean waitAfterFirstRetry = false;
+  private volatile boolean continuerOnRetryWait = false;
+  private volatile boolean waiting = false;
+
+  private volatile boolean isFirstDeleteEvent = true;
+
   @Override
   public UpdateControl<AllEventCustomResource> reconcile(
       AllEventCustomResource primary, Context<AllEventCustomResource> context)
@@ -71,5 +80,53 @@ public class AllEventReconciler extends AbstractAllEventReconciler
     }
     log.info("Reconciliation finished");
     return UpdateControl.noUpdate();
+  }
+
+  public boolean isFirstDeleteEvent() {
+    return isFirstDeleteEvent;
+  }
+
+  public void setFirstDeleteEvent(boolean firstDeleteEvent) {
+    isFirstDeleteEvent = firstDeleteEvent;
+  }
+
+  public boolean isThrowExceptionOnFirstDeleteEvent() {
+    return throwExceptionOnFirstDeleteEvent;
+  }
+
+  public void setThrowExceptionOnFirstDeleteEvent(boolean throwExceptionOnFirstDeleteEvent) {
+    this.throwExceptionOnFirstDeleteEvent = throwExceptionOnFirstDeleteEvent;
+  }
+
+  public boolean isThrowExceptionIfNoAnnotation() {
+    return throwExceptionIfNoAnnotation;
+  }
+
+  public void setThrowExceptionIfNoAnnotation(boolean throwExceptionIfNoAnnotation) {
+    this.throwExceptionIfNoAnnotation = throwExceptionIfNoAnnotation;
+  }
+
+  public boolean isWaitAfterFirstRetry() {
+    return waitAfterFirstRetry;
+  }
+
+  public void setWaitAfterFirstRetry(boolean waitAfterFirstRetry) {
+    this.waitAfterFirstRetry = waitAfterFirstRetry;
+  }
+
+  public boolean isContinuerOnRetryWait() {
+    return continuerOnRetryWait;
+  }
+
+  public void setContinuerOnRetryWait(boolean continuerOnRetryWait) {
+    this.continuerOnRetryWait = continuerOnRetryWait;
+  }
+
+  public boolean isWaiting() {
+    return waiting;
+  }
+
+  public void setWaiting(boolean waiting) {
+    this.waiting = waiting;
   }
 }
