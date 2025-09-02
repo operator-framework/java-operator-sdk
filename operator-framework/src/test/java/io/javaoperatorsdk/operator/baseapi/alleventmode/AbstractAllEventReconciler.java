d7639847e132c61a2f85e73d9beec456064fbc7c
Attila Mészáros <a_meszaros@apple.com>
2025-09-02 13:29:45 +0200

    wip
    Signed-off-by: Attila Mészáros <a_meszaros@apple.com>

--- operator-framework/src/test/java/io/javaoperatorsdk/operator/baseapi/alleventmode/AbstractAllEventReconciler.java
+++ operator-framework/src/test/java/io/javaoperatorsdk/operator/baseapi/alleventmode/AbstractAllEventReconciler.java
@@ -9,27 +9,27 @@ public class AbstractAllEventReconciler {
   public static final String NO_MORE_EXCEPTION_ANNOTATION_KEY = "no.more.exception";
 
   protected volatile boolean useFinalizer = true;
-  protected volatile boolean throwExceptionOnFirstDeleteEvent = false;
-  protected volatile boolean throwExceptionIfNoAnnotation = false;
-
-  protected volatile boolean waitAfterFirstRetry = false;
-  protected volatile boolean continuerOnRetryWait = false;
-  protected volatile boolean waiting = false;
-
-  protected volatile boolean isFirstDeleteEvent = true;
-
-  private boolean resourceEventPresent = false;
-  private boolean deleteEventPresent = false;
-  private boolean eventOnMarkedForDeletion = false;
 
   private final AtomicInteger eventCounter = new AtomicInteger(0);
 
-  public boolean isResourceEventPresent() {
-    return resourceEventPresent;
+  private boolean deleteEventPresent = false;
+  private boolean eventOnMarkedForDeletion = false;
+  private boolean resourceEventPresent = false;
+
+  public int getEventCount() {
+    return eventCounter.get();
   }
 
-  public void setResourceEventPresent(boolean resourceEventPresent) {
-    this.resourceEventPresent = resourceEventPresent;
+  public void increaseEventCount() {
+    eventCounter.incrementAndGet();
+  }
+
+  public boolean getUseFinalizer() {
+    return useFinalizer;
+  }
+
+  public void setUseFinalizer(boolean useFinalizer) {
+    this.useFinalizer = useFinalizer;
   }
 
   public boolean isDeleteEventPresent() {
@@ -48,67 +48,11 @@ public class AbstractAllEventReconciler {
     this.eventOnMarkedForDeletion = eventOnMarkedForDeletion;
   }
 
-  public int getEventCount() {
-    return eventCounter.get();
+  public boolean isResourceEventPresent() {
+    return resourceEventPresent;
   }
 
-  public void increaseEventCount() {
-    eventCounter.incrementAndGet();
-  }
-
-  public boolean getUseFinalizer() {
-    return useFinalizer;
-  }
-
-  public void setUseFinalizer(boolean useFinalizer) {
-    this.useFinalizer = useFinalizer;
-  }
-
-  public boolean isFirstDeleteEvent() {
-    return isFirstDeleteEvent;
-  }
-
-  public void setFirstDeleteEvent(boolean firstDeleteEvent) {
-    isFirstDeleteEvent = firstDeleteEvent;
-  }
-
-  public boolean isThrowExceptionOnFirstDeleteEvent() {
-    return throwExceptionOnFirstDeleteEvent;
-  }
-
-  public void setThrowExceptionOnFirstDeleteEvent(boolean throwExceptionOnFirstDeleteEvent) {
-    this.throwExceptionOnFirstDeleteEvent = throwExceptionOnFirstDeleteEvent;
-  }
-
-  public boolean isThrowExceptionIfNoAnnotation() {
-    return throwExceptionIfNoAnnotation;
-  }
-
-  public void setThrowExceptionIfNoAnnotation(boolean throwExceptionIfNoAnnotation) {
-    this.throwExceptionIfNoAnnotation = throwExceptionIfNoAnnotation;
-  }
-
-  public boolean isWaitAfterFirstRetry() {
-    return waitAfterFirstRetry;
-  }
-
-  public void setWaitAfterFirstRetry(boolean waitAfterFirstRetry) {
-    this.waitAfterFirstRetry = waitAfterFirstRetry;
-  }
-
-  public boolean isContinuerOnRetryWait() {
-    return continuerOnRetryWait;
-  }
-
-  public void setContinuerOnRetryWait(boolean continuerOnRetryWait) {
-    this.continuerOnRetryWait = continuerOnRetryWait;
-  }
-
-  public boolean isWaiting() {
-    return waiting;
-  }
-
-  public void setWaiting(boolean waiting) {
-    this.waiting = waiting;
+  public void setResourceEventPresent(boolean resourceEventPresent) {
+    this.resourceEventPresent = resourceEventPresent;
   }
 }
