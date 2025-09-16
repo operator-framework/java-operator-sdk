84d7b9285b37c693a782b006323775d3f73a1f15
Attila Mészáros <a_meszaros@apple.com>
2025-09-16 15:14:06 +0200

    feat: add experimental annotation (#2853)
    Signed-off-by: Attila Mészáros <a_meszaros@apple.com>

---
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/api/reconciler/Experimental.java
@@ -0,0 +1,24 @@
+package io.javaoperatorsdk.operator.api.reconciler;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Marks experimental features.
+ *
+ * <p>Experimental features are not yet stable and may change in future releases. Usually based on
+ * the feedback of the users.
+ */
+@Retention(RetentionPolicy.SOURCE)
+@Target({ElementType.METHOD, ElementType.TYPE, ElementType.FIELD, ElementType.PACKAGE})
+public @interface Experimental {
+
+  /**
+   * Describes why the annotated element is experimental.
+   *
+   * @return the experimental description.
+   */
+  String value();
+}
