5953136d9db96c1c9ccf67d13be8a27a4700ca8b
Peter Chen <peternchen@gmail.com>
2025-09-20 21:01:12 -0400

    feat(docs): add @Sample annotation for documentation
    Marks integration test classes so that markdown pages can be auto generated.

---
+++ operator-annotations/src/main/java/io/javaoperatorsdk/Sample.java
@@ -0,0 +1,26 @@
+package io.javaoperatorsdk;
+
+import java.lang.annotation.*;
+
+/**
+ * This annotation marks an integration test class as a sample for the documentation.
+ * Intended for use on test classes only.
+ *
+ * <p><b>Example:</b>
+ * <pre>{@code
+ * @Sample(
+ *  tldr="Usage of PrimaryToSecondaryMapper",
+ *  description="Showcases the usage of PrimaryToSecondaryMapper, in what situation it needs to be used and how to optimize typical uses with Informer indexes."
+ * )
+ * class PrimaryToSecondaryIT {
+ *   // details omitted
+ * }
+ * }</pre>
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+@Documented
+public @interface Sample {
+    String tldr();
+    String description();
+}
