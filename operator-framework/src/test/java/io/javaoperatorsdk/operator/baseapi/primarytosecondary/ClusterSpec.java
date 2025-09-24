7397be67bd28314bd56b696b3f53b372955c4c53
Attila Mészáros <a_meszaros@apple.com>
2025-09-24 08:28:38 +0200

    improve: PrimaryToSecondayMapper test improvements (#2951)
    Signed-off-by: Attila Mészáros <a_meszaros@apple.com>

---
+++ operator-framework/src/test/java/io/javaoperatorsdk/operator/baseapi/primarytosecondary/ClusterSpec.java
@@ -0,0 +1,14 @@
+package io.javaoperatorsdk.operator.baseapi.primarytosecondary;
+
+public class ClusterSpec {
+
+  private String clusterValue;
+
+  public String getClusterValue() {
+    return clusterValue;
+  }
+
+  public void setClusterValue(String clusterValue) {
+    this.clusterValue = clusterValue;
+  }
+}
