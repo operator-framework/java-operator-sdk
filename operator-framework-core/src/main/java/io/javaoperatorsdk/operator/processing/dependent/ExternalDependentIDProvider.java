f9fa4d9bf412235672d009ae4999e1612d40ca50
Attila Mészáros <a_meszaros@apple.com>
2025-09-30 10:47:55 +0200

    wip
    Signed-off-by: Attila Mészáros <a_meszaros@apple.com>

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/dependent/ExternalDependentIDProvider.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/dependent/ExternalDependentIDProvider.java
@@ -1,5 +1,12 @@
 package io.javaoperatorsdk.operator.processing.dependent;
 
+/**
+ * Provides the identifier for a bean that represents and external resource. This ID is used to
+ * select target resource for a dependent resource from the resources returned by `{@link
+ * io.javaoperatorsdk.operator.api.reconciler.Context#getSecondaryResources(Class)}`.
+ *
+ * @param <T>
+ */
 public interface ExternalDependentIDProvider<T> {
 
   T id();
