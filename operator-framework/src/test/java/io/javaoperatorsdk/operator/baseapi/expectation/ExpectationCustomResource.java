7b8e100e2c22889d1b39fa3aed71dbebd1ed3db7
Attila Mészáros <a_meszaros@apple.com>
2025-09-30 10:01:03 +0200

    wip
    Signed-off-by: Attila Mészáros <a_meszaros@apple.com>

---
+++ operator-framework/src/test/java/io/javaoperatorsdk/operator/baseapi/expectation/ExpectationCustomResource.java
@@ -0,0 +1,12 @@
+package io.javaoperatorsdk.operator.baseapi.expectation;
+
+import io.fabric8.kubernetes.api.model.Namespaced;
+import io.fabric8.kubernetes.client.CustomResource;
+import io.fabric8.kubernetes.model.annotation.Group;
+import io.fabric8.kubernetes.model.annotation.ShortNames;
+import io.fabric8.kubernetes.model.annotation.Version;
+
+@Group("sample.javaoperatorsdk")
+@Version("v1")
+@ShortNames("ecr")
+public class ExpectationCustomResource extends CustomResource<Void, Void> implements Namespaced {}
