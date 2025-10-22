a25ddb132c23b6e30de37c524c31f6e885083696
Attila Mészáros <a_meszaros@apple.com>
2025-10-22 12:57:35 +0200

    improve: showcase issue where updating the spec with SSA removes the finalizer (#2848)
    Signed-off-by: Attila Mészáros <a_meszaros@apple.com>

---
+++ operator-framework/src/test/java/io/javaoperatorsdk/operator/baseapi/ssaissue/specupdate/SSASpecUpdateCustomResourceSpec.java
@@ -0,0 +1,14 @@
+package io.javaoperatorsdk.operator.baseapi.ssaissue.specupdate;
+
+public class SSASpecUpdateCustomResourceSpec {
+
+  private String value;
+
+  public String getValue() {
+    return value;
+  }
+
+  public void setValue(String value) {
+    this.value = value;
+  }
+}
---
+++ operator-framework/src/test/java/io/javaoperatorsdk/operator/baseapi/ssaissue/specupdate/SSASpecUpdateIT.java
@@ -0,0 +1,41 @@
+package io.javaoperatorsdk.operator.baseapi.ssaissue.specupdate;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.fabric8.kubernetes.api.model.ObjectMetaBuilder;
+import io.javaoperatorsdk.operator.junit.LocallyRunOperatorExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
+
+class SSASpecUpdateIT {
+
+  public static final String TEST_RESOURCE_NAME = "test";
+
+  @RegisterExtension
+  LocallyRunOperatorExtension operator =
+      LocallyRunOperatorExtension.builder().withReconciler(SSASpecUpdateReconciler.class).build();
+
+  // showcases that if the spec of the resources is updated with SSA, but the finalizer
+  // is not explicitly added to the fresh resource, the update removes the finalizer
+  @Test
+  void showFinalizerRemovalWhenSpecUpdated() {
+    SSASpecUpdateCustomResource res = createResource();
+    operator.create(res);
+
+    await()
+        .untilAsserted(
+            () -> {
+              var actual = operator.get(SSASpecUpdateCustomResource.class, TEST_RESOURCE_NAME);
+              assertThat(actual.getSpec()).isNotNull();
+              assertThat(actual.getFinalizers()).isEmpty();
+            });
+  }
+
+  SSASpecUpdateCustomResource createResource() {
+    SSASpecUpdateCustomResource res = new SSASpecUpdateCustomResource();
+    res.setMetadata(new ObjectMetaBuilder().withName(TEST_RESOURCE_NAME).build());
+    return res;
+  }
+}
