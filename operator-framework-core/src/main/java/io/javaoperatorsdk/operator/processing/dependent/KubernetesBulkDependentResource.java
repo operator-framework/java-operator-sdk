9e64e331692f17d6438892d7a9e550551b26e6f9
Attila Mészáros <a_meszaros@apple.com>
2025-10-24 13:57:54 +0200

    wip

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/dependent/KubernetesBulkDependentResource.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/dependent/KubernetesBulkDependentResource.java
@@ -26,9 +26,21 @@ import io.javaoperatorsdk.operator.api.reconciler.dependent.DependentResource;
 import io.javaoperatorsdk.operator.processing.ResourceIDMapper;
 import io.javaoperatorsdk.operator.processing.event.ResourceID;
 
+/**
+ * A narrowed interface for bulk dependent resources for Kubernetes resources, that assumes that the
+ * ID is a {@link ResourceID}. Note that you are not limited to this when dealing with Kubernetes
+ * resources you can still choose a different ID type and directly implement {@link
+ * BulkDependentResource}.
+ */
 public interface KubernetesBulkDependentResource<R extends HasMetadata, P extends HasMetadata>
     extends BulkDependentResource<R, P, ResourceID>, DependentResource<R, P> {
 
+  /**
+   * Since we can list all the related resources and by assuming the ID is type of {@link
+   * ResourceID} it is trivial to create the target map. The only issue is if there are other
+   * secondary resources of the target type which are not managed by this bulk dependent resources,
+   * for those it is enough to override secondaryResourceFilter method.
+   */
   @Override
   default Map<ResourceID, R> getSecondaryResources(P primary, Context<P> context) {
     return context
@@ -40,6 +52,10 @@ public interface KubernetesBulkDependentResource<R extends HasMetadata, P extend
                 Function.identity()));
   }
 
+  /**
+   * Override if not all the secondary resources of target type are managed by the bulk dependent
+   * resource.
+   */
   default Predicate<R> secondaryResourceFilter(P primary, Context<P> context) {
     return r -> true;
   }
