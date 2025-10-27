3dc12547073f853bb9a2a88e2cd17b5863bab122
Attila Mészáros <a_meszaros@apple.com>
2025-10-27 17:09:06 +0100

    Update operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/ResourceIDProvider.java
    Co-authored-by: Martin Stefanko <xstefank122@gmail.com>

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/ResourceIDProvider.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/ResourceIDProvider.java
@@ -16,7 +16,7 @@
 package io.javaoperatorsdk.operator.processing;
 
 /**
- * Provides the identifier for an object that represents resource. This ID is used to select target
+ * Provides the identifier for an object that represents a resource. This ID is used to select the target
  * external resource for a dependent resource from the resources returned by `{@link
  * io.javaoperatorsdk.operator.api.reconciler.Context#getSecondaryResources(Class)}`. But also for
  * {@link ResourceIDMapper} for event sources in external resources. But also for bulk dependent
