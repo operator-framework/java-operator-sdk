de30b105fec28355425f84516757812190c4a52e
Steven Hawkins <shawkins@redhat.com>
2025-10-30 08:22:09 -0400

    Annotation removal using locking (#3015)
    Signed-off-by: Steve Hawkins <shawkins@redhat.com>

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/event/source/informer/TemporaryResourceCache.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/event/source/informer/TemporaryResourceCache.java
@@ -15,16 +15,16 @@
  */
 package io.javaoperatorsdk.operator.processing.event.source.informer;
 
-import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.ReentrantLock;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import io.fabric8.kubernetes.api.model.HasMetadata;
-import io.javaoperatorsdk.operator.api.config.ConfigurationService;
+import io.javaoperatorsdk.operator.api.reconciler.PrimaryUpdateAndCacheUtils;
 import io.javaoperatorsdk.operator.processing.dependent.kubernetes.KubernetesDependentResource;
 import io.javaoperatorsdk.operator.processing.event.ResourceID;
 
@@ -33,157 +33,152 @@ import io.javaoperatorsdk.operator.processing.event.ResourceID;
  * a create or update is executed the subsequent getResource operation might not return the
  * up-to-date resource from informer cache, since it is not received yet.
  *
- * <p>The idea of the solution is, that since an update (for create is simpler) was done
- * successfully, and optimistic locking is in place, there were no other operations between reading
- * the resource from the cache and the actual update. So when the new resource is stored in the
- * temporal cache only if the informer still has the previous resource version, from before the
- * update. If not, that means there were already updates on the cache (either by the actual update
- * from DependentResource or other) so the resource does not needs to be cached. Subsequently if
- * event received from the informer, it means that the cache of the informer was updated, so it
- * already contains a more fresh version of the resource.
+ * <p>Since an update (for create is simpler) was done successfully we can temporarily track that
+ * resource if its version is later than the events we've processed. We then know that we can skip
+ * all events that have the same resource version or earlier than the tracked resource. Once we
+ * process an event that has the same resource version or later, then we know the tracked resource
+ * can be removed.
+ *
+ * <p>In some cases it is possible for the informer to deliver events prior to the attempt to put
+ * the resource in the temporal cache. The startModifying/doneModifying methods are used to pause
+ * event delivery to ensure that temporal cache recognizes the put entry as an event that can be
+ * skipped.
+ *
+ * <p>If comparable resource versions are disabled, then this cache is effectively disabled.
  *
  * @param <T> resource to cache.
  */
 public class TemporaryResourceCache<T extends HasMetadata> {
 
-  static class ExpirationCache<K> {
-    private final LinkedHashMap<K, Long> cache;
-    private final int ttlMs;
-
-    public ExpirationCache(int maxEntries, int ttlMs) {
-      this.ttlMs = ttlMs;
-      this.cache =
-          new LinkedHashMap<>() {
-            @Override
-            protected boolean removeEldestEntry(Map.Entry<K, Long> eldest) {
-              return size() > maxEntries;
-            }
-          };
-    }
-
-    public void add(K key) {
-      clean();
-      cache.putIfAbsent(key, System.currentTimeMillis());
-    }
-
-    public boolean contains(K key) {
-      clean();
-      return cache.get(key) != null;
-    }
-
-    void clean() {
-      if (!cache.isEmpty()) {
-        long currentTimeMillis = System.currentTimeMillis();
-        var iter = cache.entrySet().iterator();
-        // the order will already be from oldest to newest, clean a fixed number of entries to
-        // amortize the cost amongst multiple calls
-        for (int i = 0; i < 10 && iter.hasNext(); i++) {
-          var entry = iter.next();
-          if (currentTimeMillis - entry.getValue() > ttlMs) {
-            iter.remove();
-          }
-        }
-      }
-    }
-  }
-
   private static final Logger log = LoggerFactory.getLogger(TemporaryResourceCache.class);
 
   private final Map<ResourceID, T> cache = new ConcurrentHashMap<>();
+  private final boolean comparableResourceVersions;
+  private final Map<ResourceID, ReentrantLock> activelyModifying = new ConcurrentHashMap<>();
+  private String latestResourceVersion;
 
-  // keep up to the last million deletions for up to 10 minutes
-  private final ExpirationCache<String> tombstones = new ExpirationCache<>(1000000, 1200000);
-  private final ManagedInformerEventSource<T, ?, ?> managedInformerEventSource;
-  private final boolean parseResourceVersions;
-
-  public TemporaryResourceCache(
-      ManagedInformerEventSource<T, ?, ?> managedInformerEventSource,
-      boolean parseResourceVersions) {
-    this.managedInformerEventSource = managedInformerEventSource;
-    this.parseResourceVersions = parseResourceVersions;
+  public TemporaryResourceCache(boolean comparableResourceVersions) {
+    this.comparableResourceVersions = comparableResourceVersions;
   }
 
-  public synchronized void onDeleteEvent(T resource, boolean unknownState) {
-    tombstones.add(resource.getMetadata().getUid());
+  public void startModifying(ResourceID id) {
+    if (!comparableResourceVersions) {
+      return;
+    }
+    activelyModifying
+        .compute(
+            id,
+            (ignored, lock) -> {
+              if (lock != null) {
+                throw new IllegalStateException(); // concurrent modifications to the same resource
+                // not allowed - this could be relaxed if needed
+              }
+              return new ReentrantLock();
+            })
+        .lock();
+  }
+
+  public void doneModifying(ResourceID id) {
+    if (!comparableResourceVersions) {
+      return;
+    }
+    activelyModifying.computeIfPresent(
+        id,
+        (ignored, lock) -> {
+          lock.unlock();
+          return null;
+        });
+  }
+
+  public void onDeleteEvent(T resource, boolean unknownState) {
     onEvent(resource, unknownState);
   }
 
-  public synchronized void onAddOrUpdateEvent(T resource) {
-    onEvent(resource, false);
-  }
-
-  synchronized void onEvent(T resource, boolean unknownState) {
-    cache.computeIfPresent(
-        ResourceID.fromResource(resource),
-        (id, cached) ->
-            (unknownState || !isLaterResourceVersion(id, cached, resource)) ? null : cached);
-  }
-
-  public synchronized void putAddedResource(T newResource) {
-    putResource(newResource, null);
-  }
-
   /**
-   * put the item into the cache if the previousResourceVersion matches the current state. If not
-   * the currently cached item is removed.
-   *
-   * @param previousResourceVersion null indicates an add
+   * @return true if the resourceVersion was already known
    */
-  public synchronized void putResource(T newResource, String previousResourceVersion) {
-    var resourceId = ResourceID.fromResource(newResource);
-    var cachedResource = managedInformerEventSource.get(resourceId).orElse(null);
+  public boolean onAddOrUpdateEvent(T resource) {
+    return onEvent(resource, false);
+  }
 
-    boolean moveAhead = false;
-    if (previousResourceVersion == null && cachedResource == null) {
-      if (tombstones.contains(newResource.getMetadata().getUid())) {
-        log.debug(
-            "Won't resurrect uid {} for resource id: {}",
-            newResource.getMetadata().getUid(),
-            resourceId);
-        return;
+  private boolean onEvent(T resource, boolean unknownState) {
+    ReentrantLock lock = activelyModifying.get(ResourceID.fromResource(resource));
+    if (lock != null) {
+      lock.lock(); // wait for the modification to finish
+      lock.unlock(); // simply unlock as the event is guaranteed after the modification
+    }
+    boolean[] known = new boolean[1];
+    synchronized (this) {
+      if (!unknownState) {
+        latestResourceVersion = resource.getMetadata().getResourceVersion();
       }
-      // we can skip further checks as this is a simple add and there's no previous entry to
-      // consider
-      moveAhead = true;
+      cache.computeIfPresent(
+          ResourceID.fromResource(resource),
+          (id, cached) -> {
+            boolean remove = unknownState;
+            if (!unknownState) {
+              int comp = PrimaryUpdateAndCacheUtils.compareResourceVersions(resource, cached);
+              if (comp >= 0) {
+                remove = true;
+              }
+              if (comp <= 0) {
+                known[0] = true;
+              }
+            }
+            if (remove) {
+              return null;
+            }
+            return cached;
+          });
+      return known[0];
+    }
+  }
+
+  /** put the item into the cache if it's for a later state than what has already been observed. */
+  public synchronized void putResource(T newResource) {
+    if (!comparableResourceVersions) {
+      return;
     }
 
-    if (moveAhead
-        || (cachedResource != null
-                && (cachedResource
-                    .getMetadata()
-                    .getResourceVersion()
-                    .equals(previousResourceVersion))
-            || isLaterResourceVersion(resourceId, newResource, cachedResource))) {
+    var resourceId = ResourceID.fromResource(newResource);
+
+    if (newResource.getMetadata().getResourceVersion() == null) {
+      log.warn(
+          "Resource {}: with no resourceVersion put in temporary cache. This is not the expected"
+              + " usage pattern, only resources returned from the api server should be put in the"
+              + " cache.",
+          resourceId);
+      return;
+    }
+
+    // check against the latestResourceVersion processed by the TemporaryResourceCache
+    // If the resource is older, then we can safely ignore.
+    //
+    // this also prevents resurrecting recently deleted entities for which the delete event
+    // has already been processed
+    if (latestResourceVersion != null
+        && PrimaryUpdateAndCacheUtils.compareResourceVersions(
+                latestResourceVersion, newResource.getMetadata().getResourceVersion())
+            > 0) {
+      log.debug(
+          "Resource {}: resourceVersion {} is not later than latest {}",
+          resourceId,
+          newResource.getMetadata().getResourceVersion(),
+          latestResourceVersion);
+      return;
+    }
+
+    // also make sure that we're later than the existing temporary entry
+    var cachedResource = getResourceFromCache(resourceId).orElse(null);
+
+    if (cachedResource == null
+        || PrimaryUpdateAndCacheUtils.compareResourceVersions(newResource, cachedResource) > 0) {
       log.debug(
           "Temporarily moving ahead to target version {} for resource id: {}",
           newResource.getMetadata().getResourceVersion(),
           resourceId);
       cache.put(resourceId, newResource);
-    } else if (cache.remove(resourceId) != null) {
-      log.debug("Removed an obsolete resource from cache for id: {}", resourceId);
     }
-  }
-
-  /**
-   * @return true if {@link ConfigurationService#parseResourceVersionsForEventFilteringAndCaching()}
-   *     is enabled and the resourceVersion of newResource is numerically greater than
-   *     cachedResource, otherwise false
-   */
-  public boolean isLaterResourceVersion(ResourceID resourceId, T newResource, T cachedResource) {
-    try {
-      if (parseResourceVersions
-          && Long.parseLong(newResource.getMetadata().getResourceVersion())
-              > Long.parseLong(cachedResource.getMetadata().getResourceVersion())) {
-        return true;
-      }
-    } catch (NumberFormatException e) {
-      log.debug(
-          "Could not compare resourceVersions {} and {} for {}",
-          newResource.getMetadata().getResourceVersion(),
-          cachedResource.getMetadata().getResourceVersion(),
-          resourceId);
-    }
-    return false;
   }
 
   public synchronized Optional<T> getResourceFromCache(ResourceID resourceID) {
