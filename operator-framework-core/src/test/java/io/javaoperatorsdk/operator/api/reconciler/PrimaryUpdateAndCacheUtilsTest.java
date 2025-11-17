23f15ed4abb14fad79440413dc323cfe1f8742c3
Attila Mészáros <a_meszaros@apple.com>
2025-11-17 17:39:11 +0100

    improve: run pr-s checks for v5.3 (#3042)
    Signed-off-by: Attila Mészáros <a_meszaros@apple.com>

--- operator-framework-core/src/test/java/io/javaoperatorsdk/operator/api/reconciler/PrimaryUpdateAndCacheUtilsTest.java
+++ operator-framework-core/src/test/java/io/javaoperatorsdk/operator/api/reconciler/PrimaryUpdateAndCacheUtilsTest.java
@@ -180,4 +180,53 @@ class PrimaryUpdateAndCacheUtilsTest {
                     10L));
     assertThat(ex.getMessage()).contains("Timeout");
   }
+
+  @Test
+  public void compareResourceVersionsTest() {
+    assertThat(compareResourceVersions("11", "22")).isNegative();
+    assertThat(compareResourceVersions("22", "11")).isPositive();
+    assertThat(compareResourceVersions("1", "1")).isZero();
+    assertThat(compareResourceVersions("11", "11")).isZero();
+    assertThat(compareResourceVersions("123", "2")).isPositive();
+    assertThat(compareResourceVersions("3", "211")).isNegative();
+
+    assertThrows(
+        NonComparableResourceVersionException.class, () -> compareResourceVersions("aa", "22"));
+    assertThrows(
+        NonComparableResourceVersionException.class, () -> compareResourceVersions("11", "ba"));
+    assertThrows(
+        NonComparableResourceVersionException.class, () -> compareResourceVersions("", "22"));
+    assertThrows(
+        NonComparableResourceVersionException.class, () -> compareResourceVersions("11", ""));
+    assertThrows(
+        NonComparableResourceVersionException.class, () -> compareResourceVersions("01", "123"));
+    assertThrows(
+        NonComparableResourceVersionException.class, () -> compareResourceVersions("123", "01"));
+    assertThrows(
+        NonComparableResourceVersionException.class, () -> compareResourceVersions("3213", "123a"));
+    assertThrows(
+        NonComparableResourceVersionException.class, () -> compareResourceVersions("321", "123a"));
+  }
+
+  // naive performance test that compares the work case scenario for the parsing and non-parsing
+  // variants
+  @Test
+  @Disabled
+  public void compareResourcePerformanceTest() {
+    var execNum = 30000000;
+    var startTime = System.currentTimeMillis();
+    for (int i = 0; i < execNum; i++) {
+      var res = compareResourceVersions("123456788", "123456789");
+    }
+    var dur1 = System.currentTimeMillis() - startTime;
+    log.info("Duration without parsing: {}", dur1);
+    startTime = System.currentTimeMillis();
+    for (int i = 0; i < execNum; i++) {
+      var res = Long.parseLong("123456788") > Long.parseLong("123456789");
+    }
+    var dur2 = System.currentTimeMillis() - startTime;
+    log.info("Duration with parsing:   {}", dur2);
+
+    assertThat(dur1).isLessThan(dur2);
+  }
 }
