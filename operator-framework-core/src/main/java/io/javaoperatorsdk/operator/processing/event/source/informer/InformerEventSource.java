bcd62f956569cea49025370e964d42e459e8af8a
Attila Mészáros <a_meszaros@apple.com>
2025-12-25 18:47:03 +0100

    wip
    Signed-off-by: Attila Mészáros <a_meszaros@apple.com>

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/event/source/informer/InformerEventSource.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/event/source/informer/InformerEventSource.java
@@ -96,22 +96,6 @@ public class InformerEventSource<R extends HasMetadata, P extends HasMetadata>
     genericFilter = informerConfig.getGenericFilter();
   }
 
-  public R updateAndCacheResource(
-      R resourceToUpdate, Context<?> context, UnaryOperator<R> updateMethod) {
-    ResourceID id = ResourceID.fromResource(resourceToUpdate);
-    if (log.isDebugEnabled()) {
-      log.debug("Update and cache: {}", id);
-    }
-    try {
-      temporaryResourceCache.startModifying(id);
-      var updated = updateMethod.apply(resourceToUpdate);
-      handleRecentResourceUpdate(id, updated, resourceToUpdate);
-      return updated;
-    } finally {
-      temporaryResourceCache.doneModifying(id);
-    }
-  }
-
   @Override
   public void onAdd(R newResource) {
     if (log.isDebugEnabled()) {
