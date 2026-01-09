ca2e47e29677a8d6e5ab843ad33955143469d9f1
Attila Mészáros <a_meszaros@apple.com>
2026-01-09 14:32:46 +0100

    wip
    Signed-off-by: Attila Mészáros <a_meszaros@apple.com>

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/api/reconciler/BaseControl.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/api/reconciler/BaseControl.java
@@ -25,20 +25,44 @@ public abstract class BaseControl<T extends BaseControl<T>> {
 
   private Long scheduleDelay = null;
 
+  /**
+   * Schedules a reconciliation to occur after the specified delay in milliseconds.
+   *
+   * @param delay the delay in milliseconds after which to reschedule
+   * @return this control instance for fluent chaining
+   */
   public T rescheduleAfter(long delay) {
     rescheduleAfter(Duration.ofMillis(delay));
     return (T) this;
   }
 
+  /**
+   * Schedules a reconciliation to occur after the specified delay.
+   *
+   * @param delay the {@link Duration} after which to reschedule
+   * @return this control instance for fluent chaining
+   */
   public T rescheduleAfter(Duration delay) {
     this.scheduleDelay = delay.toMillis();
     return (T) this;
   }
 
+  /**
+   * Schedules a reconciliation to occur after the specified delay using the given time unit.
+   *
+   * @param delay the delay value
+   * @param timeUnit the time unit of the delay
+   * @return this control instance for fluent chaining
+   */
   public T rescheduleAfter(long delay, TimeUnit timeUnit) {
     return rescheduleAfter(timeUnit.toMillis(delay));
   }
 
+  /**
+   * Schedules an instant reconciliation. The reconciliation will be triggered as soon as possible.
+   *
+   * @return this control instance for fluent chaining
+   */
   public T reschedule() {
     this.scheduleDelay = INSTANT_RESCHEDULE;
     return (T) this;
