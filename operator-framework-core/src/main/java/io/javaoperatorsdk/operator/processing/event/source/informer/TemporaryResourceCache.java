818bc0dd8f9893472cb9e594ba58233379e19d4f
Attila Mészáros <a_meszaros@apple.com>
2026-02-05 09:20:10 +0100

    wip
    Signed-off-by: Attila Mészáros <a_meszaros@apple.com>

--- operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/event/source/informer/TemporaryResourceCache.java
+++ operator-framework-core/src/main/java/io/javaoperatorsdk/operator/processing/event/source/informer/TemporaryResourceCache.java
@@ -85,20 +85,22 @@ public class TemporaryResourceCache<T extends HasMetadata> {
       return Optional.empty();
     }
     var ed = activeUpdates.get(resourceID);
-    if (ed.decreaseActiveUpdates(updatedResourceVersion)) {
-      activeUpdates.remove(resourceID);
-      var res = ed.getLatestEventAfterLastUpdateEvent();
+    if (ed == null || !ed.decreaseActiveUpdates(updatedResourceVersion)) {
       log.debug(
-          "Zero active updates for resource id: {}; event after update event: {}; updated resource"
-              + " version: {}",
-          resourceID,
-          res.isPresent(),
-          updatedResourceVersion);
-      return res;
-    } else {
-      log.debug("Active updates {} for resource id: {}", ed.getActiveUpdates(), resourceID);
+          "Active updates {} for resource id: {}",
+          ed != null ? ed.getActiveUpdates() : 0,
+          resourceID);
       return Optional.empty();
     }
+    activeUpdates.remove(resourceID);
+    var res = ed.getLatestEventAfterLastUpdateEvent();
+    log.debug(
+        "Zero active updates for resource id: {}; event after update event: {}; updated resource"
+            + " version: {}",
+        resourceID,
+        res.isPresent(),
+        updatedResourceVersion);
+    return res;
   }
 
   public void onDeleteEvent(T resource, boolean unknownState) {
