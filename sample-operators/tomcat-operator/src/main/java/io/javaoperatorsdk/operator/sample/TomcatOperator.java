feebcd7b203ee49d3144a5c836d2006800d79d42
Attila Mészáros <a_meszaros@apple.com>
2026-02-23 17:18:05 +0100

    wip
    Signed-off-by: Attila Mészáros <a_meszaros@apple.com>

--- sample-operators/tomcat-operator/src/main/java/io/javaoperatorsdk/operator/sample/TomcatOperator.java
+++ sample-operators/tomcat-operator/src/main/java/io/javaoperatorsdk/operator/sample/TomcatOperator.java
@@ -17,25 +17,25 @@ package io.javaoperatorsdk.operator.sample;
 
 import java.io.IOException;
 
-import io.javaoperatorsdk.operator.api.config.loader.ConfigLoader;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.takes.facets.fork.FkRegex;
 import org.takes.facets.fork.TkFork;
 import org.takes.http.Exit;
 import org.takes.http.FtBasic;
 
 import io.javaoperatorsdk.operator.Operator;
+import io.javaoperatorsdk.operator.api.config.loader.ConfigLoader;
 
 public class TomcatOperator {
 
   public static void main(String[] args) throws IOException {
 
     Operator operator = new Operator(ConfigLoader.DEFAULT.applyConfigs());
-    operator.register(new TomcatReconciler(),
-            ConfigLoader.DEFAULT.applyControllerConfigs(TomcatReconciler.TOMCAT_CONTROLLER_NAME));
-    operator.register(new WebappReconciler(operator.getKubernetesClient()),
-            ConfigLoader.DEFAULT.applyControllerConfigs(WebappReconciler.WEBAPP_CONTROLLER_NAME));
+    operator.register(
+        new TomcatReconciler(),
+        ConfigLoader.DEFAULT.applyControllerConfigs(TomcatReconciler.TOMCAT_CONTROLLER_NAME));
+    operator.register(
+        new WebappReconciler(operator.getKubernetesClient()),
+        ConfigLoader.DEFAULT.applyControllerConfigs(WebappReconciler.WEBAPP_CONTROLLER_NAME));
     operator.start();
 
     new FtBasic(new TkFork(new FkRegex("/health", "ALL GOOD.")), 8080).start(Exit.NEVER);
