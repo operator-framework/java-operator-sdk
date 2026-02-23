cf21fbab6efe09d85cab72ddf76b6801fd828d6e
Attila Mészáros <a_meszaros@apple.com>
2026-02-23 20:41:41 +0100

    wip
    Signed-off-by: Attila Mészáros <a_meszaros@apple.com>

--- sample-operators/tomcat-operator/src/main/java/io/javaoperatorsdk/operator/sample/WebappReconciler.java
+++ sample-operators/tomcat-operator/src/main/java/io/javaoperatorsdk/operator/sample/WebappReconciler.java
@@ -47,11 +47,11 @@ import io.javaoperatorsdk.operator.processing.event.source.EventSource;
 import io.javaoperatorsdk.operator.processing.event.source.SecondaryToPrimaryMapper;
 import io.javaoperatorsdk.operator.processing.event.source.informer.InformerEventSource;
 
-@ControllerConfiguration(name = WebappReconciler.WEBAPP_CONTROLLER_NAME)
+@ControllerConfiguration(name = WebappReconciler.NAME)
 public class WebappReconciler implements Reconciler<Webapp>, Cleaner<Webapp> {
 
   private static final Logger log = LoggerFactory.getLogger(WebappReconciler.class);
-  public static final String WEBAPP_CONTROLLER_NAME = "webapp";
+  public static final String NAME = "webapp";
 
   private final KubernetesClient kubernetesClient;
 
