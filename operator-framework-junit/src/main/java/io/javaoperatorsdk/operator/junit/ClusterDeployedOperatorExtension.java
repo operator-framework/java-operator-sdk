e8688adc839fe7485625448dd786d546722d6bf6
Attila Mészáros <a_meszaros@apple.com>
2026-04-27 10:14:26 +0200

    wip
    Signed-off-by: Attila Mészáros <a_meszaros@apple.com>

--- operator-framework-junit/src/main/java/io/javaoperatorsdk/operator/junit/ClusterDeployedOperatorExtension.java
+++ operator-framework-junit/src/main/java/io/javaoperatorsdk/operator/junit/ClusterDeployedOperatorExtension.java
@@ -247,9 +247,7 @@ public class ClusterDeployedOperatorExtension extends AbstractOperatorExtension 
           }
         } catch (Exception logEx) {
           LOGGER.error(
-              "  Could not retrieve logs for pod '{}'",
-              pod.getMetadata().getName(),
-              logEx);
+              "  Could not retrieve logs for pod '{}'", pod.getMetadata().getName(), logEx);
         }
       }
 
